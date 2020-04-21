7dce6334bdbe5b08050b4cad613a02e435a2dbb8
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-21 22:01:05 +0000

    Add test for parity in apparatus matrices

--- packages/harpstrata/src/harpstrata/Apparatus/index.test.ts
+++ packages/harpstrata/src/harpstrata/Apparatus/index.test.ts
@@ -1,4 +1,4 @@
-import { ApparatusIds } from './types'
+import { ApparatusIds, InteractionMatrix, HalfstepIndex, HalfstepIndexRow, HalfstepIndexMatrix } from './types'
 import { getActiveApparatusIds, getApparatus } from './index'
 import { MAJOR_DIATONIC_APPARATUS } from './constants'
 
@@ -11,4 +11,29 @@ test('getApparatus function can return a major diatonic apparatus', () => {
   const actualApparatus = getApparatus(ApparatusIds.MajorDiatonic)
 
   expect(actualApparatus).toStrictEqual(MAJOR_DIATONIC_APPARATUS)
+})
+
+test('Each of the apparatus halfstep matrices have parity with their interaction matrices', () => {
+  const matricesHaveParity = (matrixA: HalfstepIndexMatrix, matrixB: InteractionMatrix): boolean => {
+    const matrix1dsMatch = (matrixA.length === matrixB.length)
+    const matrix2dsMatch = (matrixA[0].length === matrixB[0].length)
+    if (!matrix1dsMatch || !matrix2dsMatch) return false
+
+    const matricesDoNotHaveParity = matrixA.some((rowA: HalfstepIndexRow, indexY: number) => {
+      return rowA.some((elementA: HalfstepIndex | undefined, indexX: number) => {
+        const elementB = matrixB[indexY][indexX]
+        const noParityA = (elementA === undefined && elementB !== undefined)
+        const noParityB = (elementA !== undefined && elementB === undefined)
+
+        if (noParityA || noParityB) console.log(`Mismatch found at YX (${indexY},${indexX})`)
+
+        return (noParityA || noParityB)
+      })
+    })
+
+    return !matricesDoNotHaveParity
+  }
+  const majorDiatonicParity = matricesHaveParity(MAJOR_DIATONIC_APPARATUS.halfstepIndexMatrix, MAJOR_DIATONIC_APPARATUS.interactionMatrix)
+
+  expect(majorDiatonicParity).toBeTruthy()
 })
