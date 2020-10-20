6cecba3abd51839a25634e22ca3e8afff682ac00
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-20 20:25:19 +0000

    Remove harp-cell as it's now fully replaced
    ...by harp-cell-wrapper

--- packages/harpguru-core/src/components/harp-cell/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useStyles } from './use-styles'
-export { useToggleHarpCell } from './use-toggle-harp-cell'
-export { usePositionAnalysis } from './use-position-analysis'
-export { useSetPozitionRoot } from './use-set-pozition-root'
