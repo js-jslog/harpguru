0ac95f4d79c28fabfb151575235eee3819090604
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-21 16:09:21 +0000

    Run prettier

--- packages/harpguru-core/src/components/harp-cell-inaccessible/harp-cell-inaccessible.tsx
+++ packages/harpguru-core/src/components/harp-cell-inaccessible/harp-cell-inaccessible.tsx
@@ -12,18 +12,16 @@ export const HarpCellInaccessible = ({
   return <View accessible={false} style={baseStyles} />
 }
 
-const areEqual = (
-  prevProps: StyleProps,
-  nextProps: StyleProps
-) => {
+const areEqual = (prevProps: StyleProps, nextProps: StyleProps) => {
   // The baseStyle object is too deep to be successfully analysed by the memo comparison.
   // It's only the width and height which will vary in each render and they are
   // correlated so we only need to check one of them.
   const equalStyle = prevProps.baseStyles.width === nextProps.baseStyles.width
 
-  return (
-    equalStyle
-  )
+  return equalStyle
 }
 
-export const MemoHarpCellInaccessible = React.memo(HarpCellInaccessible, areEqual)
+export const MemoHarpCellInaccessible = React.memo(
+  HarpCellInaccessible,
+  areEqual
+)
