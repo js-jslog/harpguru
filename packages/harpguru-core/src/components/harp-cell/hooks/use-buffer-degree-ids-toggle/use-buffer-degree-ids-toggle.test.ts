134b89f61234de621c57a15e8838d6c8c8441909
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-21 22:07:17 +0000

    Add duplicate toggle buffer handler test

--- packages/harpguru-core/src/components/harp-cell/hooks/use-buffer-degree-ids-toggle/use-buffer-degree-ids-toggle.test.ts
+++ packages/harpguru-core/src/components/harp-cell/hooks/use-buffer-degree-ids-toggle/use-buffer-degree-ids-toggle.test.ts
@@ -14,5 +14,16 @@ test('adding a toggle id to empty toggle buffer is successful', () => {
 
   useBufferDegreeIdsToggle(DegreeIds.Flat6)
 
+  expect(setToggleDegreeIdsBuffer.mock.calls.length).toBe(1)
   expect(setToggleDegreeIdsBuffer.mock.calls[0][0]).toStrictEqual([DegreeIds.Flat6])
+})
+
+test('adding a toggle id to a toggle buffer with the id already present carries no action', () => {
+  const toggleDegreeIdsBuffer = [DegreeIds.Flat6] as ActiveDegreeIds
+  const setToggleDegreeIdsBuffer = jest.fn()
+  mockUseGlobal.mockReturnValue([toggleDegreeIdsBuffer, setToggleDegreeIdsBuffer])
+
+  useBufferDegreeIdsToggle(DegreeIds.Flat6)
+
+  expect(setToggleDegreeIdsBuffer.mock.calls.length).toBe(0)
 })
