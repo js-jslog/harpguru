58918ed7d843fb210bd81971c5af9c3dc657a1f2
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-21 21:09:01 +0000

    Add global state to store buffer data
    This buffer will be used to store updates to the
    active degree's array, before it is submitted for
    a full harpstrata update.
    
    It will allow us to respond quickly to user
    interactions and prepare for the next before
    submitting the expensive full harpstrata update.

--- packages/harpguru-core/src/global.d.ts
+++ packages/harpguru-core/src/global.d.ts
@@ -1,5 +1,5 @@
 import 'reactn'
-import type { HarpStrata } from 'harpstrata'
+import type { HarpStrata, ActiveDegreeIds } from 'harpstrata'
 
 import type { CovariantMembers } from './packages/covariance-series'
 
@@ -27,5 +27,6 @@ declare module 'reactn/default' {
     activeExperienceMode: ExperienceModes
     activeDisplayMode: DisplayModes
     lockedCovariant: CovariantMembers
+    activeDegreeIdsBuffer: ActiveDegreeIds
   }
 }
