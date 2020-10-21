63daf705cb1e27758a9df7fcd2dc45af8c60786e
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-21 15:56:37 +0000

    Fix memoisation of harp-cell-inaccessible

--- packages/harpguru-core/src/components/harp-cell-inaccessible/harp-cell-inaccessible.tsx
+++ packages/harpguru-core/src/components/harp-cell-inaccessible/harp-cell-inaccessible.tsx
@@ -11,4 +11,18 @@ export const HarpCellInaccessible = ({
   return <View accessible={false} style={baseStyles} />
 }
 
-export const MemoHarpCellInaccessible = React.memo(HarpCellInaccessible)
+const areEqual = (
+  prevProps: StyleProps,
+  nextProps: StyleProps
+) => {
+  // The baseStyle object is too deep to be successfully analysed by the memo comparison.
+  // It's only the width and height which will vary in each render and they are
+  // correlated so we only need to check one of them.
+  const equalStyle = prevProps.baseStyles.width === nextProps.baseStyles.width
+
+  return (
+    equalStyle
+  )
+}
+
+export const MemoHarpCellInaccessible = React.memo(HarpCellInaccessible, areEqual)
