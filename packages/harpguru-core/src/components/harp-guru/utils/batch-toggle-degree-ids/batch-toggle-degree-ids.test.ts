29c0f9d8b1a910b615bf1890637e33cabb65a7ed
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-22 06:38:44 +0000

    Complete tests for main batch toggle functionality
    This function could be more resilient to duplications etc, but I will
    have to come back to that once we know that this approach is working.

--- packages/harpguru-core/src/components/harp-guru/utils/batch-toggle-degree-ids/batch-toggle-degree-ids.test.ts
+++ packages/harpguru-core/src/components/harp-guru/utils/batch-toggle-degree-ids/batch-toggle-degree-ids.test.ts
@@ -5,9 +5,88 @@ import { batchToggleDegreeIds } from './batch-toggle-degree-ids'
 
 test('given an empty active degree list, all the toggle ids are added', () => {
   const activeDegreeIds = [] as ActiveDegreeIds
-  const batchToggleIds = [ DegreeIds.Flat3, DegreeIds.Root, DegreeIds.Flat7 ]
+  const batchToggleIds = [DegreeIds.Flat3, DegreeIds.Root, DegreeIds.Flat7]
 
-  const newActiveDegreeIds = batchToggleDegreeIds(activeDegreeIds, batchToggleIds)
+  const newActiveDegreeIds = batchToggleDegreeIds(
+    activeDegreeIds,
+    batchToggleIds
+  )
 
   expect(newActiveDegreeIds).toStrictEqual(batchToggleIds)
+})
+
+test('given an active degree list with none of the toggled ids, all the toggle ids are added', () => {
+  const activeDegreeIds = [DegreeIds.Fourth]
+  const batchToggleIds = [DegreeIds.Flat3, DegreeIds.Root, DegreeIds.Flat7]
+
+  const newActiveDegreeIds = batchToggleDegreeIds(
+    activeDegreeIds,
+    batchToggleIds
+  )
+
+  expect(newActiveDegreeIds).toStrictEqual([
+    ...activeDegreeIds,
+    ...batchToggleIds,
+  ])
+})
+
+test('given an active degree list with all and only the toggled ids, all the toggle ids are removed', () => {
+  const activeDegreeIds = [DegreeIds.Flat7, DegreeIds.Root, DegreeIds.Flat3]
+  const batchToggleIds = [DegreeIds.Flat3, DegreeIds.Root, DegreeIds.Flat7]
+
+  const newActiveDegreeIds = batchToggleDegreeIds(
+    activeDegreeIds,
+    batchToggleIds
+  )
+
+  expect(newActiveDegreeIds).toStrictEqual([])
+})
+
+test('given an active degree list with all and the toggled ids and a few more, all the toggle ids are removed', () => {
+  const activeDegreeIds = [
+    DegreeIds.Flat7,
+    DegreeIds.Second,
+    DegreeIds.Root,
+    DegreeIds.Flat5,
+    DegreeIds.Flat3,
+  ]
+  const batchToggleIds = [DegreeIds.Flat3, DegreeIds.Root, DegreeIds.Flat7]
+  const expectedNewActiveDegreeIds = [DegreeIds.Second, DegreeIds.Flat5]
+
+  const newActiveDegreeIds = batchToggleDegreeIds(
+    activeDegreeIds,
+    batchToggleIds
+  )
+
+  expect(newActiveDegreeIds).toStrictEqual(expectedNewActiveDegreeIds)
+})
+
+test('given an active degree list with some of and the toggled ids, the absent ones are added and the present ones are removed', () => {
+  const activeDegreeIds = [
+    DegreeIds.Flat7,
+    DegreeIds.Second,
+    DegreeIds.Root,
+    DegreeIds.Flat5,
+    DegreeIds.Flat3,
+  ]
+  const batchToggleIds = [
+    DegreeIds.Flat3,
+    DegreeIds.Flat2,
+    DegreeIds.Root,
+    DegreeIds.Flat7,
+    DegreeIds.Seventh,
+  ]
+  const expectedNewActiveDegreeIds = [
+    DegreeIds.Second,
+    DegreeIds.Flat5,
+    DegreeIds.Flat2,
+    DegreeIds.Seventh,
+  ]
+
+  const newActiveDegreeIds = batchToggleDegreeIds(
+    activeDegreeIds,
+    batchToggleIds
+  )
+
+  expect(newActiveDegreeIds).toStrictEqual(expectedNewActiveDegreeIds)
 })
--- packages/harpguru-core/src/components/harp-guru/utils/batch-toggle-degree-ids/batch-toggle-degree-ids.ts
+++ packages/harpguru-core/src/components/harp-guru/utils/batch-toggle-degree-ids/batch-toggle-degree-ids.ts
@@ -1,7 +1,16 @@
 import type { ActiveDegreeIds } from 'harpstrata'
 
-export const batchToggleDegreeIds = (activeDegreeIds: ActiveDegreeIds, batchToggleIds: ActiveDegreeIds): ActiveDegreeIds => {
-  const toggleOnList = batchToggleIds.filter(id => !activeDegreeIds.includes(id))
-  const toggleOffList = batchToggleIds.filter(id => activeDegreeIds.includes(id))
-  return [ ...activeDegreeIds, ...toggleOnList ].filter(id => !toggleOffList.includes(id))
+export const batchToggleDegreeIds = (
+  activeDegreeIds: ActiveDegreeIds,
+  batchToggleIds: ActiveDegreeIds
+): ActiveDegreeIds => {
+  const toggleOnList = batchToggleIds.filter(
+    (id) => !activeDegreeIds.includes(id)
+  )
+  const toggleOffList = batchToggleIds.filter((id) =>
+    activeDegreeIds.includes(id)
+  )
+  return [...activeDegreeIds, ...toggleOnList].filter(
+    (id) => !toggleOffList.includes(id)
+  )
 }
