8459de25f044d623c7ae974bd56465c0c123afda
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-22 07:12:55 +0000

    Add candidate toggle hook for harp-cell
    This will add a toggle to the toggle buffer rather than updating the
    harpstrata directly.

--- packages/harpguru-core/src/components/harp-cell/hooks/use-buffer-degree-ids-toggle/use-buffer-degree-ids-toggle.test.ts
+++ packages/harpguru-core/src/components/harp-cell/hooks/use-buffer-degree-ids-toggle/use-buffer-degree-ids-toggle.test.ts
@@ -1,6 +1,6 @@
 import { useGlobal } from 'reactn'
 import { DegreeIds } from 'harpstrata'
-import type {ActiveDegreeIds} from 'harpstrata'
+import type { ActiveDegreeIds } from 'harpstrata'
 
 import { useBufferDegreeIdsToggle } from './use-buffer-degree-ids-toggle'
 
@@ -10,20 +10,28 @@ const mockUseGlobal = useGlobal as jest.Mock
 test('adding a toggle id to empty toggle buffer is successful', () => {
   const toggleDegreeIdsBuffer = [] as ActiveDegreeIds
   const setToggleDegreeIdsBuffer = jest.fn()
-  mockUseGlobal.mockReturnValue([toggleDegreeIdsBuffer, setToggleDegreeIdsBuffer])
+  mockUseGlobal.mockReturnValue([
+    toggleDegreeIdsBuffer,
+    setToggleDegreeIdsBuffer,
+  ])
 
-  useBufferDegreeIdsToggle(DegreeIds.Flat6)
+  useBufferDegreeIdsToggle()(DegreeIds.Flat6)
 
   expect(setToggleDegreeIdsBuffer.mock.calls.length).toBe(1)
-  expect(setToggleDegreeIdsBuffer.mock.calls[0][0]).toStrictEqual([DegreeIds.Flat6])
+  expect(setToggleDegreeIdsBuffer.mock.calls[0][0]).toStrictEqual([
+    DegreeIds.Flat6,
+  ])
 })
 
 test('adding a toggle id to a toggle buffer with the id already present carries no action', () => {
   const toggleDegreeIdsBuffer = [DegreeIds.Flat6] as ActiveDegreeIds
   const setToggleDegreeIdsBuffer = jest.fn()
-  mockUseGlobal.mockReturnValue([toggleDegreeIdsBuffer, setToggleDegreeIdsBuffer])
+  mockUseGlobal.mockReturnValue([
+    toggleDegreeIdsBuffer,
+    setToggleDegreeIdsBuffer,
+  ])
 
-  useBufferDegreeIdsToggle(DegreeIds.Flat6)
+  useBufferDegreeIdsToggle()(DegreeIds.Flat6)
 
   expect(setToggleDegreeIdsBuffer.mock.calls.length).toBe(0)
 })
