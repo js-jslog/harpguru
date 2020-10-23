dca9572f498e8f024d3cb1b400b73a7dc8349fae
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-23 16:07:48 +0000

    Correct test for previous commit

--- packages/harpguru-core/src/components/harp-cell/hooks/use-buffer-degree-ids-toggle/use-buffer-degree-ids-toggle.test.ts
+++ packages/harpguru-core/src/components/harp-cell/hooks/use-buffer-degree-ids-toggle/use-buffer-degree-ids-toggle.test.ts
@@ -23,8 +23,8 @@ test('adding a toggle id to empty toggle buffer is successful', () => {
   ])
 })
 
-test('adding a toggle id to a toggle buffer with the id already present carries no action', () => {
-  const toggleDegreeIdsBuffer = [DegreeIds.Flat6] as ActiveDegreeIds
+test('adding a toggle id to a toggle buffer with the id already present calls the setter again with the same array', () => {
+  const toggleDegreeIdsBuffer = [DegreeIds.Flat2, DegreeIds.Flat6] as ActiveDegreeIds
   const setToggleDegreeIdsBuffer = jest.fn()
   mockUseGlobal.mockReturnValue([
     toggleDegreeIdsBuffer,
@@ -33,5 +33,9 @@ test('adding a toggle id to a toggle buffer with the id already present carries 
 
   useBufferDegreeIdsToggle()(DegreeIds.Flat6)
 
-  expect(setToggleDegreeIdsBuffer.mock.calls.length).toBe(0)
+  expect(setToggleDegreeIdsBuffer.mock.calls.length).toBe(1)
+  expect(setToggleDegreeIdsBuffer.mock.calls[0][0]).toStrictEqual([
+    DegreeIds.Flat2,
+    DegreeIds.Flat6,
+  ])
 })
