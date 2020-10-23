2d01a65a8fd030e26fae597070504238568b5c7b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-23 22:54:11 +0000

    Remove unused utils and hooks
    These are all either duplicates from when harp-cell was copied to make
    harp-cell-accessible, or have just been made redundant by the
    introduction of the buffer.

--- packages/harpguru-core/src/components/harp-cell/hooks/index.ts
+++ packages/harpguru-core/src/components/harp-cell/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useBufferDegreeIdsToggle } from './use-buffer-degree-ids-toggle'
-export { useToggleHarpCell } from './use-toggle-harp-cell'
 export { usePositionAnalysis } from './use-position-analysis'
 export { useSetPozitionRoot } from './use-set-pozition-root'
