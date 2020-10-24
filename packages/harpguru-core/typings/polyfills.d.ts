03048207ab8b87087c86338a71d2a9545f1f41de
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-24 10:44:54 +0000

    Run prettier

--- packages/harpguru-core/typings/polyfills.d.ts
+++ packages/harpguru-core/typings/polyfills.d.ts
@@ -6,8 +6,15 @@ declare module 'react-native' {
   // these function defs at https://unpkg.com/@types/react-dom@16.9.8/index.d.ts
   // (adminttedly that is react-dom, but it seems to be working)
   /* eslint-disable */
-  export function unstable_batchedUpdates<A, B>(callback: (a: A, b: B) => any, a: A, b: B): void
-  export function unstable_batchedUpdates<A>(callback: (a: A) => any, a: A): void
+  export function unstable_batchedUpdates<A, B>(
+    callback: (a: A, b: B) => any,
+    a: A,
+    b: B
+  ): void
+  export function unstable_batchedUpdates<A>(
+    callback: (a: A) => any,
+    a: A
+  ): void
   export function unstable_batchedUpdates(callback: () => any): void
   /* eslint-enable */
 }
