395b610878df85a5f342f832aaf3aa9922bc7bac
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-25 18:27:39 +0000

    Add `useEffect` so that cell is animated on every tap
    See comment for details

--- packages/harpguru-core/src/components/harp-cell/hooks/use-tap-rerender-logic/use-tap-rerender-logic.ts
+++ packages/harpguru-core/src/components/harp-cell/hooks/use-tap-rerender-logic/use-tap-rerender-logic.ts
@@ -33,12 +33,20 @@ export const useTapRerenderLogic = (
     } else if (cancelToggleStates.includes(nativeEvent.state)) {
       const relevantState =
         thisIsActiveId === IsActiveIds.Active ? CellStates.On : CellStates.Off
+      // TODO: if the degree is in the buffer then this should
+      // be factored in to determining what the appropriate state is
       setCellState(relevantState)
     } else if (nativeEvent.state === State.END) {
       addBufferedActivityToggle(thisDegreeId)
     }
   }
 
+  // This ensures that once the harpstrata's activity has been updated from
+  // the toggle buffer, a render is produced in all the cells by their state
+  // being set accordingly. It is preferable to drive the cell renders from
+  // this state rather than the harpstrata's activity directly since so much
+  // of the cells render logic is already associated with it's CellState and
+  // it will be easier to grasp the logic if it is centred on a single value.
   React.useEffect(() => {
     if (thisIsActiveId === undefined) return
     const appropriateCellState =
@@ -47,6 +55,25 @@ export const useTapRerenderLogic = (
       setCellState(appropriateCellState)
     }
   }, [thisIsActiveId, setCellState])
+
+  // This sets the cellState to the untapped version after an interval sufficiently
+  // long for the associated animation to have completed.
+  // The reason this is required is because when the user taps the cell again
+  // we still want the animation to convey the recognition of the action, even
+  // if that action will not have a new effect until the harpface has updated
+  // with all of the updated harpstrata activity.
+  React.useEffect(() => {
+    if (cellState !== CellStates.TappedOn && cellState !== CellStates.TappedOff)
+      return
+    const postTapStateSet = setTimeout(() => {
+      const appropriateCellState =
+        cellState === CellStates.TappedOn ? CellStates.On : CellStates.Off
+      setCellState(appropriateCellState)
+    }, 100)
+    return () => {
+      clearTimeout(postTapStateSet)
+    }
+  }, [cellState, setCellState])
 
   return [cellState, tapHandler]
 }
