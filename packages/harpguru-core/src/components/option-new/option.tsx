dbfd9dd0a770a6e3161b5d8ca99a55ee121a49fd
js-jslog <jhs4jbs@hotmail.co.uk>
2020-10-26 19:46:26 +0000

    Replace binary option with non-scrolling implementation

--- packages/harpguru-core/src/components/option-new/option.tsx
+++ packages/harpguru-core/src/components/option-new/option.tsx
@@ -22,6 +22,29 @@ const OptionTitle = ({ children }: TitleProps): React.ReactElement => {
   return <Text style={styles.optionTitle}>{children}</Text>
 }
 
+type OptionDisplayList = [
+  OptionIds | undefined,
+  OptionIds | undefined,
+  OptionIds | undefined,
+  OptionIds | undefined,
+  OptionIds | undefined,
+  OptionIds | undefined
+]
+const selectFiveOptions = (
+  orderedOptionIds: ReadonlyArray<OptionIds>
+): OptionDisplayList => {
+  if (orderedOptionIds.length <= 6) {
+    const arrayToFill = [0, 0, 0, 0, 0, 0]
+    const retVal = arrayToFill.map((_element, index) => {
+      if (orderedOptionIds[index]) return orderedOptionIds[index]
+      return undefined
+    }) as OptionDisplayList
+
+    return retVal
+  }
+  return orderedOptionIds.slice(0, 6) as OptionDisplayList
+}
+
 export const Option = (props: OptionProps): React.ReactElement => {
   const styles = getStyles()
   const { title, activeOptionId, orderedOptionIds, setFunction } = props
@@ -43,59 +66,44 @@ export const Option = (props: OptionProps): React.ReactElement => {
     }
   }, [preactiveId])
 
-  const extendedList = [
-    undefined,
-    undefined,
-    ...orderedOptionIds,
-    undefined,
-    undefined,
-  ]
-
-  const activeIndex = orderedOptionIds.indexOf(activeOptionId)
-  const innerActiveIdPos = activeIndex + 2
-
-  const { [innerActiveIdPos + 2]: inactiveOptionId1 } = extendedList
-  const { [innerActiveIdPos + 1]: inactiveOptionId2 } = extendedList
-  const { [innerActiveIdPos - 1]: inactiveOptionId3 } = extendedList
-  const { [innerActiveIdPos - 2]: inactiveOptionId4 } = extendedList
-  const { [innerActiveIdPos - 3]: inactiveOptionId5 } = extendedList
+  const visibleOptionList = selectFiveOptions(orderedOptionIds)
 
   return (
     <View style={[styles.option]}>
       <OptionTitle>{title}</OptionTitle>
       <View>
         <OptionValue
-          id={inactiveOptionId1}
+          id={visibleOptionList[0]}
           preactiveId={preactiveId}
           setFunction={setActiveOptionCallback}
           cancelFunction={cancelActiveOptionCallback}
         />
         <OptionValue
-          id={inactiveOptionId2}
+          id={visibleOptionList[1]}
           preactiveId={preactiveId}
           setFunction={setActiveOptionCallback}
           cancelFunction={cancelActiveOptionCallback}
         />
         <OptionValue
-          id={activeOptionId}
+          id={visibleOptionList[2]}
           preactiveId={preactiveId}
           setFunction={setActiveOptionCallback}
           cancelFunction={cancelActiveOptionCallback}
         />
         <OptionValue
-          id={inactiveOptionId3}
+          id={visibleOptionList[3]}
           preactiveId={preactiveId}
           setFunction={setActiveOptionCallback}
           cancelFunction={cancelActiveOptionCallback}
         />
         <OptionValue
-          id={inactiveOptionId4}
+          id={visibleOptionList[4]}
           preactiveId={preactiveId}
           setFunction={setActiveOptionCallback}
           cancelFunction={cancelActiveOptionCallback}
         />
         <OptionValue
-          id={inactiveOptionId5}
+          id={visibleOptionList[5]}
           preactiveId={preactiveId}
           setFunction={setActiveOptionCallback}
           cancelFunction={cancelActiveOptionCallback}
