51f363c84b800641fe66f72a5c55a06aebfbc123
js-jslog <jhs4jbs@hotmail.co.uk>
2020-11-17 22:51:03 +0000

    Include type polyfills for dependants
    If the dependants are importing a non-compiled version of the package
    then they will need to benefit from the very same type polyfills that
    this project needs. Since these files won't be automatically imported
    from the canonical file paths in the local project when the project is
    an import, we need to make sure they are included by other means.

--- packages/harpguru-core/src/index.ts
+++ packages/harpguru-core/src/index.ts
@@ -1,1 +1,11 @@
+// Import type definitions to index.ts so that
+// dependants benefit from them too.
+// These imports aren't required for this project
+// since `global` and `typings` are referenced
+// automatically in a local context. When this
+// project is imported, these types are otherwise
+// ignored which is a problem.
+import './global'
+import '../typings/polyfills.d'
+
 export { HarpGuru } from './components'
