877b0986634f58053ce3ff9e9a3f6141fa72d59a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-11-17 23:21:30 +0000

    Add missing type and remove redundant comment
    The comment isn't required any more because I'm pretty committed to not
    using the `useDispatch` anymore.
    
    The missing type was overlooked because it happened inside a `declare`
    statement (I think), which (I think) means that typechecking cannot be
    applied: https://stackoverflow.com/a/40700848/4402041

--- packages/harpguru-core/src/global.d.ts
+++ packages/harpguru-core/src/global.d.ts
@@ -1,17 +1,9 @@
 import 'reactn'
-import type { HarpStrata } from 'harpstrata'
+import type { HarpStrata, DegreeIds } from 'harpstrata'
 
 import type { CovariantMembers } from './packages/covariance-series'
 
 import type { DisplayModes, ExperienceModes } from './types'
-
-// All of the `dispatch: any` lines in here are
-// required because of a typescript complaint
-// about circular references on the `useDispatch`
-// calls which reference these reducers. I believe
-// I have followed the rules perfectly here but I
-// cannot figure out why this error occurs and this
-// is the best workaround I can figure out for now.
 
 declare module 'reactn/default' {
   export interface State {
