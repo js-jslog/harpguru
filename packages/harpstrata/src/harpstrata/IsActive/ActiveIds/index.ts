c8695af78e1743470425544ed18dd6d54be3d420
js-jslog <jhs4jbs@hotmail.co.uk>
2020-11-23 22:21:20 +0000

    Rename the IsActive test resources
    The idea of a "complex" is no longer applicable so we need to rename all
    the relevant test resources.

--- packages/harpstrata/src/harpstrata/IsActive/ActiveIds/index.ts
+++ packages/harpstrata/src/harpstrata/IsActive/ActiveIds/index.ts
@@ -1,4 +1,4 @@
 export { getActiveIdsPair } from './getActiveIdsPair'
 export type { ActiveDegreeIds, ActivePitchIds, ActiveIds } from './types'
 
-export { EXAMPLE_ACTIVE_IDS } from './testResources'
+export { EXAMPLE_IS_ACTIVE_IDS_PAIR } from './testResources'
--- packages/harpstrata/src/harpstrata/IsActive/index.ts
+++ packages/harpstrata/src/harpstrata/IsActive/index.ts
@@ -4,4 +4,4 @@ export type { ActiveDegreeIds, ActivePitchIds, ActiveIds } from './ActiveIds'
 
 export { IsActiveIds } from './types'
 
-export { EXAMPLE_IS_ACTIVE_COMPLEXES } from './testResources'
+export { EXAMPLE_IS_ACTIVE_IDS_PAIR } from './ActiveIds'
