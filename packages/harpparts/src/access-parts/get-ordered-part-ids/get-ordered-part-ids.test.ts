a776c7a05fa0ee0808754a57dd2a8b72306ba1fb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-11-25 20:06:55 +0000

    Complete basic orderable id functionality

--- packages/harpparts/src/access-parts/get-ordered-part-ids/get-ordered-part-ids.test.ts
+++ packages/harpparts/src/access-parts/get-ordered-part-ids/get-ordered-part-ids.test.ts
@@ -1,10 +1,31 @@
 import { OrderableParts } from '../types'
+import { PozitionIds } from '../../pozition'
+import { PitchIds } from '../../pitch'
+import { DegreeIds } from '../../degree'
 import { ApparatusIds } from '../../apparatus'
 
 import { getOrderedPartIds } from './get-ordered-part-ids'
 
-test('getOrderedPartIds can returns some apparatus ids', () => {
+test('getOrderedPartIds can returns some ApparatusIds', () => {
   const orderedApparatusIds = getOrderedPartIds(OrderableParts.Apparatus)
   expect(orderedApparatusIds.length).toBeTruthy()
   expect(orderedApparatusIds).toContain(ApparatusIds.MajorDiatonic)
+})
+
+test('getOrderedPartIds can return some DegreeIds', () => {
+  const orderedApparatusIds = getOrderedPartIds(OrderableParts.Degrees)
+  expect(orderedApparatusIds.length).toBeTruthy()
+  expect(orderedApparatusIds).toContain(DegreeIds.Root)
+})
+
+test('getOrderedPartIds can return some PitchIds', () => {
+  const orderedApparatusIds = getOrderedPartIds(OrderableParts.Pitches)
+  expect(orderedApparatusIds.length).toBeTruthy()
+  expect(orderedApparatusIds).toContain(PitchIds.A)
+})
+
+test('getOrderedPartIds can return some PozitionIds', () => {
+  const orderedApparatusIds = getOrderedPartIds(OrderableParts.Pozitions)
+  expect(orderedApparatusIds.length).toBeTruthy()
+  expect(orderedApparatusIds).toContain(PozitionIds.First)
 })
--- packages/harpparts/src/access-parts/get-ordered-part-ids/get-ordered-part-ids.ts
+++ packages/harpparts/src/access-parts/get-ordered-part-ids/get-ordered-part-ids.ts
@@ -1,10 +1,21 @@
 import { OrderablePartId, OrderableParts } from '../types'
-import { ORDERED_APPARATUS } from '../constants'
+import {
+  ORDERED_APPARATUS,
+  ORDERED_DEGREES,
+  ORDERED_PITCHES,
+  ORDERED_POZITIONS,
+} from '../constants'
 
 export const getOrderedPartIds = (
   partType: OrderableParts
 ): ReadonlyArray<OrderablePartId> => {
   if (partType === OrderableParts.Apparatus)
     return Object.values(ORDERED_APPARATUS).map((part) => part.id)
+  if (partType === OrderableParts.Degrees)
+    return Object.values(ORDERED_DEGREES).map((part) => part.id)
+  if (partType === OrderableParts.Pitches)
+    return Object.values(ORDERED_PITCHES).map((part) => part.id)
+  if (partType === OrderableParts.Pozitions)
+    return Object.values(ORDERED_POZITIONS).map((part) => part.id)
   throw 'this is not acceptable'
 }
