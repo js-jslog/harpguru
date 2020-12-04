157f9fabdadb2ad4368fffd03253bf08c0ecc440
js-jslog <jhs4jbs@hotmail.co.uk>
2020-12-04 16:17:10 +0000

    Ensure all tabs protrusions are in sync
    'Ensure' to the extent that they will resize on harpstrata update and
    since there is a harpstrata update as soon as the app starts, and it's
    only upon app opening that I've detecetd a problem with elements needing
    resizing, I think we've implicitely got the sync points covered.

--- packages/harpguru-core/src/components/next-page-button/next-page-button.tsx
+++ packages/harpguru-core/src/components/next-page-button/next-page-button.tsx
@@ -1,3 +1,4 @@
+import { useGlobal } from 'reactn'
 import { Text, View } from 'react-native'
 import React from 'react'
 
@@ -20,6 +21,13 @@ export const NextPageButton = ({
   stashPosition,
   getNextPage,
 }: NextPageButtonProps): React.ReactElement => {
+  // This line only exists to make sure that this tab rerenders
+  // when the harpstrata changes just like the other menu tabs
+  // if it doesn't then it's possible that the tabs' protrusions
+  // will become unequal after the screen is resized.
+  // This is really a hacky workaround, but it's cheap and effective.
+  // eslint-disable-next-line no-empty-pattern
+  const [] = useGlobal('activeHarpStrata')
   const menuLikeProps: MenuProps = {
     isMenuStashed: true,
     isLabelHidden: false,
