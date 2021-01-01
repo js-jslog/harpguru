11f66dee5a2cd89946d181c9413a9f7e63aeb1d7
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-01 16:34:18 +0000

    Add functionality to handle PitchIds quiz questions
    The toggle buffer only buffers DegreeIds values so performing this check
    on PitchIds quiz questions requires some conversion.

--- packages/harpguru-core/src/components/quiz-question-display/utils/has-toggled-incorrect-cell/has-toggled-incorrect-cell.test.ts
+++ packages/harpguru-core/src/components/quiz-question-display/utils/has-toggled-incorrect-cell/has-toggled-incorrect-cell.test.ts
@@ -1,22 +1,45 @@
-import { DegreeIds } from 'harpparts'
+import { DegreeIds, PitchIds, PozitionIds } from 'harpparts'
 
 import { hasToggledIncorrectCell } from './has-toggled-incorrect-cell'
 
 test('returns false when toggle buffer is empty', () => {
   const { Root: quizQuestion } = DegreeIds
   const toggleBuffer = [] as ReadonlyArray<DegreeIds>
+  const { C: harpKeyId } = PitchIds
+  const { First: pozitionId } = PozitionIds
 
-  expect(hasToggledIncorrectCell(toggleBuffer, quizQuestion)).toBeFalsy()
+  expect(
+    hasToggledIncorrectCell(toggleBuffer, quizQuestion, harpKeyId, pozitionId)
+  ).toBeFalsy()
 })
 test('returns false when toggle buffer only has the quiz question in it', () => {
   const { Root: quizQuestion } = DegreeIds
   const toggleBuffer = [DegreeIds.Root]
+  const { C: harpKeyId } = PitchIds
+  const { First: pozitionId } = PozitionIds
 
-  expect(hasToggledIncorrectCell(toggleBuffer, quizQuestion)).toBeFalsy()
+  expect(
+    hasToggledIncorrectCell(toggleBuffer, quizQuestion, harpKeyId, pozitionId)
+  ).toBeFalsy()
 })
 test('returns true when toggle buffer has a single incorrect value in it', () => {
   const { Root: quizQuestion } = DegreeIds
   const toggleBuffer = [DegreeIds.Second]
+  const { C: harpKeyId } = PitchIds
+  const { First: pozitionId } = PozitionIds
 
-  expect(hasToggledIncorrectCell(toggleBuffer, quizQuestion)).toBeTruthy()
+  expect(
+    hasToggledIncorrectCell(toggleBuffer, quizQuestion, harpKeyId, pozitionId)
+  ).toBeTruthy()
+})
+
+test('returns false when the quizQuestion is the correlated Pitch value', () => {
+  const { C: quizQuestion } = PitchIds
+  const toggleBuffer = [DegreeIds.Root]
+  const { C: harpKeyId } = PitchIds
+  const { First: pozitionId } = PozitionIds
+
+  expect(
+    hasToggledIncorrectCell(toggleBuffer, quizQuestion, harpKeyId, pozitionId)
+  ).toBeFalsy()
 })
--- packages/harpguru-core/src/components/quiz-question-display/utils/has-toggled-incorrect-cell/has-toggled-incorrect-cell.ts
+++ packages/harpguru-core/src/components/quiz-question-display/utils/has-toggled-incorrect-cell/has-toggled-incorrect-cell.ts
@@ -1,8 +1,28 @@
-import type { DegreeIds } from 'harpparts'
+import { getHarpStrata } from 'harpstrata'
+import type { DegreeIds, PitchIds, PozitionIds } from 'harpparts'
+import { isPitchId, getApparatusIds } from 'harpparts'
 
 export const hasToggledIncorrectCell = (
   toggleBuffer: ReadonlyArray<DegreeIds>,
-  quizQuestion: DegreeIds
+  quizQuestion: DegreeIds | PitchIds,
+  harpKeyId: PitchIds,
+  pozitionId: PozitionIds
 ): boolean => {
-  return !toggleBuffer.every((degree) => degree === quizQuestion)
+  if (!isPitchId(quizQuestion))
+    return !toggleBuffer.every((degree) => degree === quizQuestion)
+
+  const surrogateHarpStrata = getHarpStrata({
+    apparatusId: getApparatusIds()[0],
+    pozitionId,
+    harpKeyId,
+    activeIds: [quizQuestion],
+  })
+
+  const {
+    activeDegreeIds: { [0]: counterpartDegreeQuizQuestion },
+  } = surrogateHarpStrata
+
+  return !toggleBuffer.every(
+    (degree) => degree === counterpartDegreeQuizQuestion
+  )
 }
