c7ec30f646159ec53e8beb7166d5d4e8a2c5b0ba
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-01 16:10:45 +0000

    Add tests for expected behaviour (existing implementation)

--- packages/harpguru-core/src/components/quiz-question-display/utils/has-toggled-incorrect-cell/has-toggled-incorrect-cell.test.ts
+++ packages/harpguru-core/src/components/quiz-question-display/utils/has-toggled-incorrect-cell/has-toggled-incorrect-cell.test.ts
@@ -2,9 +2,21 @@ import { DegreeIds } from 'harpparts'
 
 import { hasToggledIncorrectCell } from './has-toggled-incorrect-cell'
 
+test('returns false when toggle buffer is empty', () => {
+  const { Root: quizQuestion } = DegreeIds
+  const toggleBuffer = [] as ReadonlyArray<DegreeIds>
+
+  expect(hasToggledIncorrectCell(toggleBuffer, quizQuestion)).toBeFalsy()
+})
 test('returns false when toggle buffer only has the quiz question in it', () => {
   const { Root: quizQuestion } = DegreeIds
   const toggleBuffer = [DegreeIds.Root]
 
   expect(hasToggledIncorrectCell(toggleBuffer, quizQuestion)).toBeFalsy()
+})
+test('returns true when toggle buffer has a single incorrect value in it', () => {
+  const { Root: quizQuestion } = DegreeIds
+  const toggleBuffer = [DegreeIds.Second]
+
+  expect(hasToggledIncorrectCell(toggleBuffer, quizQuestion)).toBeTruthy()
 })
