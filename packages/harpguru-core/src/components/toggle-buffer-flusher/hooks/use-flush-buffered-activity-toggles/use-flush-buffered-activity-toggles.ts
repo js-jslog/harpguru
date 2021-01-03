46f0a15dec808ad3fd7ffa661068298a7e7b571e
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-03 17:22:42 +0000

    Split the toggle flushing dispatch updates apart
    It seems like trying to update both of the global properties in a single
    dispatch might lead to a perceived update to all of the properties in
    the global state, while updating individually like this means that we
    only get a single update for each one.
    
    It's possible that there's a better way to do this with reactn's
    dispatches, but this will do for now. I have not tested whether this
    results in two sets of renders and whether we might benefit from
    reintroducing unstable_batchUpdates to streamline that.
    
    The mystery posed in the extended comment in this commit is a real one
    and one I would like to get to the bottom of.

--- packages/harpguru-core/src/components/toggle-buffer-flusher/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
+++ packages/harpguru-core/src/components/toggle-buffer-flusher/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
@@ -1,10 +1,10 @@
 import { useGlobal, useDispatch } from 'reactn'
 import { useEffect, useState } from 'react'
-import type { HarpStrataProps } from 'harpstrata'
+import type { HarpStrataProps, HarpStrata } from 'harpstrata'
 import { getHarpStrata } from 'harpstrata'
+import type { DegreeIds } from 'harpparts'
 
 import { batchToggleDegreeIds } from '../../utils'
-import type { GlobalState } from '../../../../types'
 
 export const useFlushBufferedActivityToggles = (): [
   (arg0: boolean) => void,
@@ -16,14 +16,11 @@ export const useFlushBufferedActivityToggles = (): [
     false
   )
 
-  const flushBufferedToggles = useDispatch(
-    ({
-      bufferedActivityToggles,
-      activeHarpStrata,
-    }: Pick<GlobalState, 'bufferedActivityToggles' | 'activeHarpStrata'>):
-      | undefined
-      | Pick<GlobalState, 'bufferedActivityToggles' | 'activeHarpStrata'> => {
-      if (bufferedActivityToggles.length === 0) return
+  const updateActiveHarpStrata = useDispatch(
+    (
+      activeHarpStrata: HarpStrata,
+      bufferedActivityToggles: ReadonlyArray<DegreeIds>
+    ): HarpStrata => {
       const {
         apparatus: { id: apparatusId },
         pozitionId,
@@ -40,24 +37,51 @@ export const useFlushBufferedActivityToggles = (): [
         harpKeyId,
         activeIds: newActiveDegreeIds,
       }
-      return {
-        bufferedActivityToggles: [],
-        activeHarpStrata: getHarpStrata(newHarpStrataProps),
-      }
-    }
+      return getHarpStrata(newHarpStrataProps)
+    },
+    'activeHarpStrata'
   )
+
+  const emptyBufferedToggles = useDispatch(
+    (): [] => [],
+    'bufferedActivityToggles'
+  )
+
+  const flushBufferedToggles = (
+    bufferedActivityToggles: ReadonlyArray<DegreeIds>
+  ) => {
+    if (bufferedActivityToggles.length === 0) return
+    // It is essential that the harpstrata update is the last thing to
+    // happen. Otherwise the scale notification flash won't happen. I
+    // think this might be because the `usePrevious` hook in that
+    // component is somehow missing a step in it's history and not
+    // seeing the change. You can test this by printing out the
+    // current and previous scale label values each time it renders.
+
+    // It is a complete mystery why this problem was introduced at
+    // commit 9a2d523b43741ae3bf44b390b3371cb3a7d38a08 when only the
+    // quiz cycle component was modified there and the problem isn't
+    // resolved if you remove that component from the app entirely..
+    // It's an even bigger surprise when you consider that the previous
+    // commit at 49c0e580d532a675586be6e4880b5ab31251594b removed the
+    // batch processing from this toggle flush hook, which seems like
+    // something which would be much more likely to be the cause of
+    // such an issue.
+    emptyBufferedToggles()
+    updateActiveHarpStrata(bufferedActivityToggles)
+  }
 
   useEffect(() => {
     if (!isOverridden) return
     if (shouldForceFlush === false) return
-    flushBufferedToggles()
-    setShouldForceFlush(false)
-  }, [isOverridden, shouldForceFlush])
+    flushBufferedToggles(bufferedActivityToggles)
+    return setShouldForceFlush(false)
+  }, [bufferedActivityToggles, isOverridden, shouldForceFlush])
 
   useEffect(() => {
     if (isOverridden) return
     const regularFlushBufferedToggles = setTimeout(() => {
-      flushBufferedToggles()
+      flushBufferedToggles(bufferedActivityToggles)
     }, 1000)
     return () => {
       clearTimeout(regularFlushBufferedToggles)
