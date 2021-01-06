11e287bf5d66222f2fdf709cb5e5a3781d377f27
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-06 23:01:48 +0000

    Add comment describing need for batch update

--- packages/harpguru-core/src/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
+++ packages/harpguru-core/src/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
@@ -35,6 +35,19 @@ export const useFlushBufferedActivityToggles = (): (() => void) => {
     }
   })
 
+  // The dispatch seems to function just
+  // fine without the batch wrapper, except
+  // when it's being called from a `setTimeout`
+  // function, which is something we use quite
+  // frequently in this project. I wonder
+  // whether this is something to do with a
+  // failure to operate properly from async
+  // contexts. This could use investigation.
+  // Possibly if I were actually returning
+  // a Promise here rather than a function
+  // which runs the Promise then things would
+  // work better. Lot's to consider, but this
+  // at least appears to work well.
   return () => {
     unstable_batchedUpdates(() => {
       updateActiveHarpStrata()
