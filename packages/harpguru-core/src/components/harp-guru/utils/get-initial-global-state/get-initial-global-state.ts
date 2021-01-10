5ff634bea0c86538015c61192cd34fba383d4b98
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-10 17:04:07 +0000

    Empty toggle buffer for all but 1st page
    I don't want there to be renders happening at the point that the app
    loads which aren't seen. It's a waste of renders. If it feels wrong to
    not have cells highlighted on the other screens then we can just set
    them in the activeHarpStrata rather than in the bufferedActivityToggles.

--- packages/harpguru-core/src/components/harp-guru/utils/get-initial-global-state/get-initial-global-state.ts
+++ packages/harpguru-core/src/components/harp-guru/utils/get-initial-global-state/get-initial-global-state.ts
@@ -27,25 +27,6 @@ export const getInitialGlobalState = (pageNumber: PageNumber): GlobalState => {
   }
   const { [pageNumber]: pozitionId } = pozitionMap
 
-  // The idea of what happens next is that since the 12 bar blues will be
-  // the most common use case, I want the 3 pages to represent the I, IV & V
-  // chords in 2nd, 1st & 3rd positions respectively, with the notes of the
-  // major pentatonic of the I chord highlighted in all of them.
-  // Basically for the benefits illustrated in https://youtu.be/ZyNb39rPpng
-  const { activePitchIds: pentatonicPitches2ndPoz } = getHarpStrata({
-    apparatusId,
-    pozitionId: PozitionIds.Second,
-    harpKeyId,
-    activeIds: getScale(ScaleIds.MajorPentatonic).degrees,
-  })
-
-  const { activeDegreeIds: thisPozitionDegrees } = getHarpStrata({
-    apparatusId,
-    pozitionId,
-    harpKeyId,
-    activeIds: pentatonicPitches2ndPoz,
-  })
-
   const initialHarpStrataProps: HarpStrataProps = {
     apparatusId,
     pozitionId,
@@ -56,6 +37,9 @@ export const getInitialGlobalState = (pageNumber: PageNumber): GlobalState => {
   const { Explore: initialExperienceMode } = ExperienceModes
   const { Degree: initialDisplayMode } = DisplayModes
   const { Pozition: initialLockedCovariant } = CovariantMembers
+
+  const thisPozitionDegrees =
+    pageNumber === 1 ? getScale(ScaleIds.MajorPentatonic).degrees : []
 
   const state = {
     activeHarpStrata: initialHarpStrata,
