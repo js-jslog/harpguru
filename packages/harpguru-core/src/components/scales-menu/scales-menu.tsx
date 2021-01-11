35ec7cf431d2af0b157c721fe3b7e25206da2d1d
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-11 21:45:57 +0000

    Replace arbitrary list with all scales

--- packages/harpguru-core/src/components/scales-menu/scales-menu.tsx
+++ packages/harpguru-core/src/components/scales-menu/scales-menu.tsx
@@ -1,6 +1,7 @@
 import { FlatList } from 'react-native-gesture-handler'
 import { Text, SafeAreaView, StyleSheet } from 'react-native'
 import React from 'react'
+import { getScale, getScaleIds } from 'harpparts'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import { MenuOpenButton } from '../menu-open-button'
@@ -14,30 +15,12 @@ export const ScalesMenu = (menuProps: MenuProps): React.ReactElement => {
 
   const styles = StyleSheet.create({
     title: {
+      alignSelf: 'center',
       fontSize: 64,
     },
   })
 
-  const data = [
-    { id: 'one', title: 'item one' },
-    { id: 'two', title: 'item two' },
-    { id: 'three', title: 'item three' },
-    { id: 'four', title: 'item four' },
-    { id: 'five', title: 'item five' },
-    { id: 'six', title: 'item six' },
-    { id: 'seven', title: 'item seven' },
-    { id: 'eight', title: 'item eight' },
-    { id: 'nine', title: 'item nine' },
-    { id: 'ten', title: 'item ten' },
-    { id: 'eleven', title: 'item eleven' },
-    { id: 'twelve', title: 'item twelve' },
-    { id: 'thirteen', title: 'item thirteen' },
-    { id: 'fourteen', title: 'item fourteen' },
-    { id: 'fifteen', title: 'item fifteen' },
-    { id: 'sixteen', title: 'item sixteen' },
-    { id: 'seventeen', title: 'item seventeen' },
-    { id: 'eighteen', title: 'item eighteen' },
-  ]
+  const scales = getScaleIds().map((id) => getScale(id))
 
   const List = () => {
     return (
@@ -45,11 +28,11 @@ export const ScalesMenu = (menuProps: MenuProps): React.ReactElement => {
         <FlatList
           scrollEnabled={true}
           initialNumToRender={18}
-          data={data}
+          data={scales}
           renderItem={({ item }) => (
-            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.title}>{item.label}</Text>
           )}
-          keyExtractor={(item) => item.id}
+          keyExtractor={(item) => `${item.id}`}
         />
       </SafeAreaView>
     )
