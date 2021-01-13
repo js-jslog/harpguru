da03dc7b5044808bf9a1df642c2eb470bb89695b
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-13 17:50:46 +0000

    Remove swipe related menu open / actions
    These gestures will not cooperate well with the swipe I'm proposing in
    the scales menu. It would be possible to disable the swipe while
    actually in the menu, but I don't think there's much use or need for it.

--- packages/harpguru-core/src/components/harp-guru-page/harp-guru-page.tsx
+++ packages/harpguru-core/src/components/harp-guru-page/harp-guru-page.tsx
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler'
 
 import type { Value } from 'react-native-reanimated'
-import { PanGestureHandler } from 'react-native-gesture-handler'
 import { View, StyleSheet } from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
@@ -19,7 +18,7 @@ import { DisplayModeButton } from '../display-mode-button'
 import { CovariantMenu } from '../covariant-menu'
 import { ActivityLegend } from '../activity-legend'
 import { MenuStates, MenuStashPosition, PageNumber } from '../../types'
-import { colors, getSizes } from '../../styles'
+import { colors } from '../../styles'
 
 import { useMenus } from './hooks'
 
@@ -32,16 +31,13 @@ export const HarpGuruPage = ({
   pageOnDisplay,
   thisPage,
 }: HarpGuruPageProps): ReactElement => {
-  const [menuState, handleMenuSwipe, handleManuTap] = useMenus()
+  const [menuState, handleManuTap] = useMenus()
 
   const nextPageNumberMap: Record<PageNumber, PageNumber> = {
     1: 2,
     2: 3,
     3: 1,
   } as const
-
-  const sizes = getSizes()
-  const { 8: swipeThreshold } = sizes
 
   const styles = StyleSheet.create({
     fillScreen: {
@@ -53,64 +49,55 @@ export const HarpGuruPage = ({
   })
 
   return (
-    <PanGestureHandler
-      activeOffsetX={[swipeThreshold * -1, swipeThreshold]}
-      onHandlerStateChange={handleMenuSwipe}
-    >
-      <View style={styles.fillScreen}>
-        <ActivityLegend />
-        <HarpFaceMemo />
-        <CovariantMenu
-          isMenuStashed={menuState !== MenuStates.CovariantMenu}
-          isLabelHidden={
-            menuState !== MenuStates.CovariantMenu &&
-            menuState !== MenuStates.NoMenu
-          }
-          stashPosition={MenuStashPosition.First}
-          openCloseMenu={() => handleManuTap(MenuStates.CovariantMenu)}
-        />
-        <LayoutMenu
-          isMenuStashed={menuState !== MenuStates.LayoutMenu}
-          isLabelHidden={
-            menuState !== MenuStates.LayoutMenu &&
-            menuState !== MenuStates.NoMenu
-          }
-          stashPosition={MenuStashPosition.Second}
-          openCloseMenu={() => handleManuTap(MenuStates.LayoutMenu)}
-        />
-        <ScalesMenu
-          isMenuStashed={menuState !== MenuStates.ScalesMenu}
-          isLabelHidden={
-            menuState !== MenuStates.ScalesMenu &&
-            menuState !== MenuStates.NoMenu
-          }
-          stashPosition={MenuStashPosition.Third}
-          openCloseMenu={() => handleManuTap(MenuStates.ScalesMenu)}
-        />
-        <DisplayModeButton
-          isLabelHidden={menuState !== MenuStates.NoMenu}
-          stashPosition={MenuStashPosition.Fourth}
-        />
-        <FragmentationButton
-          isLabelHidden={menuState !== MenuStates.NoMenu}
-          stashPosition={MenuStashPosition.Fifth}
-        />
-        <ExperienceModeButton
-          isLabelHidden={menuState !== MenuStates.NoMenu}
-          stashPosition={MenuStashPosition.Sixth}
-        />
-        <NextPageButton
-          thisPage={thisPage}
-          totalPages={3}
-          stashPosition={MenuStashPosition.Seventh}
-          getNextPage={() =>
-            pageOnDisplay.setValue(nextPageNumberMap[thisPage])
-          }
-        />
-        <ToggleBufferFlusher />
-        <QuizQuestionDisplay isScreenFree={menuState === MenuStates.NoMenu} />
-        <ScaleNotification />
-      </View>
-    </PanGestureHandler>
+    <View style={styles.fillScreen}>
+      <ActivityLegend />
+      <HarpFaceMemo />
+      <CovariantMenu
+        isMenuStashed={menuState !== MenuStates.CovariantMenu}
+        isLabelHidden={
+          menuState !== MenuStates.CovariantMenu &&
+          menuState !== MenuStates.NoMenu
+        }
+        stashPosition={MenuStashPosition.First}
+        openCloseMenu={() => handleManuTap(MenuStates.CovariantMenu)}
+      />
+      <LayoutMenu
+        isMenuStashed={menuState !== MenuStates.LayoutMenu}
+        isLabelHidden={
+          menuState !== MenuStates.LayoutMenu && menuState !== MenuStates.NoMenu
+        }
+        stashPosition={MenuStashPosition.Second}
+        openCloseMenu={() => handleManuTap(MenuStates.LayoutMenu)}
+      />
+      <ScalesMenu
+        isMenuStashed={menuState !== MenuStates.ScalesMenu}
+        isLabelHidden={
+          menuState !== MenuStates.ScalesMenu && menuState !== MenuStates.NoMenu
+        }
+        stashPosition={MenuStashPosition.Third}
+        openCloseMenu={() => handleManuTap(MenuStates.ScalesMenu)}
+      />
+      <DisplayModeButton
+        isLabelHidden={menuState !== MenuStates.NoMenu}
+        stashPosition={MenuStashPosition.Fourth}
+      />
+      <FragmentationButton
+        isLabelHidden={menuState !== MenuStates.NoMenu}
+        stashPosition={MenuStashPosition.Fifth}
+      />
+      <ExperienceModeButton
+        isLabelHidden={menuState !== MenuStates.NoMenu}
+        stashPosition={MenuStashPosition.Sixth}
+      />
+      <NextPageButton
+        thisPage={thisPage}
+        totalPages={3}
+        stashPosition={MenuStashPosition.Seventh}
+        getNextPage={() => pageOnDisplay.setValue(nextPageNumberMap[thisPage])}
+      />
+      <ToggleBufferFlusher />
+      <QuizQuestionDisplay isScreenFree={menuState === MenuStates.NoMenu} />
+      <ScaleNotification />
+    </View>
   )
 }
--- packages/harpguru-core/src/components/harp-guru-page/hooks/use-menus/use-menus.ts
+++ packages/harpguru-core/src/components/harp-guru-page/hooks/use-menus/use-menus.ts
@@ -1,41 +1,11 @@
-import { useEffect } from 'reactn'
-import { State } from 'react-native-gesture-handler'
-import type { PanGestureHandlerGestureEvent } from 'react-native-gesture-handler'
 import { useState } from 'react'
 
 import { MenuStates } from '../../../../types'
 
-type HandleSwipe = (arg0: PanGestureHandlerGestureEvent) => void
 type OpenCloseMenu = (arg0: MenuStates) => void
 
-export const useMenus = (): [MenuStates, HandleSwipe, OpenCloseMenu] => {
-  const [panState, setPanState] = useState<State>(State.UNDETERMINED)
+export const useMenus = (): [MenuStates, OpenCloseMenu] => {
   const [menuState, setMenuState] = useState<MenuStates>(MenuStates.NoMenu)
-  const [translationX, setTranslationX] = useState<number>(0)
-  const [translationY, setTranslationY] = useState<number>(0)
-
-  const isSwipeLeft = (x: number) => x < 0
-  const isSwipeRight = (x: number) => x > 0
-  const isSwipeUp = (y: number) => y < 0
-  const isSwipeDown = (y: number) => y > 0
-
-  useEffect(() => {
-    if (isSwipeLeft(translationX) && isSwipeDown(translationY)) {
-      setMenuState(MenuStates.CovariantMenu)
-    }
-    if (isSwipeLeft(translationX) && isSwipeUp(translationY)) {
-      setMenuState(MenuStates.LayoutMenu)
-    }
-    if (
-      (menuState === MenuStates.CovariantMenu ||
-        menuState === MenuStates.LayoutMenu) &&
-      isSwipeRight(translationX)
-    ) {
-      setMenuState(MenuStates.NoMenu)
-    }
-    setPanState(State.UNDETERMINED)
-    setTranslationX(0)
-  }, [panState])
 
   const openCloseMenu = (menu: MenuStates) => {
     if (menuState === menu) {
@@ -45,11 +15,5 @@ export const useMenus = (): [MenuStates, HandleSwipe, OpenCloseMenu] => {
     }
   }
 
-  const handleSwipe = ({ nativeEvent }: PanGestureHandlerGestureEvent) => {
-    if (nativeEvent.state !== State.END) return
-    setTranslationX(nativeEvent.translationX)
-    setTranslationY(nativeEvent.translationY)
-    setPanState(nativeEvent.state)
-  }
-  return [menuState, handleSwipe, openCloseMenu]
+  return [menuState, openCloseMenu]
 }
