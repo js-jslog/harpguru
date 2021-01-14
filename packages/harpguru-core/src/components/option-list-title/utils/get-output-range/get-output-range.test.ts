cf8d3e3f42e651ae9b96e3d8a5faaf076c4c38ab
js-jslog <jhs4jbs@hotmail.co.uk>
2021-01-14 22:29:16 +0000

    Add test cases to the util

--- packages/harpguru-core/src/components/option-list-title/utils/get-output-range/get-output-range.test.ts
+++ packages/harpguru-core/src/components/option-list-title/utils/get-output-range/get-output-range.test.ts
@@ -1,6 +1,23 @@
 import { getOutputRange } from './get-output-range'
 
+test('that output ranges for a range of 1 can be produced', () => {
+  expect(getOutputRange(0, 1)).toStrictEqual([1])
+})
+
 test('that output ranges for a range of 2 can be produced', () => {
   expect(getOutputRange(0, 2)).toStrictEqual([1, 0])
   expect(getOutputRange(1, 2)).toStrictEqual([0, 1])
+})
+
+test('that output ranges for a range of 3 can be produced', () => {
+  expect(getOutputRange(0, 3)).toStrictEqual([1, 0, 0])
+  expect(getOutputRange(1, 3)).toStrictEqual([0, 1, 0])
+  expect(getOutputRange(2, 3)).toStrictEqual([0, 0, 1])
+})
+
+test('that output ranges for a range of 4 can be produced', () => {
+  expect(getOutputRange(0, 4)).toStrictEqual([1, 0, 0, 0])
+  expect(getOutputRange(1, 4)).toStrictEqual([0, 1, 0, 0])
+  expect(getOutputRange(2, 4)).toStrictEqual([0, 0, 1, 0])
+  expect(getOutputRange(3, 4)).toStrictEqual([0, 0, 0, 1])
 })
