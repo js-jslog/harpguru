js-jslog/harpguru

commit d047e0c7725f72ae04e559045af9466a4297ce95

Remove unnecessary shared functions
With the migration of some of the option functions up to the shared
resource level it seems like a good idea to thin out some of the other
stuff that isn't needed. In the process of removing some of these I've
been forced to also update and improve some functionality elsewhere.
This commit might seem a bit offroad, but I think it's valuable in the
context of this feature branch.

packages/harpguru-core/src/components/experience-mode-button/experience-mode-button.tsx
@@ -1,4 +1,4 @@
-import { useGlobal } from 'reactn'
+import { useGlobal, useDispatch } from 'reactn'
 import { View } from 'react-native'
 import React from 'react'
 import { MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons'
@@ -9,7 +9,7 @@ import { ExperienceModes, MenuStashPosition } from '../../types'
 import type { MenuProps } from '../../types'
 import { getSizes, harpguruColors } from '../../styles'
 
-import { useNudgeExperienceMode } from './hooks'
+import { getNewExperienceModeForDispatcher } from './utils'
 
 type ExperienceModeButtonProps = {
   readonly isLabelHidden: boolean
@@ -28,12 +28,12 @@ export const ExperienceModeButton = ({
   // eslint-disable-next-line no-empty-pattern
   const [] = useGlobal('activeHarpStrata')
   const [activeExperienceMode] = useGlobal('activeExperienceMode')
-  const nudgeExperienceMode = useNudgeExperienceMode()
+  const nudgeExperienceMode = useDispatch(getNewExperienceModeForDispatcher)
   const menuLikeProps: MenuProps = {
     isMenuStashed: true,
     isLabelHidden: isLabelHidden,
     stashPosition,
-    openCloseMenu: () => nudgeExperienceMode('DOWN'),
+    openCloseMenu: () => nudgeExperienceMode(),
   }
 
   const sizes = getSizes()

packages/harpguru-core/src/components/display-mode-button/display-mode-button.tsx
@@ -1,4 +1,4 @@
-import { useGlobal } from 'reactn'
+import { useGlobal, useDispatch } from 'reactn'
 import { View } from 'react-native'
 import React from 'react'
 import { MaterialIcons } from '@expo/vector-icons'
@@ -9,7 +9,7 @@ import { MenuStashPosition } from '../../types'
 import type { MenuProps } from '../../types'
 import { getSizes, harpguruColors } from '../../styles'
 
-import { useNudgeDisplayMode } from './hooks'
+import { getNewDisplayModeForDispatcher } from './utils'
 
 type DisplayModeButtonProps = {
   readonly isLabelHidden: boolean
@@ -27,12 +27,15 @@ export const DisplayModeButton = ({
   // This is really a hacky workaround, but it's cheap and effective.
   // eslint-disable-next-line no-empty-pattern
   const [] = useGlobal('activeHarpStrata')
-  const nudgeDisplayMode = useNudgeDisplayMode()
+  const nudgeDisplayMode = useDispatch(
+    getNewDisplayModeForDispatcher,
+    'activeDisplayMode'
+  )
   const menuLikeProps: MenuProps = {
     isMenuStashed: true,
     isLabelHidden: isLabelHidden,
     stashPosition,
-    openCloseMenu: () => nudgeDisplayMode('DOWN'),
+    openCloseMenu: () => nudgeDisplayMode(),
   }
 
   const sizes = getSizes()

packages/harpguru-core/src/components/display-mode-button/hooks/index.ts
@@ -1,1 +1,0 @@
-export { useNudgeDisplayMode } from './use-nudge-display-mode'

packages/harpguru-core/src/components/display-mode-button/hooks/use-nudge-display-mode/index.ts
@@ -1,1 +1,0 @@
-export { useNudgeDisplayMode } from './use-nudge-display-mode'

packages/harpguru-core/src/components/display-mode-button/hooks/use-nudge-display-mode/use-nudge-display-mode.test.ts
@@ -1,30 +1,0 @@
-import { useGlobal } from 'reactn'
-
-import { DisplayModes } from '../../../../types'
-
-import { useNudgeDisplayMode } from './use-nudge-display-mode'
-
-jest.mock('reactn')
-const mockUseGlobal = useGlobal as jest.Mock
-
-test('sets the opposite display mode when input DisplayMode is Pitch', () => {
-  const setActiveDisplayMode = jest.fn()
-  mockUseGlobal.mockReturnValue([DisplayModes.Pitch, setActiveDisplayMode])
-  const nudgeDisplayMode = useNudgeDisplayMode()
-  nudgeDisplayMode('DOWN')
-
-  expect(setActiveDisplayMode.mock.calls[0][0]).toStrictEqual(
-    DisplayModes.Degree
-  )
-})
-
-test('sets the opposite display mode when input DisplayMode is Degree', () => {
-  const setActiveDisplayMode = jest.fn()
-  mockUseGlobal.mockReturnValue([DisplayModes.Degree, setActiveDisplayMode])
-  const nudgeDisplayMode = useNudgeDisplayMode()
-  nudgeDisplayMode('DOWN')
-
-  expect(setActiveDisplayMode.mock.calls[0][0]).toStrictEqual(
-    DisplayModes.Pitch
-  )
-})

packages/harpguru-core/src/components/display-mode-button/hooks/use-nudge-display-mode/use-nudge-display-mode.ts
@@ -1,30 +1,0 @@
-import { useGlobal } from 'reactn'
-
-import { partiallyApplyNudgeFunction } from '../../../../utils'
-import { DisplayModes } from '../../../../types'
-
-export const useNudgeDisplayMode = (): ((arg0: 'UP' | 'DOWN') => void) => {
-  const [activeDisplayMode, setActiveDisplayMode] = useGlobal(
-    'activeDisplayMode'
-  )
-
-  return partiallyApplyNudgeFunction(nudgeDisplayMode, {
-    activeDisplayMode,
-    setActiveDisplayMode,
-  })
-}
-
-type PartialParams = {
-  readonly activeDisplayMode: DisplayModes
-  readonly setActiveDisplayMode: (arg0: DisplayModes) => void
-}
-
-const nudgeDisplayMode = (partialParams: PartialParams): void => {
-  const { activeDisplayMode, setActiveDisplayMode } = partialParams
-
-  if (activeDisplayMode === DisplayModes.Degree) {
-    setActiveDisplayMode(DisplayModes.Pitch)
-  } else {
-    setActiveDisplayMode(DisplayModes.Degree)
-  }
-}

packages/harpguru-core/src/components/display-mode-button/utils/get-new-display-mode-for-dispatcher/get-new-display-mode-for-dispatcher.test.ts
@@ -1,0 +1,19 @@
+import { DisplayModes } from '../../../../types'
+
+import { getNewDisplayModeForDispatcher } from './get-new-display-mode-for-dispatcher'
+
+test('returns Degree when input DisplayMode is Pitch', () => {
+  const newActiveDisplayMode = getNewDisplayModeForDispatcher(
+    DisplayModes.Pitch
+  )
+
+  expect(newActiveDisplayMode).toBe(DisplayModes.Degree)
+})
+
+test('returns Pitch when input DisplayMode is Degree', () => {
+  const newActiveDisplayMode = getNewDisplayModeForDispatcher(
+    DisplayModes.Degree
+  )
+
+  expect(newActiveDisplayMode).toStrictEqual(DisplayModes.Pitch)
+})

packages/harpguru-core/src/components/display-mode-button/utils/get-new-display-mode-for-dispatcher/get-new-display-mode-for-dispatcher.ts
@@ -1,0 +1,11 @@
+import { DisplayModes } from '../../../../types'
+
+export const getNewDisplayModeForDispatcher = (
+  activeDisplayMode: DisplayModes
+): DisplayModes => {
+  const { Degree, Pitch } = DisplayModes
+
+  const newDisplayMode = activeDisplayMode === Pitch ? Degree : Pitch
+
+  return newDisplayMode
+}

commit e83d483794107040917bcd5f98f4b68ef51fc2e7

Fix overlooked mistake in dispatcher config
This bug was introduced in d047e0c7725f72ae04e559045af9466a4297ce95

Evidence, if it were needed, that my test and type coverage is not
perfect.

I'm surprised about the lack of type safety here. See the `TODO` added
which poses what should be an achievable challenge to me and the type
engine.

packages/harpguru-core/src/components/experience-mode-button/experience-mode-button.tsx
@@ -28,7 +28,14 @@ export const ExperienceModeButton = ({
   // eslint-disable-next-line no-empty-pattern
   const [] = useGlobal('activeHarpStrata')
   const [activeExperienceMode] = useGlobal('activeExperienceMode')
-  const nudgeExperienceMode = useDispatch(getNewExperienceModeForDispatcher)
+  // TODO: Figure out why this wasn't typesafe and try to resolve in case
+  // I make the same mistake in other dispatchers. The error when the
+  // second param was removed was the following:
+  // `Error: ReactN global state must be a function, null, objecct, or Promise`
+  const nudgeExperienceMode = useDispatch(
+    getNewExperienceModeForDispatcher,
+    'activeExperienceMode'
+  )
   const menuLikeProps: MenuProps = {
     isMenuStashed: true,
     isLabelHidden: isLabelHidden,