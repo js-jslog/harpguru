6a41816401139447be9f15676fbdadb1f647d612
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-07 19:37:44 +0000

    Add usecase to populate range not starting at 0

--- packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.ts
+++ packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.ts
@@ -2,8 +2,7 @@ export const ascendingExclusiveRange = (
   start: number,
   end: number
 ): number[] => {
-  return Array.from({ length: end - start }, (_, index) => index + 1).slice(
-    0,
-    -1
-  )
+  return Array.from({ length: end - start }, (_, index) => index + 1)
+    .slice(0, -1)
+    .map((val) => val + start)
 }
