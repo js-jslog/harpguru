e69dcfece5ad84c39eb8a06e9471cfe4bba553db
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-07 19:40:30 +0000

    Add usecases to produce no range results

--- packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.test.ts
+++ packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.test.ts
@@ -7,3 +7,8 @@ test('ascendingExclusiveRange can return a simple 1 integar range', () => {
 test('ascendingExclusiveRange can return a simple 1 integar range not starting at 0', () => {
   expect(ascendingExclusiveRange(1, 3)).toStrictEqual([2])
 })
+
+test('ascendingExclusiveRange can return an empty array when there is no range', () => {
+  expect(ascendingExclusiveRange(1, 2)).toStrictEqual([])
+  expect(ascendingExclusiveRange(1, 1)).toStrictEqual([])
+})
