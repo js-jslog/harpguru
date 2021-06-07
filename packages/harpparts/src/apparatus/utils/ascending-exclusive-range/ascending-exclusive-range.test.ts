713799c9b5dba0c947ae64dd5392291782538e5f
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-07 19:41:25 +0000

    Group tests for readability

--- packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.test.ts
+++ packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.test.ts
@@ -2,9 +2,6 @@ import { ascendingExclusiveRange } from './ascending-exclusive-range'
 
 test('ascendingExclusiveRange can return a simple 1 integar range', () => {
   expect(ascendingExclusiveRange(0, 2)).toStrictEqual([1])
-})
-
-test('ascendingExclusiveRange can return a simple 1 integar range not starting at 0', () => {
   expect(ascendingExclusiveRange(1, 3)).toStrictEqual([2])
 })
 
