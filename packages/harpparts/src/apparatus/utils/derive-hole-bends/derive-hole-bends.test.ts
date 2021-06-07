4864a4bf6d52409101a74614a361a20202ef1833
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-07 21:43:43 +0000

    Handle bendless holes

--- packages/harpparts/src/apparatus/utils/derive-hole-bends/derive-hole-bends.test.ts
+++ packages/harpparts/src/apparatus/utils/derive-hole-bends/derive-hole-bends.test.ts
@@ -2,6 +2,17 @@ import type { Hole } from '../../types'
 
 import { deriveHoleBends } from './derive-hole-bends'
 
+test('deriveHoleBends adds no bends', () => {
+  const holeInput: Hole = {
+    blow: 9,
+    draw: 10,
+  }
+
+  const actualHoleOutput = deriveHoleBends(holeInput)
+
+  expect(actualHoleOutput).toStrictEqual(holeInput)
+})
+
 test('deriveHoleBends adds a simple single bend', () => {
   const holeInput: Hole = {
     blow: 0,
@@ -16,4 +27,74 @@ test('deriveHoleBends adds a simple single bend', () => {
   const actualHoleOutput = deriveHoleBends(holeInput)
 
   expect(actualHoleOutput).toStrictEqual(expectedHoleOutput)
+})
+
+test('deriveHoleBends adds a triple bend', () => {
+  const holeInput: Hole = {
+    blow: 25,
+    draw: 29,
+  }
+  const expectedHoleOutput: Hole = {
+    blow: 25,
+    draw: 29,
+    bends: [26, 27, 28],
+  }
+
+  const actualHoleOutput = deriveHoleBends(holeInput)
+
+  expect(actualHoleOutput).toStrictEqual(expectedHoleOutput)
+})
+
+test('deriveHoleBends adds no blow bends', () => {
+  const holeInput: Hole = {
+    blow: 20,
+    draw: 19,
+  }
+
+  const actualHoleOutput = deriveHoleBends(holeInput)
+
+  expect(actualHoleOutput).toStrictEqual(holeInput)
+})
+
+test('deriveHoleBends adds a simple single blow bend', () => {
+  const holeInput: Hole = {
+    blow: 9,
+    draw: 11,
+  }
+  const expectedHoleOutput: Hole = {
+    blow: 9,
+    draw: 11,
+    bends: [10],
+  }
+
+  const actualHoleOutput = deriveHoleBends(holeInput)
+
+  expect(actualHoleOutput).toStrictEqual(expectedHoleOutput)
+})
+
+test('deriveHoleBends adds a triple blow bend', () => {
+  const holeInput: Hole = {
+    blow: 12,
+    draw: 8,
+  }
+  const expectedHoleOutput: Hole = {
+    blow: 12,
+    draw: 8,
+    blowBends: [9, 10, 11],
+  }
+
+  const actualHoleOutput = deriveHoleBends(holeInput)
+
+  expect(actualHoleOutput).toStrictEqual(expectedHoleOutput)
+})
+
+test('deriveHoleBends adds no bends on 2 identical reeds', () => {
+  const holeInput: Hole = {
+    blow: 18,
+    draw: 18,
+  }
+
+  const actualHoleOutput = deriveHoleBends(holeInput)
+
+  expect(actualHoleOutput).toStrictEqual(holeInput)
 })
--- packages/harpparts/src/apparatus/utils/derive-hole-bends/derive-hole-bends.ts
+++ packages/harpparts/src/apparatus/utils/derive-hole-bends/derive-hole-bends.ts
@@ -3,6 +3,7 @@ import type { Hole } from '../../types'
 
 export const deriveHoleBends = (holeInput: Hole): Hole => {
   const bendRange = ascendingExclusiveRange(holeInput.blow, holeInput.draw)
+  if (bendRange.length === 0) return holeInput
   if (holeInput.blow < holeInput.draw) return { ...holeInput, bends: bendRange }
   return { ...holeInput, blowBends: bendRange }
 }
