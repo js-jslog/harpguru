f1e5def5f9f45f28a075510622c8137c132c87f6
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-07 19:43:34 +0000

    Add tests for producing multple integar ranges

--- packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.test.ts
+++ packages/harpparts/src/apparatus/utils/ascending-exclusive-range/ascending-exclusive-range.test.ts
@@ -9,3 +9,21 @@ test('ascendingExclusiveRange can return an empty array when there is no range',
   expect(ascendingExclusiveRange(1, 2)).toStrictEqual([])
   expect(ascendingExclusiveRange(1, 1)).toStrictEqual([])
 })
+
+test('ascendingExclusiveRange can return a multi integar range', () => {
+  expect(ascendingExclusiveRange(1, 10)).toStrictEqual([2, 3, 4, 5, 6, 7, 8, 9])
+  expect(ascendingExclusiveRange(8, 21)).toStrictEqual([
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+  ])
+})
