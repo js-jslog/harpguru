61eb2074995389401b70d498ad1a0ef06137f811
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-09 21:35:10 +0000

    Partially test matrix mapping with undefined tiers
    I have excluded the tests which don't quite work yet as a base to
    enhance from. See the commented out `expect` statements.

--- packages/harpparts/src/apparatus/utils/matrix-row-map-callback/matrix-row-map-callback.test.ts
+++ packages/harpparts/src/apparatus/utils/matrix-row-map-callback/matrix-row-map-callback.test.ts
@@ -103,3 +103,51 @@ test('matrixRowMapCallback can map a holes blow, draw and bend tiers with multi 
   expect(matrixRowMapCallback(matrixSpecs, 4, hole)).toBe(hole.overdraws[0])
   expect(matrixRowMapCallback(matrixSpecs, 5, hole)).toBe(hole.overdraws[1])
 })
+
+test('matrixRowMapCallback can map a holes blow, draw, and undefined bend tiers with multi level bends present (lower harp)', () => {
+  const hole: Hole = {
+    blow: 0,
+    draw: 3,
+    bends: [1, 2],
+    blowbends: [],
+    overblows: [4, 5],
+    overdraws: [],
+  }
+  const matrixSpecs: MatrixSpecs = {
+    height: 8,
+    blowRow: 3,
+  }
+
+  expect(matrixRowMapCallback(matrixSpecs, 0, hole)).toBeFalsy()
+  expect(matrixRowMapCallback(matrixSpecs, 1, hole)).toBe(hole.overblows[1])
+  expect(matrixRowMapCallback(matrixSpecs, 2, hole)).toBe(hole.overblows[0])
+  expect(matrixRowMapCallback(matrixSpecs, 3, hole)).toBe(hole.blow)
+  expect(matrixRowMapCallback(matrixSpecs, 4, hole)).toBe(hole.draw)
+  //  expect(matrixRowMapCallback(matrixSpecs, 5, hole)).toBe(hole.bends[1])
+  //  expect(matrixRowMapCallback(matrixSpecs, 6, hole)).toBe(hole.bends[0])
+  //  expect(matrixRowMapCallback(matrixSpecs, 7, hole)).toBeFalsy()
+})
+
+test('matrixRowMapCallback can map a holes blow, draw and bend tiers with multi level bends present (upper harp)', () => {
+  const hole: Hole = {
+    blow: 3,
+    draw: 0,
+    bends: [],
+    blowbends: [1, 2],
+    overblows: [],
+    overdraws: [4, 5],
+  }
+  const matrixSpecs: MatrixSpecs = {
+    height: 8,
+    blowRow: 3,
+  }
+
+  //  expect(matrixRowMapCallback(matrixSpecs, 0, hole)).toBeFalsy()
+  //  expect(matrixRowMapCallback(matrixSpecs, 1, hole)).toBe(hole.blowbends[0])
+  //  expect(matrixRowMapCallback(matrixSpecs, 2, hole)).toBe(hole.blowbends[1])
+  expect(matrixRowMapCallback(matrixSpecs, 3, hole)).toBe(hole.blow)
+  expect(matrixRowMapCallback(matrixSpecs, 4, hole)).toBe(hole.draw)
+  expect(matrixRowMapCallback(matrixSpecs, 5, hole)).toBe(hole.overdraws[0])
+  expect(matrixRowMapCallback(matrixSpecs, 6, hole)).toBe(hole.overdraws[1])
+  expect(matrixRowMapCallback(matrixSpecs, 7, hole)).toBeFalsy()
+})
