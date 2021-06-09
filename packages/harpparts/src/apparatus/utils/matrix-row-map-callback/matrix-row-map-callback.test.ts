333a64b2d6c96660efa271544b375396ffaf0f9e
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-09 20:30:28 +0000

    Add functionality to matrix row map callback

--- packages/harpparts/src/apparatus/utils/matrix-row-map-callback/matrix-row-map-callback.test.ts
+++ packages/harpparts/src/apparatus/utils/matrix-row-map-callback/matrix-row-map-callback.test.ts
@@ -19,3 +19,23 @@ test('matrixRowMapCallback can map a holes blow and draw tiers with no bends', (
   expect(matrixRowMapCallback(matrixSpecs, 0, hole)).toBe(hole.blow)
   expect(matrixRowMapCallback(matrixSpecs, 1, hole)).toBe(hole.draw)
 })
+
+test('matrixRowMapCallback can map a holes blow, draw and bend tiers with single level bends present', () => {
+  const hole: Hole = {
+    blow: 0,
+    draw: 2,
+    bends: [1],
+    blowbends: [],
+    overblows: [3],
+    overdraws: [],
+  }
+  const matrixSpecs: MatrixSpecs = {
+    height: 4,
+    blowRow: 1,
+  }
+
+  expect(matrixRowMapCallback(matrixSpecs, 0, hole)).toBe(hole.overblows[0])
+  expect(matrixRowMapCallback(matrixSpecs, 1, hole)).toBe(hole.blow)
+  expect(matrixRowMapCallback(matrixSpecs, 2, hole)).toBe(hole.draw)
+  expect(matrixRowMapCallback(matrixSpecs, 3, hole)).toBe(hole.bends[0])
+})
--- packages/harpparts/src/apparatus/utils/matrix-row-map-callback/matrix-row-map-callback.ts
+++ packages/harpparts/src/apparatus/utils/matrix-row-map-callback/matrix-row-map-callback.ts
@@ -2,9 +2,12 @@ import type { Hole, MatrixSpecs } from '../../types'
 
 export const matrixRowMapCallback = (
   { blowRow }: MatrixSpecs,
-  currentY: number,
+  currentRow: number,
   hole: Hole
 ): number => {
-  if (currentY === blowRow) return hole.blow
-  return hole.draw
+  const drawRow = blowRow + 1
+  if (currentRow === blowRow) return hole.blow
+  if (currentRow === drawRow) return hole.draw
+  if (currentRow < blowRow) return hole.blowbends[0] || hole.overblows[0]
+  return hole.bends[0] || hole.overdraws[0]
 }
