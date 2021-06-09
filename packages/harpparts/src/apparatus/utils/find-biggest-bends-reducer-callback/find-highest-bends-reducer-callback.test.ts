cf3dec5e103dc2e2fccc102e725bc91a76d3c9dc
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-09 15:59:17 +0000

    Add test cases to the biggest bends reducer callback
    The existing functionality already covers them.

--- packages/harpparts/src/apparatus/utils/find-biggest-bends-reducer-callback/find-highest-bends-reducer-callback.test.ts
+++ packages/harpparts/src/apparatus/utils/find-biggest-bends-reducer-callback/find-highest-bends-reducer-callback.test.ts
@@ -20,3 +20,118 @@ test('findBiggestBendsReducerCallback returns accumulator when no bends', () => 
 
   expect(output).toStrictEqual(accumulator)
 })
+
+test('findBiggestBendsReducerCallback returns accumulator when all bends are smaller than accumulator', () => {
+  const accumulator = {
+    biggestBlow: 5,
+    biggestDraw: 5,
+  }
+  const currentValue: Hole = {
+    blow: 9,
+    draw: 10,
+    bends: [],
+    blowbends: [],
+    overblows: [],
+    overdraws: [],
+  }
+
+  const output = findBiggestBendsReducerCallback(accumulator, currentValue)
+
+  expect(output).toStrictEqual(accumulator)
+})
+
+test('findBiggestBendsReducerCallback adds new biggestBlow when blowbends are bigger', () => {
+  const accumulator = {
+    biggestBlow: 1,
+    biggestDraw: 4,
+  }
+  const currentValue: Hole = {
+    blow: 12,
+    draw: 9,
+    bends: [],
+    blowbends: [10, 11],
+    overblows: [],
+    overdraws: [],
+  }
+
+  const expectedOutput = {
+    biggestBlow: 2,
+    biggestDraw: 4,
+  }
+
+  const output = findBiggestBendsReducerCallback(accumulator, currentValue)
+
+  expect(output).toStrictEqual(expectedOutput)
+})
+
+test('findBiggestBendsReducerCallback adds new biggestBlow when overblows are bigger', () => {
+  const accumulator = {
+    biggestBlow: 0,
+    biggestDraw: 0,
+  }
+  const currentValue: Hole = {
+    blow: 9,
+    draw: 10,
+    bends: [],
+    blowbends: [],
+    overblows: [11],
+    overdraws: [],
+  }
+
+  const expectedOutput = {
+    biggestBlow: 1,
+    biggestDraw: 0,
+  }
+
+  const output = findBiggestBendsReducerCallback(accumulator, currentValue)
+
+  expect(output).toStrictEqual(expectedOutput)
+})
+
+test('findBiggestBendsReducerCallback adds new biggestDraw when bends are bigger', () => {
+  const accumulator = {
+    biggestBlow: 4,
+    biggestDraw: 1,
+  }
+  const currentValue: Hole = {
+    blow: 8,
+    draw: 12,
+    bends: [9, 10, 11],
+    blowbends: [],
+    overblows: [],
+    overdraws: [],
+  }
+
+  const expectedOutput = {
+    biggestBlow: 4,
+    biggestDraw: 3,
+  }
+
+  const output = findBiggestBendsReducerCallback(accumulator, currentValue)
+
+  expect(output).toStrictEqual(expectedOutput)
+})
+
+test('findBiggestBendsReducerCallback adds new biggestDraw when overdraws are bigger', () => {
+  const accumulator = {
+    biggestBlow: 0,
+    biggestDraw: 0,
+  }
+  const currentValue: Hole = {
+    blow: 10,
+    draw: 9,
+    bends: [],
+    blowbends: [],
+    overblows: [],
+    overdraws: [11],
+  }
+
+  const expectedOutput = {
+    biggestBlow: 0,
+    biggestDraw: 1,
+  }
+
+  const output = findBiggestBendsReducerCallback(accumulator, currentValue)
+
+  expect(output).toStrictEqual(expectedOutput)
+})
