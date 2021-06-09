56ddff9c295bc7aa758700b0c2199f6b318e3d36
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-09 16:35:03 +0000

    Improve variables for readability

--- packages/harpparts/src/apparatus/utils/biggest-bends-reducer-callback/biggest-bends-reducer-callback.ts
+++ packages/harpparts/src/apparatus/utils/biggest-bends-reducer-callback/biggest-bends-reducer-callback.ts
@@ -9,21 +9,20 @@ export const biggestBendsReducerCallback = (
   accumulator: Accumulator,
   currentValue: Hole
 ): Accumulator => {
-  const { bends, blowbends, overblows, overdraws } = currentValue
-  const biggestBlow =
-    blowbends.length > overblows.length ? blowbends.length : overblows.length
-  const biggestDraw =
-    bends.length > overdraws.length ? bends.length : overdraws.length
+  const {
+    bends: { length: currBends },
+    blowbends: { length: currBlowbends },
+    overblows: { length: currOverblows },
+    overdraws: { length: currOverdraws },
+  } = currentValue
+  const { biggestBlow: accBigBlow, biggestDraw: accBigDraw } = accumulator
+  const currBigBlow =
+    currBlowbends > currOverblows ? currBlowbends : currOverblows
+  const currBigDraw = currBends > currOverdraws ? currBends : currOverdraws
 
   return {
     ...accumulator,
-    biggestBlow:
-      accumulator.biggestBlow > biggestBlow
-        ? accumulator.biggestBlow
-        : biggestBlow,
-    biggestDraw:
-      accumulator.biggestDraw > biggestDraw
-        ? accumulator.biggestDraw
-        : biggestDraw,
+    biggestBlow: accBigBlow > currBigBlow ? accBigBlow : currBigBlow,
+    biggestDraw: accBigDraw > currBigDraw ? accBigDraw : currBigDraw,
   }
 }
