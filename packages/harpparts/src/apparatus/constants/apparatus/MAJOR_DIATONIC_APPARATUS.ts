1c17b82ad51a13b88aa6c0c56f64efaefdfb7746
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-10 16:26:47 +0000

    Add more plans for validation stage

--- packages/harpparts/src/apparatus/constants/apparatus/MAJOR_DIATONIC_APPARATUS.ts
+++ packages/harpparts/src/apparatus/constants/apparatus/MAJOR_DIATONIC_APPARATUS.ts
@@ -69,6 +69,7 @@ import type { HarpFaceMatrix, HalfstepIndex } from '../../../types'
 // Things taht need to be validated in the Hole objects
 // only bends & overblows or blowbends & overdraws
 // all bend arrays are sequenced and ascending with no gaps
+// There are no more than 5 bends / blowbends and no more than 2 overblows / overdraws
 
 // prettier-ignore
 const halfstepIndexMatrix: HarpFaceMatrix<HalfstepIndex> = [
