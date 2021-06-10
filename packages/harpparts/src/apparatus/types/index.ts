6bb7bbb74005e8445c24153dc347928bfcfc100d
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-10 16:31:15 +0000

    Add maaping function for interaction matrix
    This is mostly largely based on the halfstepIndexRowMapCallback function
    and it's tests but I've pushed the tests a bit further in to the
    ridiculous extremes of bending, just to prove that the mapping for each
    of the available interaction ids works.
    
    There's no accomodation of the ValvedBend1 interaction id yet since that
    functionality is still forthcoming.

--- packages/harpparts/src/apparatus/types/index.ts
+++ packages/harpparts/src/apparatus/types/index.ts
@@ -75,10 +75,17 @@ export enum InteractionIds {
   Bend1 = 'BEND1',
   Bend2 = 'BEND2',
   Bend3 = 'BEND3',
+  Bend4 = 'BEND4',
+  Bend5 = 'BEND5',
   BlowBend1 = 'BLOWBEND1',
   BlowBend2 = 'BLOWBEND2',
+  BlowBend3 = 'BLOWBEND3',
+  BlowBend4 = 'BLOWBEND4',
+  BlowBend5 = 'BLOWBEND5',
   OverDraw1 = 'OVERDRAW1',
+  OverDraw2 = 'OVERDRAW2',
   OverBlow1 = 'OVERBLOW1',
+  OverBlow2 = 'OVERBLOW2',
   ValveBend = 'VALVEBEND',
 }
 
---
+++ packages/harpparts/src/apparatus/utils/interactionid-row-map-callback/interactionid-row-map-callback.ts
@@ -0,0 +1,66 @@
+import type { Hole, MatrixSpecs } from '../../types'
+import { InteractionIds } from '../../types'
+
+export const interactionIdRowMapCallback = (
+  { blowRow }: MatrixSpecs,
+  currentRow: number,
+  hole: Hole
+): InteractionIds | undefined => {
+  const {
+    Blow,
+    Draw,
+    Bend1,
+    Bend2,
+    Bend3,
+    Bend4,
+    Bend5,
+    BlowBend1,
+    BlowBend2,
+    BlowBend3,
+    BlowBend4,
+    BlowBend5,
+    OverDraw1,
+    OverDraw2,
+    OverBlow1,
+    OverBlow2,
+  } = InteractionIds
+  const drawRow = blowRow + 1
+  const { bends, blowbends, overblows, overdraws } = hole
+
+  const bendInteractions = [Bend1, Bend2, Bend3, Bend4, Bend5]
+  const overblowInteractions = [OverBlow1, OverBlow2]
+  const blowbendInteractions = [
+    BlowBend1,
+    BlowBend2,
+    BlowBend3,
+    BlowBend4,
+    BlowBend5,
+  ]
+  const overdrawInteractions = [OverDraw1, OverDraw2]
+
+  if (currentRow === blowRow) return Blow
+  if (currentRow === drawRow) return Draw
+  if (currentRow < blowRow) {
+    const distance = blowRow - currentRow - 1
+    if ([...blowbends].reverse()[distance])
+      return blowbendInteractions[distance]
+    if (overblows[blowRow - currentRow - 1])
+      return overblowInteractions[distance]
+    return undefined
+  }
+  if (currentRow > drawRow) {
+    const distance = currentRow - drawRow - 1
+    if ([...bends].reverse()[distance]) return bendInteractions[distance]
+    if (overdraws[distance]) return overdrawInteractions[distance]
+    return undefined
+  }
+
+  const errorMessage = `
+    Current row is somehow not related to blow and draw rows as expected.
+
+    blowRow: ${blowRow}
+    drawRow: ${drawRow}
+    currentRow: ${currentRow}
+  `
+  throw new Error(errorMessage)
+}
