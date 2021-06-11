f535ff8a3db47b6b7ea00e15456a36e15c40e0f4
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-11 14:37:35 +0000

    Bring functions together for end to end process
    The fact that there is a `reed-array-to-matrices.ts` file already in
    place was a mistake from the previous commit where a commented out
    partial attempt at the file was committed while some preparation files
    were committed.

--- packages/harpparts/src/apparatus/utils/reed-array-to-matrices/reed-array-to-matrices.ts
+++ packages/harpparts/src/apparatus/utils/reed-array-to-matrices/reed-array-to-matrices.ts
@@ -1,12 +1,43 @@
-//import { pivotReedArray } from '../pivot-reed-array'
-//import type { ReedArray } from '../../types'
-//import type { HarpFaceMatrix, HalfstepIndex, Interaction } from '../../../types'
-//
-//type Matrices = {
-//  halfsteindexMatrix: HarpFaceMatrix<HalfstepIndex>
-//  halfsteindexMatrix: HarpFaceMatrix<Interaction>
-//}
-//
-//export const reedArrayToMatrices = (reedArray: ReedArray): Matrices => {
-//  const holeArray = pivotReedArray(reedArray).map(insertHoleBends
-//}
+import { pivotReedArray } from '../pivot-reed-array'
+import { mapReedPairToHole } from '../map-reed-pair-to-hole'
+import { mapHoleToIncludeOverbends } from '../map-hole-to-include-overbends'
+import { mapHoleToIncludeBends } from '../map-hole-to-include-bends'
+import { mapHoleToFilterOverbends } from '../map-hole-to-filter-overbends'
+import { mapHoleTierToInteractionid } from '../map-hole-tier-to-interactionid'
+import { mapHoleTierToHalfstepindex } from '../map-hole-tier-to-halstepindex'
+import { deriveMatrixSpecs } from '../derive-matrix-specs/derive-matrix-specs'
+import type { HoleArray, ReedArray } from '../../types'
+import type { HarpFaceMatrix, HalfstepIndex } from '../../../types'
+import type { Interaction } from '../../../interaction'
+
+type Matrices = {
+  halfstepindexMatrix: HarpFaceMatrix<HalfstepIndex>
+  interactionMatrix: HarpFaceMatrix<Interaction>
+}
+
+export const reedArrayToMatrices = (reedArray: ReedArray): Matrices => {
+  const holeArray = pivotReedArray(reedArray)
+    .map(mapReedPairToHole)
+    .map(mapHoleToIncludeBends)
+    .map(mapHoleToIncludeOverbends)
+    .map(mapHoleToFilterOverbends) as HoleArray
+
+  const matrixSpecs = deriveMatrixSpecs(holeArray)
+
+  const matrixPrimer = new Array(matrixSpecs.height).fill(undefined)
+
+  const halfstepindexMatrix = matrixPrimer.map((_, index) =>
+    holeArray.map(
+      mapHoleTierToHalfstepindex.bind(undefined, matrixSpecs, index)
+    )
+  )
+  const interactionMatrix = matrixPrimer.map((_, index) =>
+    holeArray.map(
+      mapHoleTierToInteractionid.bind(undefined, matrixSpecs, index)
+    )
+  )
+  return {
+    halfstepindexMatrix,
+    interactionMatrix,
+  }
+}
