f5383c637a60edd3baf6cab89e2afe920ffe944e
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-13 09:14:16 +0000

    Add test for legitimate combination of bends

--- packages/harpparts/src/apparatus/utils/is-hole-valid/is-hole-valid.test.ts
+++ packages/harpparts/src/apparatus/utils/is-hole-valid/is-hole-valid.test.ts
@@ -2,7 +2,7 @@ import type { Hole } from '../../types'
 
 import { isHoleValid } from './is-hole-valid'
 
-test('isHoleValid returns true on with no bends', () => {
+test('isHoleValid returns true with no bends', () => {
   const hole: Hole = {
     blow: 0,
     draw: 1,
@@ -14,7 +14,31 @@ test('isHoleValid returns true on with no bends', () => {
   expect(isHoleValid(hole)).toBeTruthy()
 })
 
-test('isHoleValid returns false when bends and overdraws are available', () => {
+test('isHoleValid returns true when blowbends and overdraws are both available', () => {
+  const hole: Hole = {
+    blow: 0,
+    draw: 1,
+    bends: [],
+    blowbends: [1],
+    overblows: [],
+    overdraws: [1],
+  }
+  expect(isHoleValid(hole)).toBeTruthy()
+})
+
+test('isHoleValid returns true when bends and overblows are both available', () => {
+  const hole: Hole = {
+    blow: 0,
+    draw: 1,
+    bends: [1],
+    blowbends: [],
+    overblows: [1],
+    overdraws: [],
+  }
+  expect(isHoleValid(hole)).toBeTruthy()
+})
+
+test('isHoleValid returns false when bends and overdraws are both available', () => {
   const hole: Hole = {
     blow: 0,
     draw: 1,
@@ -22,6 +46,18 @@ test('isHoleValid returns false when bends and overdraws are available', () => {
     blowbends: [],
     overblows: [],
     overdraws: [1],
+  }
+  expect(isHoleValid(hole)).toBeFalsy()
+})
+
+test('isHoleValid returns false when blowbends and overblows are both available', () => {
+  const hole: Hole = {
+    blow: 0,
+    draw: 1,
+    bends: [],
+    blowbends: [1],
+    overblows: [1],
+    overdraws: [],
   }
   expect(isHoleValid(hole)).toBeFalsy()
 })
--- packages/harpparts/src/apparatus/utils/is-hole-valid/is-hole-valid.ts
+++ packages/harpparts/src/apparatus/utils/is-hole-valid/is-hole-valid.ts
@@ -3,5 +3,7 @@ import type { Hole } from '../../types'
 export const isHoleValid = (hole: Hole): boolean => {
   const hasConflictingDrawBends =
     hole.bends.length > 0 && hole.overdraws.length > 0
-  return !hasConflictingDrawBends
+  const hasConflictingBlowBends =
+    hole.blowbends.length > 0 && hole.overblows.length > 0
+  return !hasConflictingDrawBends && !hasConflictingBlowBends
 }
