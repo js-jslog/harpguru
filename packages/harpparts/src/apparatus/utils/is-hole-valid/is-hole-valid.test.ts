fc0e000ca85f4051b046884bf23dce6ae05d3c15
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-13 09:08:11 +0000

    Add test for existing functionality

--- packages/harpparts/src/apparatus/utils/is-hole-valid/is-hole-valid.test.ts
+++ packages/harpparts/src/apparatus/utils/is-hole-valid/is-hole-valid.test.ts
@@ -2,6 +2,18 @@ import type { Hole } from '../../types'
 
 import { isHoleValid } from './is-hole-valid'
 
+test('isHoleValid returns true on with no bends', () => {
+  const hole: Hole = {
+    blow: 0,
+    draw: 1,
+    bends: [],
+    blowbends: [],
+    overblows: [],
+    overdraws: [],
+  }
+  expect(isHoleValid(hole)).toBeTruthy()
+})
+
 test('isHoleValid returns false when bends and overdraws are available', () => {
   const hole: Hole = {
     blow: 0,
