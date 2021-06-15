7913cd6e159f9cf0ed0655cc02cf906bcde4494d
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-15 16:55:04 +0000

    Convert natural minor to reed array defined
    And discover that my original matrix definitions were inaccurate. I was
    missing an overdraw on the 10 hole.

--- packages/harpparts/src/apparatus/constants/apparatus/NATURAL_MINOR_APPARATUS.ts
+++ packages/harpparts/src/apparatus/constants/apparatus/NATURAL_MINOR_APPARATUS.ts
@@ -1,45 +1,37 @@
-import { ApparatusIds } from '../../types'
-import type { Apparatus } from '../../types'
-import type { HarpFaceMatrix, HalfstepIndex } from '../../../types'
-import {
-  BLOW,
-  DRAW,
-  BEND1,
-  BEND2,
-  BEND3,
-  BLOWBEND1,
-  BLOWBEND2,
-  OVERDRAW1,
-  OVERBLOW1,
-} from '../../../interaction'
-import type { Interaction } from '../../../interaction'
+import { reedArrayToMatrices } from '../../utils'
+import { ApparatusIds, ReedTuningPitches } from '../../types'
+import type { Apparatus, ReedArray } from '../../types'
+
+const {
+  c1,
+  d1,
+  eb1,
+  g1,
+  bb1,
+  c2,
+  d2,
+  eb2,
+  f2,
+  g2,
+  a2,
+  bb2,
+  d3,
+  eb3,
+  f3,
+  g3,
+  a3,
+  c3,
+  c4,
+} = ReedTuningPitches
 
 // prettier-ignore
-const halfstepIndexMatrix: HarpFaceMatrix<HalfstepIndex> = [
-  //    1          2          3          4          5          6          7          8          9         10
-  [ undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 34        ],
-  [ undefined, undefined, 11       , undefined, 18       , undefined, 23       , undefined, 30       , 35        ],
-  [ 0        , 3        , 7        , 12       , 15       , 19       , 24       , 27       , 31       , 36        ], // BLOW
-  [ 2        , 7        , 10       , 14       , 17       , 21       , 22       , 26       , 29       , 33        ],
-  [ 1        , 6        , 9        , 13       , 16       , 20       , 25       , 28       , 32       , undefined ],
-  [ undefined, 5        , 8        , undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
-  [ undefined, 4        , undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
-] as const
-
-// prettier-ignore
-const interactionMatrix: HarpFaceMatrix<Interaction> = [
-  //    1          2          3          4          5          6          7          8          9         10
-  [ undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, BLOWBEND2 ],
-  [ undefined, undefined, OVERBLOW1, undefined, OVERBLOW1, undefined, BLOWBEND1, undefined, BLOWBEND1, BLOWBEND1 ],
-  [ BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW      ],
-  [ DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW      ],
-  [ BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , OVERDRAW1, OVERDRAW1, OVERDRAW1, undefined ],
-  [ undefined, BEND2    , BEND2    , undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
-  [ undefined, BEND3    , undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
-] as const
+const reedArray: ReedArray = [
+  // 1    2    3    4    5    6    7    8    9   10
+  [ c1 , eb1, g1 , c2 , eb2, g2 , c3 , eb3, g3 , c4 ],
+  [ d1 , g1 , bb1, d2 , f2 , a2 , bb2, d3 , f3 , a3 ],
+]
 
 export const NATURAL_MINOR_APPARATUS: Apparatus = {
   id: ApparatusIds.NaturalMinor,
-  halfstepIndexMatrix,
-  interactionMatrix,
+  ...reedArrayToMatrices(reedArray, ApparatusIds.NaturalMinor),
 } as const
--- packages/harpparts/src/apparatus/utils/reed-array-to-matrices/reed-array-to-matrices.test.ts
+++ packages/harpparts/src/apparatus/utils/reed-array-to-matrices/reed-array-to-matrices.test.ts
@@ -20,18 +20,23 @@ import { reedArrayToMatrices } from './reed-array-to-matrices'
 const {
   c1,
   d1,
+  eb1,
   e1,
   g1,
+  bb1,
   b1,
   c2,
   d2,
+  eb2,
   e2,
   f2,
   gb2,
   g2,
   a2,
+  bb2,
   b2,
   d3,
+  eb3,
   e3,
   f3,
   g3,
@@ -133,6 +138,42 @@ test('reedArrayToMatrices works as expected for a Country tuned harp', () => {
     [ undefined, undefined, BEND3    , undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
   ] as const
   // prettier-ignore
+
+  const matrices = reedArrayToMatrices(reedArray, CountryTuned)
+  expect(matrices).toStrictEqual({ halfstepIndexMatrix, interactionMatrix })
+})
+
+test('reedArrayToMatrices works as expected for a Natrual Minor tuned harp', () => {
+  // prettier-ignore
+  const reedArray: ReedArray = [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , eb1, g1 , c2 , eb2, g2 , c3 , eb3, g3 , c4 ],
+    [ d1 , g1 , bb1, d2 , f2 , a2 , bb2, d3 , f3 , a3 ],
+  ]
+
+  // prettier-ignore
+  const halfstepIndexMatrix: HarpFaceMatrix<HalfstepIndex> = [
+    //    1          2          3          4          5          6          7          8          9         10
+    [ undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 34        ],
+    [ undefined, undefined, 11       , undefined, 18       , undefined, 23       , undefined, 30       , 35        ],
+    [ 0        , 3        , 7        , 12       , 15       , 19       , 24       , 27       , 31       , 36        ], // BLOW
+    [ 2        , 7        , 10       , 14       , 17       , 21       , 22       , 26       , 29       , 33        ],
+    [ 1        , 6        , 9        , 13       , 16       , 20       , 25       , 28       , 32       , 37        ],
+    [ undefined, 5        , 8        , undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
+    [ undefined, 4        , undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
+  ] as const
+
+  // prettier-ignore
+  const interactionMatrix: HarpFaceMatrix<Interaction> = [
+    //    1          2          3          4          5          6          7          8          9         10
+    [ undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, BLOWBEND2 ],
+    [ undefined, undefined, OVERBLOW1, undefined, OVERBLOW1, undefined, BLOWBEND1, undefined, BLOWBEND1, BLOWBEND1 ],
+    [ BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW      ],
+    [ DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW      ],
+    [ BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , OVERDRAW1, OVERDRAW1, OVERDRAW1, OVERDRAW1 ],
+    [ undefined, BEND2    , BEND2    , undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
+    [ undefined, BEND3    , undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined ],
+  ] as const
 
   const matrices = reedArrayToMatrices(reedArray, CountryTuned)
   expect(matrices).toStrictEqual({ halfstepIndexMatrix, interactionMatrix })
