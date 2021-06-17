7d8846625379e8eeab725ed89e4ada4776c9e9be
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-17 15:47:49 +0000

    Convert woozle minor to reed-array defined

--- packages/harpparts/src/apparatus/constants/apparatus/WOOZLE_MINOR_APPARATUS.ts
+++ packages/harpparts/src/apparatus/constants/apparatus/WOOZLE_MINOR_APPARATUS.ts
@@ -1,40 +1,38 @@
-import { ApparatusIds } from '../../types'
-import type { Apparatus } from '../../types'
-import type { HarpFaceMatrix, HalfstepIndex } from '../../../types'
-import {
-  BLOW,
-  DRAW,
-  BEND1,
-  BEND2,
-  BEND3,
-  OVERBLOW1,
-} from '../../../interaction'
-import type { Interaction } from '../../../interaction'
+import { reedArrayToMatrices } from '../../utils'
+import { ApparatusIds, ReedTuningPitches } from '../../types'
+import type { Apparatus, ReedArray } from '../../types'
+
+const {
+  c1,
+  d1,
+  eb1,
+  g1,
+  bb1,
+  c2,
+  d2,
+  eb2,
+  f2,
+  g2,
+  a2,
+  bb2,
+  d3,
+  eb3,
+  g3,
+  a3,
+  c3,
+  c4,
+} = ReedTuningPitches
 
 // prettier-ignore
-const halfstepIndexMatrix: HarpFaceMatrix<HalfstepIndex> = [
-  //    1          2          3          4          5          6          7          8          9         10
-  [ undefined, undefined, 11       , undefined, 18       , undefined, 23       , undefined, 32       , 37        ], 
-  [ 0        , 3        , 7        , 12       , 15       , 19       , 21       , 24       , 27       , 33        ],
-  [ 2        , 7        , 10       , 14       , 17       , 21       , 22       , 26       , 31       , 36        ],
-  [ 1        , 6        , 9        , 13       , 16       , 20       , undefined, 25       , 30       , 35        ],
-  [ undefined, 5        , 8        , undefined, undefined, undefined, undefined, undefined, 29       , 34        ],
-  [ undefined, 4        , undefined, undefined, undefined, undefined, undefined, undefined, 28       , undefined ],
-] as const
+const reedArray: ReedArray = [
+  // 1    2    3    4    5    6    7    8    9   10
+  [ c1 , eb1, g1 , c2 , eb2, g2 , a2 , c3 , eb3, a3 ],
+  [ d1 , g1 , bb1, d2 , f2 , a2 , bb2, d3 , g3 , c4 ],
+]
 
-// prettier-ignore
-const interactionMatrix: HarpFaceMatrix<Interaction> = [
-  //    1          2          3          4          5          6          7          8          9         10
-  [ undefined, undefined, OVERBLOW1, undefined, OVERBLOW1, undefined, OVERBLOW1, undefined, OVERBLOW1, OVERBLOW1 ],
-  [ BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW      ],
-  [ DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW      ],
-  [ BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , undefined, BEND1    , BEND1    , BEND1     ],
-  [ undefined, BEND2    , BEND2    , undefined, undefined, undefined, undefined, undefined, BEND2    , BEND2     ],
-  [ undefined, BEND3    , undefined, undefined, undefined, undefined, undefined, undefined, BEND3    , undefined ],
-] as const
+const { WoozleMinor: id } = ApparatusIds
 
 export const WOOZLE_MINOR_APPARATUS: Apparatus = {
-  id: ApparatusIds.WoozleMinor,
-  halfstepIndexMatrix,
-  interactionMatrix,
+  id,
+  ...reedArrayToMatrices(reedArray, id),
 } as const
--- packages/harpparts/src/apparatus/utils/reed-array-to-matrices/reed-array-to-matrices.test.ts
+++ packages/harpparts/src/apparatus/utils/reed-array-to-matrices/reed-array-to-matrices.test.ts
@@ -50,6 +50,7 @@ const {
   PowerBender,
   PowerDraw,
   WildeTuned,
+  WoozleMinor,
 } = ApparatusIds
 
 test('reedArrayToMatrices throws errors as expected when any error is encountered', () => {
@@ -284,5 +285,39 @@ test('reedArrayToMatrices works as expected for a Wilde tuned harp', () => {
   ] as const
 
   const matrices = reedArrayToMatrices(reedArray, WildeTuned)
+  expect(matrices).toStrictEqual({ halfstepIndexMatrix, interactionMatrix })
+})
+
+test('reedArrayToMatrices works as expected for a Woozle Minor tuned harp', () => {
+  // prettier-ignore
+  const reedArray: ReedArray = [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , eb1, g1 , c2 , eb2, g2 , a2 , c3 , eb3, a3 ],
+    [ d1 , g1 , bb1, d2 , f2 , a2 , bb2, d3 , g3 , c4 ],
+  ]
+
+  // prettier-ignore
+  const halfstepIndexMatrix: HarpFaceMatrix<HalfstepIndex> = [
+    //    1          2          3          4          5          6          7          8          9         10
+    [ undefined, undefined, 11       , undefined, 18       , undefined, 23       , undefined, 32       , 37        ], 
+    [ 0        , 3        , 7        , 12       , 15       , 19       , 21       , 24       , 27       , 33        ],
+    [ 2        , 7        , 10       , 14       , 17       , 21       , 22       , 26       , 31       , 36        ],
+    [ 1        , 6        , 9        , 13       , 16       , 20       , undefined, 25       , 30       , 35        ],
+    [ undefined, 5        , 8        , undefined, undefined, undefined, undefined, undefined, 29       , 34        ],
+    [ undefined, 4        , undefined, undefined, undefined, undefined, undefined, undefined, 28       , undefined ],
+  ] as const
+
+  // prettier-ignore
+  const interactionMatrix: HarpFaceMatrix<Interaction> = [
+    //    1          2          3          4          5          6          7          8          9         10
+    [ undefined, undefined, OVERBLOW1, undefined, OVERBLOW1, undefined, OVERBLOW1, undefined, OVERBLOW1, OVERBLOW1 ],
+    [ BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW     , BLOW      ],
+    [ DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW     , DRAW      ],
+    [ BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , BEND1    , undefined, BEND1    , BEND1    , BEND1     ],
+    [ undefined, BEND2    , BEND2    , undefined, undefined, undefined, undefined, undefined, BEND2    , BEND2     ],
+    [ undefined, BEND3    , undefined, undefined, undefined, undefined, undefined, undefined, BEND3    , undefined ],
+  ] as const
+
+  const matrices = reedArrayToMatrices(reedArray, WoozleMinor)
   expect(matrices).toStrictEqual({ halfstepIndexMatrix, interactionMatrix })
 })
