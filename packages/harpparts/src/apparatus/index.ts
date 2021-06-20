671fbe0ec39aed0c45580fa36a0b32730bfec55c
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-20 15:00:41 +0000

    Remove redundant apparatus constants
    The valved files are still useful as a record of the expected matrices
    for these apparatus until we've got the valved logic included.

--- packages/harpparts/src/apparatus/index.ts
+++ packages/harpparts/src/apparatus/index.ts
@@ -1,11 +1,2 @@
-export {
-  MAJOR_DIATONIC_APPARATUS,
-  COUNTRY_TUNED_APPARATUS,
-  NATURAL_MINOR_APPARATUS,
-  WILDE_TUNED_APPARATUS,
-  POWER_BENDER_APPARATUS,
-  POWER_DRAW_APPARATUS,
-  WOOZLE_MINOR_APPARATUS,
-} from './constants'
 export type { Apparatus } from './types'
 export { buildApparatus } from './utils'
