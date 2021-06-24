75fe2d958ffb9ad89e25c8e144151141e36f2db9
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-24 07:51:07 +0000

    Add comment to clarify assumptions in implementation

--- packages/harpparts/src/apparatus/utils/reduce-hole-for-biggest-bends/reduce-hole-for-biggest-bends.ts
+++ packages/harpparts/src/apparatus/utils/reduce-hole-for-biggest-bends/reduce-hole-for-biggest-bends.ts
@@ -18,6 +18,11 @@ export const reduceHoleForBiggestBends = (
     valveddraws: { length: currValveddraws },
   } = currentValue
   const { biggestBlow: accBigBlow, biggestDraw: accBigDraw } = accumulator
+  // Lengths of zero will evaluate to false here. We are relying on the
+  // feature of Hole's that they do not have multiple bends of a blow or
+  // draw type on a single hole. This logic would not work if it were
+  // possible to have a bend and an overdraw on the same hole, because
+  // it would not necessariliy find the largest of the two.
   const currBigBlow = currBlowbends || currOverblows || currValvedblows
   const currBigDraw = currBends || currOverdraws || currValveddraws
 
