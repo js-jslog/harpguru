bb85d5a82ad644ec7419016c6748b51d4e24c2b7
js-jslog <jhs4jbs@hotmail.co.uk>
2021-06-27 18:49:17 +0000

    Add extra security to integration test
    This *should* be unnecessary but since the utility function doesn't
    itself have the ability to test halfstepindex vs interaction matrices I
    thought it would be prudent to have an extra check that misaligned
    matrices here would actually produce failing tests.

--- packages/harpparts/src/apparatus-integration.test.ts
+++ packages/harpparts/src/apparatus-integration.test.ts
@@ -1,9 +1,9 @@
+import { ValvingIds } from './valving'
+import { TuningIds } from './tuning'
 import { matricesHaveParity } from './packages/matrices-have-parity'
 import { buildApparatus } from './apparatus'
 import { getTuningIds, getValvingIds } from './access-parts'
 
-// It would also be nice to get all the results of the partiy check in before the test fails too.
-// We need to check that this test fails if a reed config is made available which is leads to a hole array error.
 test('From each of the available tunings, both the halfstepindex interaction matrices have parity, and no errors are thrown in the process of producing them', () => {
   getTuningIds()
     .map((tuningId) =>
@@ -18,4 +18,14 @@ test('From each of the available tunings, both the halfstepindex interaction mat
       expect(hasParity).toBeTruthy()
       expect(errorMessages).toStrictEqual([])
     })
+
+  const [hasParity, errorMessages] = matricesHaveParity(
+    buildApparatus(TuningIds.MajorDiatonic, ValvingIds.NotValved)
+      .halfstepIndexMatrix,
+    buildApparatus(TuningIds.MajorDiatonic, ValvingIds.HalfValved)
+      .interactionMatrix
+  )
+
+  expect(hasParity).toBeFalsy()
+  expect(errorMessages.length).toBeGreaterThan(0)
 })
