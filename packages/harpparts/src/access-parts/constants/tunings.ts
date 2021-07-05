f607a40ddd07f30022b975fab4f72ce8fa63b9b8
js-jslog <jhs4jbs@hotmail.co.uk>
2021-07-05 17:45:50 +0000

    Add new tunings config and rename awful mappings
    I haven't been extremely careful with these configurations because it
    will be easiest to verify that the output of the running app which I'll
    do shortly.
    
    The reason the renamed mappings were "awful" is because they should have
    been caught in the code review of the pull request which introduced
    them.

--- packages/harpparts/src/access-parts/constants/tunings.ts
+++ packages/harpparts/src/access-parts/constants/tunings.ts
@@ -4,14 +4,26 @@ import {
   COUNTRY,
   NATURAL_MINOR,
   HARMONIC_MINOR,
+  MELODY_MAKER,
+  WILDE,
   POWER_BENDER,
   POWER_DRAW,
   PADDY_RICHTER,
-  MELODY_MAKER,
-  WILDE,
+  BLUES_ONE,
+  BLUES_TWO,
+  BLUES_THREE,
+  CHORD,
+  EASY_THIRD,
+  BAGPIPE,
+  WILL_SCARLETT,
   WOOZLE_MINOR,
   AUGMENTED_SPANISH,
   SPIRAL_CIRCULAR,
+  MIXOLYDIAN,
+  PHRYGIAN,
+  LOCRIAN,
+  WHOLE_TONE,
+  DIMINISHED,
 } from '../../tuning'
 import type { Tuning } from '../../tuning'
 
@@ -20,11 +32,23 @@ orderedTunings.set(TuningIds.Richter, RICHTER)
 orderedTunings.set(TuningIds.Country, COUNTRY)
 orderedTunings.set(TuningIds.NaturalMinor, NATURAL_MINOR)
 orderedTunings.set(TuningIds.HarmonicMinor, HARMONIC_MINOR)
+orderedTunings.set(TuningIds.MelodyMaker, MELODY_MAKER)
 orderedTunings.set(TuningIds.Wilde, WILDE)
 orderedTunings.set(TuningIds.PowerBender, POWER_BENDER)
 orderedTunings.set(TuningIds.PowerDraw, POWER_DRAW)
-orderedTunings.set(TuningIds.PowerDraw, PADDY_RICHTER)
-orderedTunings.set(TuningIds.MelodyMaker, MELODY_MAKER)
+orderedTunings.set(TuningIds.PaddyRichter, PADDY_RICHTER)
+orderedTunings.set(TuningIds.BluesOne, BLUES_ONE)
+orderedTunings.set(TuningIds.BluesTwo, BLUES_TWO)
+orderedTunings.set(TuningIds.BluesThree, BLUES_THREE)
+orderedTunings.set(TuningIds.Chord, CHORD)
+orderedTunings.set(TuningIds.EasyThird, EASY_THIRD)
+orderedTunings.set(TuningIds.Bagpipe, BAGPIPE)
+orderedTunings.set(TuningIds.WillScarlett, WILL_SCARLETT)
 orderedTunings.set(TuningIds.WoozleMinor, WOOZLE_MINOR)
-orderedTunings.set(TuningIds.WoozleMinor, AUGMENTED_SPANISH)
-orderedTunings.set(TuningIds.WoozleMinor, SPIRAL_CIRCULAR)
+orderedTunings.set(TuningIds.AugmentedSpanish, AUGMENTED_SPANISH)
+orderedTunings.set(TuningIds.SpiralCircular, SPIRAL_CIRCULAR)
+orderedTunings.set(TuningIds.Mixolydian, MIXOLYDIAN)
+orderedTunings.set(TuningIds.Phrygian, PHRYGIAN)
+orderedTunings.set(TuningIds.Locrian, LOCRIAN)
+orderedTunings.set(TuningIds.WholeTone, WHOLE_TONE)
+orderedTunings.set(TuningIds.Diminished, DIMINISHED)
--- packages/harpparts/src/tuning/constants/index.ts
+++ packages/harpparts/src/tuning/constants/index.ts
@@ -3,16 +3,19 @@ import type { Tuning } from '../types'
 
 const {
   c1,
+  db1,
   d1,
   eb1,
   e1,
   f1,
+  gb1,
   g1,
   ab1,
   a1,
   bb1,
   b1,
   c2,
+  db2,
   d2,
   eb2,
   e2,
@@ -23,6 +26,7 @@ const {
   a2,
   bb2,
   b2,
+  db3,
   d3,
   eb3,
   e3,
@@ -31,9 +35,11 @@ const {
   g3,
   ab3,
   a3,
+  bb3,
   b3,
   c3,
   c4,
+  d4,
   eb4,
 } = ReedTuningPitches
 
@@ -107,6 +113,76 @@ export const PADDY_RICHTER: Tuning = {
   ],
 } as const
 
+export const BLUES_ONE: Tuning = {
+  id: TuningIds.BluesOne,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , g1 , c2 , e2 , g2 , c3 , e3 , g3 , c4 ],
+    [ d1 , g1 , b1 , d2 , g2 , b2 , d3 , f3 , f3 , a3 ],
+  ],
+} as const
+
+export const BLUES_TWO: Tuning = {
+  id: TuningIds.BluesTwo,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , g1 , bb1, c2 , g2 , c3 , e3 , g3 , c4 ],
+    [ d1 , g1 , b1 , d2 , f2 , a2 , b2 , d3 , f3 , a3 ],
+  ],
+} as const
+
+export const BLUES_THREE: Tuning = {
+  id: TuningIds.BluesThree,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , g1 , c2 , e2 , g2 , bb2, e3 , g3 , c4 ],
+    [ d1 , g1 , b1 , d2 , f2 , a2 , c3 , d3 , f3 , a3 ],
+  ],
+} as const
+
+export const CHORD: Tuning = {
+  id: TuningIds.Chord,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , g1 , c2 , e2 , g2 , bb2, bb2, d3 , f3 ],
+    [ d1 , g1 , b1 , d2 , f2 , a2 , c3 , d3 , gb3, a3 ],
+  ],
+} as const
+
+export const EASY_THIRD: Tuning = {
+  id: TuningIds.EasyThird,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , g1 , c2 , e2 , g2 , c3 , e3 , g3 , c4 ],
+    [ d1 , f1 , a1 , d2 , f2 , a2 , b2 , d3 , f3 , a3 ],
+  ],
+} as const
+
+export const BAGPIPE: Tuning = {
+  id: TuningIds.Bagpipe,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , g1 , g1 , c2 , e2 , g2 , c3 , e3 , g3 , c4 ],
+    [ c1 , g1 , bb1, d2 , f2 , a2 , bb2, d3 , f3 , a3 ],
+  ],
+} as const
+
+export const WILL_SCARLETT: Tuning = {
+  id: TuningIds.WillScarlett,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , a1 , c2 , e2 , g2 , c3 , e3 , g3 , c4 ],
+    [ e1 , g1 , b1 , d2 , gb2, a2 , b2 , d3 , gb3, a3 ],
+  ],
+} as const
+
 export const MELODY_MAKER: Tuning = {
   id: TuningIds.MelodyMaker,
   // prettier-ignore
@@ -154,5 +230,55 @@ export const SPIRAL_CIRCULAR: Tuning = {
     // 1    2    3    4    5    6    7    8    9   10
     [ c1 , e1 , g1 , bb1, d2 , f2 , a2 , c3 , e3 , g3 ],
     [ d1 , f1 , a1 , c2 , e2 , g2 , bb2, d3 , f3 , a3 ],
+  ],
+} as const
+
+export const MIXOLYDIAN: Tuning = {
+  id: TuningIds.Mixolydian,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , g1 , c2 , e2 , g2 , c3 , e3 , g3 , c4 ],
+    [ d1 , g1 , bb1, d2 , f2 , a2 , bb2, d3 , f3 , a3 ],
+  ],
+} as const
+
+export const PHRYGIAN: Tuning = {
+  id: TuningIds.Phrygian,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , eb1, g1 , c2 , eb2, g2 , c3 , eb3, g3 , c4 ],
+    [ db1, g1 , bb1, db2, f2 , ab2, bb2, db3, f3 , ab3],
+  ],
+} as const
+
+export const LOCRIAN: Tuning = {
+  id: TuningIds.Locrian,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , eb1, gb1, c2 , eb2, gb2, c3 , eb3, gb3, c4 ],
+    [ db1, gb1, bb1, db2, f2 , ab2, bb2, db3, f3 , ab3],
+  ],
+} as const
+
+export const WHOLE_TONE: Tuning = {
+  id: TuningIds.WholeTone,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , e1 , ab1, c2 , e2 , ab2, c3 , e3 , ab3, c4 ],
+    [ d1 , gb1, bb1, d2 , gb2, bb2, d3 , gb3, bb3, d4 ],
+  ],
+} as const
+
+export const DIMINISHED: Tuning = {
+  id: TuningIds.Diminished,
+  // prettier-ignore
+  reedArray: [
+    // 1    2    3    4    5    6    7    8    9   10
+    [ c1 , eb1, gb1, a1 , c2 , eb2, gb2, a2 , c3 , eb3],
+    [ d1 , f1 , ab1, b1 , d2 , f2 , ab2, b2 , d3 , f3 ],
   ],
 } as const
