05005de33082008c2f9cd45fb46fefd62b4f5a00
js-jslog <jhs4jbs@hotmail.co.uk>
2021-07-11 11:37:21 +0000

    Make scales menu title adaptive to Scale vs Chord

--- packages/harpguru-core/src/components/menu-of-scales/hooks/use-scale-title/use-scale-title.tsx
+++ packages/harpguru-core/src/components/menu-of-scales/hooks/use-scale-title/use-scale-title.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getScaleByDegreeIds } from 'harpparts'
+import { getScaleByDegreeIds, ScaleCategory } from 'harpparts'
 
 import { OptionLabel } from '../../../option-label'
 import type { OptionLabelProps } from '../../../option-label'
@@ -10,10 +10,12 @@ export const useScaleTitle = (
 ): React.ReactElement<OptionLabelProps> => {
   const [activeHarpStrata] = useGlobal('activeHarpStrata')
   const { activeDegreeIds } = activeHarpStrata
-  const { label: scaleLabel } = getScaleByDegreeIds(activeDegreeIds) || {}
+  const { label: scaleLabel, category: scaleCategory } =
+    getScaleByDegreeIds(activeDegreeIds) || {}
+  const title = scaleCategory === ScaleCategory.Chord ? 'Chord' : 'Scale'
   return (
     <OptionLabel
-      title={'Scale'}
+      title={title}
       isLargeTitle={true}
       value={scaleLabel || ''}
       alignItems={'flex-start'}
