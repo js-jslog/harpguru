61ccd70c393ad371cbb4f8e79835eae440667005
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-17 12:08:20 +0000

    Add functionality to slice viewable matrix
    I have moved the slice-matrix package from harpstrata because it will
    not be required there anymore.

--- packages/harpguru-core/src/components/harp-face/hooks/use-viewable-matrices/use-viewable-matrices.test.ts
+++ packages/harpguru-core/src/components/harp-face/hooks/use-viewable-matrices/use-viewable-matrices.test.ts
@@ -1,4 +1,5 @@
 import { useGlobal } from 'reactn'
+import { DegreeIds } from 'harpparts'
 
 import { inactiveCellsHarpStrata } from '../../../../test-resources'
 
@@ -10,11 +11,34 @@ const mockUseGlobal = useGlobal as jest.Mock
 test('Entire degreeMatrix is viewable when columnBounds is FIT', () => {
   mockUseGlobal.mockImplementation((stateItem: string) => {
     if (stateItem === 'activeHarpStrata') return [inactiveCellsHarpStrata]
-    if (stateItem === 'columnBounds') return 'FIT'
+    if (stateItem === 'columnBounds') return ['FIT']
     return undefined
   })
 
   expect(useViewableMatrices().viewableDegreeMatrix).toStrictEqual(
     inactiveCellsHarpStrata.degreeMatrix
+  )
+})
+
+test('First column of degreeMatrix is viewable when columnBounds is [0, 0]', () => {
+  const { Root, Second, Third } = DegreeIds
+  const simplifiedDegreeMatrix = [
+    [Root, Second, Third],
+    [Second, Third, Root],
+    [Third, Second, Root],
+  ]
+  const modifiedHarpStrata = {
+    ...inactiveCellsHarpStrata,
+    degreeMatrix: simplifiedDegreeMatrix,
+  }
+  const expectedViewableMatrix = [[Root], [Second], [Third]]
+  mockUseGlobal.mockImplementation((stateItem: string) => {
+    if (stateItem === 'activeHarpStrata') return [modifiedHarpStrata]
+    if (stateItem === 'columnBounds') return [[0, 0]]
+    return undefined
+  })
+
+  expect(useViewableMatrices().viewableDegreeMatrix).toStrictEqual(
+    expectedViewableMatrix
   )
 })
--- packages/harpguru-core/src/components/harp-face/hooks/use-viewable-matrices/use-viewable-matrices.ts
+++ packages/harpguru-core/src/components/harp-face/hooks/use-viewable-matrices/use-viewable-matrices.ts
@@ -1,14 +1,28 @@
 import { useGlobal } from 'reactn'
 import type { Degree, HarpFaceMatrix } from 'harpparts'
 
+import { sliceMatrix } from '../../../../packages/slice-matrix'
+
 type ViewableMatrices = {
   viewableDegreeMatrix: HarpFaceMatrix<Degree>
 }
 
 export const useViewableMatrices = (): ViewableMatrices => {
   const [activeHarpStrata] = useGlobal('activeHarpStrata')
+  const [columnBounds] = useGlobal('columnBounds')
+
+  if (columnBounds === 'FIT')
+    return {
+      viewableDegreeMatrix: activeHarpStrata.degreeMatrix,
+    }
+
+  const [start, end] = columnBounds
 
   return {
-    viewableDegreeMatrix: activeHarpStrata.degreeMatrix,
+    viewableDegreeMatrix: sliceMatrix(
+      activeHarpStrata.degreeMatrix,
+      start,
+      end + 1
+    ),
   }
 }
