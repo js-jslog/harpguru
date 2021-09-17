e34177c30e54568eca899bd69be984f1f0d2cb47
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-17 15:34:58 +0000

    Add columnBound filter to octave column grouping logic

--- packages/harpguru-core/src/components/harp-face/hooks/use-octave-column-groups/use-octave-column-groups.test.ts
+++ packages/harpguru-core/src/components/harp-face/hooks/use-octave-column-groups/use-octave-column-groups.test.ts
@@ -11,6 +11,7 @@ test('Identifies the columns grouped by octave', () => {
   mockUseGlobal.mockImplementation((stateItem: string) => {
     if (stateItem === 'activeHarpStrata') return [activeHarpStrata]
     if (stateItem === 'fragmentHarpFaceByOctaves') return [true]
+    if (stateItem === 'columnBounds') return ['FIT']
     return undefined
   })
   const octaveColumnGroups = useOctaveColumnGroups()
@@ -23,10 +24,37 @@ test('Returns an ungrouped set if the harp fragmentation is off', () => {
   mockUseGlobal.mockImplementation((stateItem: string) => {
     if (stateItem === 'activeHarpStrata') return [activeHarpStrata]
     if (stateItem === 'fragmentHarpFaceByOctaves') return [false]
+    if (stateItem === 'columnBounds') return ['FIT']
     return undefined
   })
   const octaveColumnGroups = useOctaveColumnGroups()
   const expectedOctaveGroups = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+  expect(octaveColumnGroups).toStrictEqual(expectedOctaveGroups)
+})
+
+test('Returns a bounded grouped set', () => {
+  mockUseGlobal.mockImplementation((stateItem: string) => {
+    if (stateItem === 'activeHarpStrata') return [activeHarpStrata]
+    if (stateItem === 'fragmentHarpFaceByOctaves') return [true]
+    if (stateItem === 'columnBounds') return [[2, 7]]
+    return undefined
+  })
+  const octaveColumnGroups = useOctaveColumnGroups()
+  const expectedOctaveGroups = [[2], [3, 4, 5], [6, 7]]
+
+  expect(octaveColumnGroups).toStrictEqual(expectedOctaveGroups)
+})
+
+test('Returns a bounded ungrouped set', () => {
+  mockUseGlobal.mockImplementation((stateItem: string) => {
+    if (stateItem === 'activeHarpStrata') return [activeHarpStrata]
+    if (stateItem === 'fragmentHarpFaceByOctaves') return [false]
+    if (stateItem === 'columnBounds') return [[3, 8]]
+    return undefined
+  })
+  const octaveColumnGroups = useOctaveColumnGroups()
+  const expectedOctaveGroups = [[3, 4, 5, 6, 7, 8]]
 
   expect(octaveColumnGroups).toStrictEqual(expectedOctaveGroups)
 })
