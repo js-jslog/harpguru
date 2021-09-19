7c18b0524658dc58cf8dc55dbcbe06ee694b1b6b
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-19 10:43:29 +0000

    Add test to prove that edge case is actually handled

--- packages/harpguru-core/src/utils/determine-best-7-holes/determine-best-7-holes.test.ts
+++ packages/harpguru-core/src/utils/determine-best-7-holes/determine-best-7-holes.test.ts
@@ -1,10 +1,14 @@
 import { determineBest7Holes } from './determine-best-7-holes'
 
-test('best 7 holes is the same as input if end bound is less than harp length', () => {
+test('best 7 holes is the same as input if end bound is less than or equal to harp length', () => {
   const harpLength = 10
-  const inputColumnBounds = [0, 6] as const
-  expect(determineBest7Holes(harpLength, inputColumnBounds)).toBe(
-    inputColumnBounds
+  const inputColumnBoundsLessThan = [0, 6] as const
+  expect(determineBest7Holes(harpLength, inputColumnBoundsLessThan)).toBe(
+    inputColumnBoundsLessThan
+  )
+  const inputColumnBoundsOnLimit = [3, 9] as const
+  expect(determineBest7Holes(harpLength, inputColumnBoundsOnLimit)).toBe(
+    inputColumnBoundsOnLimit
   )
 })
 
