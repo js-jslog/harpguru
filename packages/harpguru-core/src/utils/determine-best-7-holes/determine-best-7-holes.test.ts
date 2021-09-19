9ed361ac56ed5e9e81ba8709aaf1494f64c8a43c
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-19 10:40:05 +0000

    Add test for shifting best 7 holes down
    And realise in the process that the first set of functionality is not
    quite right, and still isn't. I will need to make some more edge case
    tests here to make sure it's tight.

--- packages/harpguru-core/src/utils/determine-best-7-holes/determine-best-7-holes.test.ts
+++ packages/harpguru-core/src/utils/determine-best-7-holes/determine-best-7-holes.test.ts
@@ -7,3 +7,12 @@ test('best 7 holes is the same as input if end bound is less than harp length', 
     inputColumnBounds
   )
 })
+
+test('best 7 holes is shifted down one when the end bound is past the harp length', () => {
+  const harpLength = 10
+  const inputColumnBounds = [4, 10] as const
+  const expectedColumnBounds = [3, 9] as const
+  expect(determineBest7Holes(harpLength, inputColumnBounds)).toStrictEqual(
+    expectedColumnBounds
+  )
+})
--- packages/harpguru-core/src/utils/determine-best-7-holes/determine-best-7-holes.ts
+++ packages/harpguru-core/src/utils/determine-best-7-holes/determine-best-7-holes.ts
@@ -2,7 +2,10 @@ export const determineBest7Holes = (
   harpLength: number,
   inputColumnBounds: readonly [number, number]
 ): readonly [number, number] => {
-  const [, endColumn] = inputColumnBounds
-  if (endColumn < harpLength + 1) return inputColumnBounds
+  const [startColumn, endColumn] = inputColumnBounds
+  if (endColumn < harpLength) return inputColumnBounds
+  if (endColumn === harpLength) {
+    return [startColumn - 1, endColumn - 1]
+  }
   throw Error('temporary error')
 }
