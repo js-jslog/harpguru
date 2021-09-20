4c3c099e76e38ddf443fa45d9f9473f8cbc62c0c
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-20 21:32:20 +0000

    Use saga reducer in harpstrata update
    Similar to previous commits. I've also refactored a little bit to
    separate out the reducer in keeping with the pattern which is starting
    to be important to my understanding of what's going on.

--- packages/harpguru-core/src/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
+++ packages/harpguru-core/src/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
@@ -1,40 +1,10 @@
 import { useDispatch } from 'reactn'
 import { unstable_batchedUpdates } from 'react-native'
-import type { HarpStrataProps } from 'harpstrata'
-import { getHarpStrata } from 'harpstrata'
 
-import { batchToggleDegreeIds } from '../../utils'
-import type { GlobalState } from '../../types'
+import { reduceForFlushedToggles } from './reduce-for-flushed-toggles'
 
 export const useFlushBufferedActivityToggles = (): (() => void) => {
-  const updateActiveHarpStrata = useDispatch((global: GlobalState) => {
-    const { activeHarpStrata, bufferedActivityToggles } = global
-
-    if (bufferedActivityToggles.length === 0) return
-
-    const {
-      apparatus: { tuningId, valvingId },
-      pozitionId,
-      harpKeyId,
-      activeDegreeIds,
-    } = activeHarpStrata
-    const newActiveDegreeIds = batchToggleDegreeIds(
-      activeDegreeIds,
-      bufferedActivityToggles
-    )
-    const newHarpStrataProps: HarpStrataProps = {
-      tuningId,
-      valvingId,
-      pozitionId,
-      harpKeyId,
-      activeIds: newActiveDegreeIds,
-    }
-
-    return {
-      activeHarpStrata: getHarpStrata(newHarpStrataProps),
-      bufferedActivityToggles: [],
-    }
-  })
+  const updateActiveHarpStrata = useDispatch(reduceForFlushedToggles)
 
   // The dispatch seems to function just
   // fine without the batch wrapper, except
