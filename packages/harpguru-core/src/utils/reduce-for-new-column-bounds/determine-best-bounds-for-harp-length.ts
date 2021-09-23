02366643de1c70a42cbe17d16addec695bb54ca5
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-23 21:45:52 +0000

    Remove unnecessary conditional
    The final conditional would always evaluate as true based if the
    conditions above it were false, so there is no need for the condition.
    
    I think the condition perhaps made it a little more explicit what was
    happening, but I didn't like the inaccessible error pattern. It seems
    like both require inspection to detect what's going on and then after
    that inspection the error is wasteful rather than informative.

--- packages/harpguru-core/src/utils/reduce-for-new-column-bounds/determine-best-bounds-for-harp-length.ts
+++ packages/harpguru-core/src/utils/reduce-for-new-column-bounds/determine-best-bounds-for-harp-length.ts
@@ -6,9 +6,6 @@ export const determineBestBoundsForHarpLength = (
   const boundsLength = endColumn - startColumn + 1
   if (harpLength <= boundsLength) return [0, boundsLength - 1] as const
   if (endColumn < harpLength) return inputColumnBounds
-  if (endColumn >= harpLength) {
-    const difference = endColumn - harpLength + 1
-    return [startColumn - difference, endColumn - difference]
-  }
-  throw Error('temporary error')
+  const difference = endColumn - harpLength + 1
+  return [startColumn - difference, endColumn - difference]
 }
