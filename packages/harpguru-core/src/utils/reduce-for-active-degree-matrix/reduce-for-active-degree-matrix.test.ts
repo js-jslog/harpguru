99c348da9e835ce94c16f6af83c969449239b708
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-26 19:49:06 +0000

    Add naive tests for naive existing functionality
    The naivety here is that the reducer needs to return an object which
    matches a `Object.is` evaluation if there is no update to be made.
    Currently that will not be the case, as we'll see in the next commit's
    enhancement.

---
+++ packages/harpguru-core/src/utils/reduce-for-active-degree-matrix/reduce-for-active-degree-matrix.test.ts
@@ -0,0 +1,81 @@
+import type { Dispatch } from 'reactn/default'
+import { getDegree, DegreeIds, HarpFaceMatrix } from 'harpparts'
+import type { Degree } from 'harpparts'
+
+import type { GlobalState } from '../../types'
+
+import { reduceForActiveDegreeMatrix } from './reduce-for-active-degree-matrix'
+
+test('the original degreeMatrix gets returned from the reducer when new matches old', () => {
+  const originalDegreeMatrix = [
+    [
+      getDegree(DegreeIds.Root),
+      getDegree(DegreeIds.Second),
+      getDegree(DegreeIds.Third),
+    ],
+    [
+      getDegree(DegreeIds.Fourth),
+      getDegree(DegreeIds.Fifth),
+      getDegree(DegreeIds.Sixth),
+    ],
+  ] as HarpFaceMatrix<Degree>
+  const newDegreeMatrix = [
+    [
+      getDegree(DegreeIds.Root),
+      getDegree(DegreeIds.Second),
+      getDegree(DegreeIds.Third),
+    ],
+    [
+      getDegree(DegreeIds.Fourth),
+      getDegree(DegreeIds.Fifth),
+      getDegree(DegreeIds.Sixth),
+    ],
+  ] as HarpFaceMatrix<Degree>
+  const global = {
+    activeDegreeMatrix: originalDegreeMatrix,
+  } as GlobalState
+  const unusedDispatcher = (jest.fn() as unknown) as Dispatch
+
+  const actualResult = reduceForActiveDegreeMatrix(
+    global,
+    unusedDispatcher,
+    newDegreeMatrix
+  )
+  const expectedResult = { activeDegreeMatrix: originalDegreeMatrix }
+  expect(actualResult).toStrictEqual(expectedResult)
+})
+
+test('a new degreeMatrix gets returned from the reducer', () => {
+  const originalDegreeMatrix = [
+    [
+      getDegree(DegreeIds.Root),
+      getDegree(DegreeIds.Second),
+      getDegree(DegreeIds.Third),
+    ],
+    [
+      getDegree(DegreeIds.Fourth),
+      getDegree(DegreeIds.Fifth),
+      getDegree(DegreeIds.Sixth),
+    ],
+  ] as HarpFaceMatrix<Degree>
+  const newDegreeMatrix = [
+    [undefined, getDegree(DegreeIds.Second), getDegree(DegreeIds.Third)],
+    [
+      getDegree(DegreeIds.Fourth),
+      getDegree(DegreeIds.Fifth),
+      getDegree(DegreeIds.Sixth),
+    ],
+  ] as HarpFaceMatrix<Degree>
+  const global = {
+    activeDegreeMatrix: originalDegreeMatrix,
+  } as GlobalState
+  const unusedDispatcher = (jest.fn() as unknown) as Dispatch
+
+  const actualResult = reduceForActiveDegreeMatrix(
+    global,
+    unusedDispatcher,
+    newDegreeMatrix
+  )
+  const expectedResult = { activeDegreeMatrix: newDegreeMatrix }
+  expect(actualResult).toStrictEqual(expectedResult)
+})
