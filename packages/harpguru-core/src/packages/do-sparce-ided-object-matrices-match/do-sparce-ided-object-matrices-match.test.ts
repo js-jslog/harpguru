a53234d6b9cf45e699ea773c6775afefb6eafadf
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-26 19:07:50 +0000

    Add unit tests for existing matrix comparison functionality

--- packages/harpguru-core/src/packages/do-sparce-ided-object-matrices-match/do-sparce-ided-object-matrices-match.test.ts
+++ packages/harpguru-core/src/packages/do-sparce-ided-object-matrices-match/do-sparce-ided-object-matrices-match.test.ts
@@ -2,6 +2,38 @@ import { getPitch, PitchIds } from 'harpparts'
 
 import { doSparceIdedObjectMatricesMatch } from './do-sparce-ided-object-matrices-match'
 
+test('single item matched matrix returns true', () => {
+  const pitchMatrix1 = [[getPitch(PitchIds.A)]]
+  const pitchMatrix2 = [[getPitch(PitchIds.A)]]
+
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix1, pitchMatrix2)
+  ).toBeTruthy()
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix2, pitchMatrix1)
+  ).toBeTruthy()
+})
+
+test('mixed matching matrices return true', () => {
+  const pitchMatrix1 = [
+    [getPitch(PitchIds.A), getPitch(PitchIds.B), getPitch(PitchIds.C)],
+    [getPitch(PitchIds.D), undefined, getPitch(PitchIds.E)],
+    [getPitch(PitchIds.F), getPitch(PitchIds.G), undefined],
+  ]
+  const pitchMatrix2 = [
+    [getPitch(PitchIds.A), getPitch(PitchIds.B), getPitch(PitchIds.C)],
+    [getPitch(PitchIds.D), undefined, getPitch(PitchIds.E)],
+    [getPitch(PitchIds.F), getPitch(PitchIds.G), undefined],
+  ]
+
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix1, pitchMatrix2)
+  ).toBeTruthy()
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix2, pitchMatrix1)
+  ).toBeTruthy()
+})
+
 test('single item mismatched matrix returns false', () => {
   const pitchMatrix1 = [[getPitch(PitchIds.Ab)]]
   const pitchMatrix2 = [[getPitch(PitchIds.A)]]
@@ -11,11 +43,39 @@ test('single item mismatched matrix returns false', () => {
   ).toBeFalsy()
 })
 
-test('single item matched matrix returns true', () => {
-  const pitchMatrix1 = [[getPitch(PitchIds.A)]]
-  const pitchMatrix2 = [[getPitch(PitchIds.A)]]
+test('mixed unmatched matrices return false', () => {
+  const pitchMatrix1 = [
+    [getPitch(PitchIds.A), getPitch(PitchIds.B), getPitch(PitchIds.C)],
+    [getPitch(PitchIds.D), undefined, getPitch(PitchIds.E)],
+    [getPitch(PitchIds.F), getPitch(PitchIds.G), undefined],
+  ]
+  const pitchMatrix2 = [
+    [getPitch(PitchIds.C), getPitch(PitchIds.B), getPitch(PitchIds.C)],
+    [getPitch(PitchIds.D), undefined, getPitch(PitchIds.E)],
+    [getPitch(PitchIds.F), getPitch(PitchIds.G), undefined],
+  ]
+  const pitchMatrix3 = [
+    [undefined, getPitch(PitchIds.B), getPitch(PitchIds.C)],
+    [getPitch(PitchIds.D), undefined, getPitch(PitchIds.E)],
+    [getPitch(PitchIds.F), getPitch(PitchIds.G), undefined],
+  ]
 
   expect(
     doSparceIdedObjectMatricesMatch(pitchMatrix1, pitchMatrix2)
-  ).toBeTruthy()
+  ).toBeFalsy()
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix2, pitchMatrix1)
+  ).toBeFalsy()
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix1, pitchMatrix2)
+  ).toBeFalsy()
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix3, pitchMatrix1)
+  ).toBeFalsy()
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix2, pitchMatrix3)
+  ).toBeFalsy()
+  expect(
+    doSparceIdedObjectMatricesMatch(pitchMatrix3, pitchMatrix2)
+  ).toBeFalsy()
 })
