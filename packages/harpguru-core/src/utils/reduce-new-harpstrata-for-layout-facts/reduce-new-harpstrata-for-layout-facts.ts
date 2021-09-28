671061af3d4cd98925bcdc7d5f762b76032dc993
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-28 21:04:29 +0000

    Utilise reducer inside other reducer to reduce duplication

--- packages/harpguru-core/src/utils/reduce-new-harpstrata-for-layout-facts/reduce-new-harpstrata-for-layout-facts.ts
+++ packages/harpguru-core/src/utils/reduce-new-harpstrata-for-layout-facts/reduce-new-harpstrata-for-layout-facts.ts
@@ -1,10 +1,7 @@
 import type { Dispatch } from 'reactn/default'
 import type { HarpStrata } from 'harpstrata'
 
-// TODO: Sort out this import
-import { determineNextColumnBounds } from '../reduce-for-new-column-bounds/determine-next-column-bounds'
-import { getViewableMatrix } from '../get-viewable-matrix'
-import { determineZoomId } from '../determine-zoom-id'
+import { reduceNewHarpStrataForViewableDegreeMatrix } from '../reduce-new-harpstrata-for-viewable-degree-matrix'
 import { determineMatrixDimensions } from '../determine-matrix-dimensions'
 import type { GlobalState } from '../../types'
 
@@ -20,40 +17,25 @@ export const reduceNewHarpStrataForLayoutFacts = (
 ): Pick<GlobalState, 'layoutFacts'> => {
   const { layoutFacts } = global
   const { harpfaceColumns, harpfaceRows } = layoutFacts
-  // TODO: All of this has been seen before in the
-  // `reduceNewHarpStrataForColumnBounds` reducer so we
-  // need to make this logic shared and then mocked during
-  // the test of this reducer.
-  const { columnBounds } = global
-  const { degreeMatrix: newDegreeMatrix } = newHarpStrata
-  const { columns: columnCount1 } = determineMatrixDimensions(newDegreeMatrix)
-  // TODO: This defaulting should perhaps be done in the
-  // `determineNextColumnBounds` function since it's going
-  // to be useful in multiple locations.
-  const zoomId = determineZoomId(columnBounds)
-  const newColumnBounds = determineNextColumnBounds(
-    columnCount1,
-    columnBounds,
-    zoomId
+  const {
+    viewableDegreeMatrix: newViewableDegreeMatrix,
+  } = reduceNewHarpStrataForViewableDegreeMatrix(
+    global,
+    _dipatch,
+    newHarpStrata
+  )
+  const { columns: columnCount, rows: rowCount } = determineMatrixDimensions(
+    newViewableDegreeMatrix
   )
 
-  const viewableDegreeMatrix = getViewableMatrix(
-    newDegreeMatrix,
-    newColumnBounds
-  )
-  const { length: rowCount } = viewableDegreeMatrix
-  const { columns: columnCount2 } = determineMatrixDimensions(
-    viewableDegreeMatrix
-  )
-
-  if (rowCount === harpfaceRows && columnCount2 === harpfaceColumns)
+  if (rowCount === harpfaceRows && columnCount === harpfaceColumns)
     return {
       layoutFacts,
     }
 
   return {
     layoutFacts: {
-      harpfaceColumns: columnCount2,
+      harpfaceColumns: columnCount,
       harpfaceRows: rowCount,
     },
   }
