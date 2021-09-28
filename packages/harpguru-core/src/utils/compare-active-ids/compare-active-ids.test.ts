0764393311b25ae4c7007aabcb5baa3088625f6c
js-jslog <jhs4jbs@hotmail.co.uk>
2021-09-28 17:09:17 +0000

    Add functionality to activeIds comparison
    I'm not entirely happy with the typing here. I'd like it to accept the
    two specific types as being required symetrically. ie the two params
    must be either ActiveDegreeIds or ActivePitchIds. It doesn't like that
    though, and seeing as how I know that the pitch and degreeids will never
    have the same text this is safe, for now..

--- packages/harpguru-core/src/utils/compare-active-ids/compare-active-ids.test.ts
+++ packages/harpguru-core/src/utils/compare-active-ids/compare-active-ids.test.ts
@@ -11,3 +11,18 @@ test('two identical arrays match', () => {
   expect(compareActiveIds(pitchIds, pitchIds)).toBeTruthy()
   expect(compareActiveIds(degreeIds, degreeIds)).toBeTruthy()
 })
+
+test('two similar arrays match', () => {
+  const emptyIds1 = [] as ActiveIds
+  const emptyIds2 = [] as ActiveIds
+  const pitchIds1 = [PitchIds.A, PitchIds.B]
+  const pitchIds2 = [PitchIds.A, PitchIds.B]
+  const degreeIds1 = [DegreeIds.Root, DegreeIds.Second]
+  const degreeIds2 = [DegreeIds.Root, DegreeIds.Second]
+  expect(compareActiveIds(emptyIds1, emptyIds2)).toBeTruthy()
+  expect(compareActiveIds(pitchIds1, pitchIds2)).toBeTruthy()
+  expect(compareActiveIds(degreeIds1, degreeIds2)).toBeTruthy()
+  expect(compareActiveIds(emptyIds2, emptyIds1)).toBeTruthy()
+  expect(compareActiveIds(pitchIds2, pitchIds1)).toBeTruthy()
+  expect(compareActiveIds(degreeIds2, degreeIds1)).toBeTruthy()
+})
--- packages/harpguru-core/src/utils/compare-active-ids/compare-active-ids.ts
+++ packages/harpguru-core/src/utils/compare-active-ids/compare-active-ids.ts
@@ -1,5 +1,10 @@
-import type { ActiveIds } from 'harpstrata'
+import type { DegreeIds, PitchIds } from 'harpparts'
 
-export const compareActiveIds = (ids1: ActiveIds, ids2: ActiveIds): boolean => {
-  return Object.is(ids1, ids2)
+export const compareActiveIds = (
+  ids1: ReadonlyArray<DegreeIds | PitchIds>,
+  ids2: ReadonlyArray<DegreeIds | PitchIds>
+): boolean => {
+  const match1 = ids1.every((item, index: number) => item === ids2[index])
+  const match2 = ids2.every((item, index) => item === ids1[index])
+  return match1 && match2
 }
