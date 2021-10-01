98417a4880b83eac9baa1590dfbc766cc0cabbe8
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-01 18:46:33 +0000

    Remove unnecessary forced batching
    This was included as a security while experimenting with the
    implications of removing all of the `avtiveHarpStrata` hooks. It looks
    like this was not the reason that we were able to see a decent
    syncronisation of component updates, so I will now remove it.

--- packages/harpguru-core/src/components/new-harpstrata-state-callback/new-harpstrata-state-callback.tsx
+++ packages/harpguru-core/src/components/new-harpstrata-state-callback/new-harpstrata-state-callback.tsx
@@ -1,5 +1,4 @@
 import { useGlobal } from 'reactn'
-import { unstable_batchedUpdates } from 'react-native'
 import React, { useEffect } from 'react'
 
 import { compareLayoutFacts } from '../../utils/compare-layout-facts'
@@ -43,98 +42,96 @@ export const NewHarpStrataStateCallback = (): React.ReactElement => {
     'viewablePitchMatrix'
   )
   useEffect(() => {
-    unstable_batchedUpdates(() => {
-      const {
-        apparatus: { tuningId: nextTuningId },
-      } = activeHarpStrata
-      if (prevTuningId !== nextTuningId) setTuningId(nextTuningId)
+    const {
+      apparatus: { tuningId: nextTuningId },
+    } = activeHarpStrata
+    if (prevTuningId !== nextTuningId) setTuningId(nextTuningId)
 
-      const {
-        apparatus: { valvingId: nextValvingId },
-      } = activeHarpStrata
-      if (prevValvingId !== nextValvingId) setValvingId(nextValvingId)
+    const {
+      apparatus: { valvingId: nextValvingId },
+    } = activeHarpStrata
+    if (prevValvingId !== nextValvingId) setValvingId(nextValvingId)
 
-      const {
-        apparatus: { interactionMatrix: nextActiveInteractionMatrix },
-      } = activeHarpStrata
-      if (
-        !doSparceIdedObjectMatricesMatch(
-          prevActiveInteractionMatrix,
-          nextActiveInteractionMatrix
-        )
+    const {
+      apparatus: { interactionMatrix: nextActiveInteractionMatrix },
+    } = activeHarpStrata
+    if (
+      !doSparceIdedObjectMatricesMatch(
+        prevActiveInteractionMatrix,
+        nextActiveInteractionMatrix
       )
-        setActiveInteractionMatrix(nextActiveInteractionMatrix)
+    )
+      setActiveInteractionMatrix(nextActiveInteractionMatrix)
 
-      const { degreeMatrix: nextActiveDegreeMatrix } = activeHarpStrata
-      if (
-        !doSparceIdedObjectMatricesMatch(
-          prevActiveDegreeMatrix,
-          nextActiveDegreeMatrix
-        )
+    const { degreeMatrix: nextActiveDegreeMatrix } = activeHarpStrata
+    if (
+      !doSparceIdedObjectMatricesMatch(
+        prevActiveDegreeMatrix,
+        nextActiveDegreeMatrix
       )
-        setActiveDegreeMatrix(nextActiveDegreeMatrix)
+    )
+      setActiveDegreeMatrix(nextActiveDegreeMatrix)
 
-      const { pitchMatrix: nextActivePitchMatrix } = activeHarpStrata
-      if (
-        !doSparceIdedObjectMatricesMatch(
-          prevActivePitchMatrix,
-          nextActivePitchMatrix
-        )
+    const { pitchMatrix: nextActivePitchMatrix } = activeHarpStrata
+    if (
+      !doSparceIdedObjectMatricesMatch(
+        prevActivePitchMatrix,
+        nextActivePitchMatrix
       )
-        setActivePitchMatrix(nextActivePitchMatrix)
+    )
+      setActivePitchMatrix(nextActivePitchMatrix)
 
-      const nextActiveDegreeIds = deriveActiveDegreeIds(activeHarpStrata)
-      if (!compareActiveIds(nextActiveDegreeIds, prevActiveDegreeIds))
-        setActiveDegreeIds(nextActiveDegreeIds)
+    const nextActiveDegreeIds = deriveActiveDegreeIds(activeHarpStrata)
+    if (!compareActiveIds(nextActiveDegreeIds, prevActiveDegreeIds))
+      setActiveDegreeIds(nextActiveDegreeIds)
 
-      const { harpKeyId: nextHarpKeyId } = activeHarpStrata
-      if (prevHarpKeyId !== nextHarpKeyId) setHarpKeyId(nextHarpKeyId)
+    const { harpKeyId: nextHarpKeyId } = activeHarpStrata
+    if (prevHarpKeyId !== nextHarpKeyId) setHarpKeyId(nextHarpKeyId)
 
-      const { pozitionId: nextPozitionId } = activeHarpStrata
-      if (prevPozitionId !== nextPozitionId) setPozitionId(nextPozitionId)
+    const { pozitionId: nextPozitionId } = activeHarpStrata
+    if (prevPozitionId !== nextPozitionId) setPozitionId(nextPozitionId)
 
-      const { rootPitchId: nextRootPitchId } = activeHarpStrata
-      if (prevRootPitchId !== nextRootPitchId) setRootPitchId(nextRootPitchId)
+    const { rootPitchId: nextRootPitchId } = activeHarpStrata
+    if (prevRootPitchId !== nextRootPitchId) setRootPitchId(nextRootPitchId)
 
-      const nextActivePitchIds = deriveActivePitchIds(activeHarpStrata)
-      if (!compareActiveIds(nextActivePitchIds, prevActivePitchIds))
-        setActivePitchIds(nextActivePitchIds)
+    const nextActivePitchIds = deriveActivePitchIds(activeHarpStrata)
+    if (!compareActiveIds(nextActivePitchIds, prevActivePitchIds))
+      setActivePitchIds(nextActivePitchIds)
 
-      const nextColumnBounds = deriveColumnBounds(
-        activeHarpStrata,
-        prevColumnBounds
+    const nextColumnBounds = deriveColumnBounds(
+      activeHarpStrata,
+      prevColumnBounds
+    )
+    if (!compareColumnBounds(prevColumnBounds, nextColumnBounds))
+      setColumnBounds(nextColumnBounds)
+
+    const nextViewableDegreeMatrix = deriveViewableDegreeMatrix(
+      nextActiveDegreeMatrix,
+      nextColumnBounds
+    )
+    if (
+      !doSparceIdedObjectMatricesMatch(
+        prevViewableDegreeMatrix,
+        nextViewableDegreeMatrix
       )
-      if (!compareColumnBounds(prevColumnBounds, nextColumnBounds))
-        setColumnBounds(nextColumnBounds)
+    )
+      setViewableDegreeMatrix(nextViewableDegreeMatrix)
 
-      const nextViewableDegreeMatrix = deriveViewableDegreeMatrix(
-        nextActiveDegreeMatrix,
-        nextColumnBounds
+    const nextViewablePitchMatrix = deriveViewablePitchMatrix(
+      nextActivePitchMatrix,
+      nextColumnBounds
+    )
+    if (
+      !doSparceIdedObjectMatricesMatch(
+        prevViewablePitchMatrix,
+        nextViewablePitchMatrix
       )
-      if (
-        !doSparceIdedObjectMatricesMatch(
-          prevViewableDegreeMatrix,
-          nextViewableDegreeMatrix
-        )
-      )
-        setViewableDegreeMatrix(nextViewableDegreeMatrix)
+    )
+      setViewablePitchMatrix(nextViewablePitchMatrix)
 
-      const nextViewablePitchMatrix = deriveViewablePitchMatrix(
-        nextActivePitchMatrix,
-        nextColumnBounds
-      )
-      if (
-        !doSparceIdedObjectMatricesMatch(
-          prevViewablePitchMatrix,
-          nextViewablePitchMatrix
-        )
-      )
-        setViewablePitchMatrix(nextViewablePitchMatrix)
-
-      const nextLayoutFacts = deriveLayoutFacts(nextViewableDegreeMatrix)
-      if (!compareLayoutFacts(prevLayoutFacts, nextLayoutFacts))
-        setLayoutFacts(nextLayoutFacts)
-    })
+    const nextLayoutFacts = deriveLayoutFacts(nextViewableDegreeMatrix)
+    if (!compareLayoutFacts(prevLayoutFacts, nextLayoutFacts))
+      setLayoutFacts(nextLayoutFacts)
   }, [activeHarpStrata])
 
   return <></>
