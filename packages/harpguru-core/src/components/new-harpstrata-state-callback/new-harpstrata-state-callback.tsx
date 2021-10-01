fb5cf984b25df54f0299e4cbdbf02648a927bd6e
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-01 12:40:28 +0000

    Move all derivative state updates in to shared useEffect
    This may not be necessary, but it is the most likely setup to produce
    something like simultaneous updates in all the states which are being
    updated.
    
    I am experimenting with using the `layoutFacts` inside the `useSizes`
    hook, but the results are not satisying. There is a delay between
    certain elements being rerendered and others being rerendered. I think
    this might be to do with the fact that most components are still
    actually being driven in to rerender by the `activeHarpStrata` which is
    happening first, and then others are only being driven afterwards by the
    `useSizes` hook.
    
    This is an all or nothing experiment, it seems. I'll have to remove the
    use of `activeHarpStrata` from everything to see whether my theory is
    correct. If it is then I can consider moving back to the use of
    separate `useEffect` hooks for every state prop.

--- packages/harpguru-core/src/components/new-harpstrata-state-callback/new-harpstrata-state-callback.tsx
+++ packages/harpguru-core/src/components/new-harpstrata-state-callback/new-harpstrata-state-callback.tsx
@@ -1,31 +1,103 @@
-import React from 'react'
+import { useGlobal } from 'reactn'
+import { unstable_batchedUpdates } from 'react-native'
+import React, { useEffect } from 'react'
+
+import { compareLayoutFacts } from '../../utils/compare-layout-facts'
+import { compareActiveIds } from '../../utils/compare-active-ids'
+import { compareColumnBounds } from '../../utils'
+import { doSparceIdedObjectMatricesMatch } from '../../packages/do-sparce-ided-object-matrices-match'
 
 import {
-  useUpdatePozitionId,
-  useUpdateRootPitchId,
-  useUpdateHarpKeyId,
-  useDeriveActiveDegreeIds,
-  useDeriveActivePitchIds,
-  useDeriveDegreeMatrix,
-  useDerivePitchMatrix,
-  useDeriveViewableDegreeMatrix,
-  useDeriveViewablePitchMatrix,
-  useDeriveColumnBounds,
-  useDeriveLayoutFacts,
-} from './hooks'
+  deriveActiveDegreeIds,
+  deriveActivePitchIds,
+  deriveViewableDegreeMatrix,
+  deriveViewablePitchMatrix,
+  deriveColumnBounds,
+  deriveLayoutFacts,
+} from './utils'
 
 export const NewHarpStrataStateCallback = (): React.ReactElement => {
-  useUpdatePozitionId()
-  useUpdateRootPitchId()
-  useUpdateHarpKeyId()
-  useDeriveActiveDegreeIds()
-  useDeriveActivePitchIds()
-  useDeriveDegreeMatrix()
-  useDerivePitchMatrix()
-  useDeriveViewableDegreeMatrix()
-  useDeriveViewablePitchMatrix()
-  useDeriveColumnBounds()
-  useDeriveLayoutFacts()
+  const [activeHarpStrata] = useGlobal('activeHarpStrata')
+  const [prevActiveDegreeIds, setActiveDegreeIds] = useGlobal('activeDegreeIds')
+  const [prevActivePitchIds, setActivePitchIds] = useGlobal('activePitchIds')
+  const [prevColumnBounds, setColumnBounds] = useGlobal('columnBounds')
+  const [prevActiveDegreeMatrix, setActiveDegreeMatrix] = useGlobal(
+    'activeDegreeMatrix'
+  )
+  const [prevActivePitchMatrix, setActivePitchMatrix] = useGlobal(
+    'activePitchMatrix'
+  )
+  const [prevLayoutFacts, setLayoutFacts] = useGlobal('layoutFacts')
+  const [prevViewableDegreeMatrix, setViewableDegreeMatrix] = useGlobal(
+    'viewableDegreeMatrix'
+  )
+  const [prevViewablePitchMatrix, setViewablePitchMatrix] = useGlobal(
+    'viewablePitchMatrix'
+  )
+  useEffect(() => {
+    unstable_batchedUpdates(() => {
+      const nextActiveDegreeIds = deriveActiveDegreeIds(activeHarpStrata)
+      if (!compareActiveIds(nextActiveDegreeIds, prevActiveDegreeIds))
+        setActiveDegreeIds(nextActiveDegreeIds)
+
+      const nextActivePitchIds = deriveActivePitchIds(activeHarpStrata)
+      if (!compareActiveIds(nextActivePitchIds, prevActivePitchIds))
+        setActivePitchIds(nextActivePitchIds)
+
+      const nextColumnBounds = deriveColumnBounds(
+        activeHarpStrata,
+        prevColumnBounds
+      )
+      if (!compareColumnBounds(prevColumnBounds, nextColumnBounds))
+        setColumnBounds(nextColumnBounds)
+
+      const { degreeMatrix: activeDegreeMatrix } = activeHarpStrata
+      if (
+        !doSparceIdedObjectMatricesMatch(
+          prevActiveDegreeMatrix,
+          activeDegreeMatrix
+        )
+      )
+        setActiveDegreeMatrix(activeDegreeMatrix)
+
+      const { pitchMatrix: activePitchMatrix } = activeHarpStrata
+      if (
+        !doSparceIdedObjectMatricesMatch(
+          prevActivePitchMatrix,
+          activePitchMatrix
+        )
+      )
+        setActivePitchMatrix(activePitchMatrix)
+
+      const nextViewableDegreeMatrix = deriveViewableDegreeMatrix(
+        activeDegreeMatrix,
+        nextColumnBounds
+      )
+      if (
+        !doSparceIdedObjectMatricesMatch(
+          prevViewableDegreeMatrix,
+          nextViewableDegreeMatrix
+        )
+      )
+        setViewableDegreeMatrix(nextViewableDegreeMatrix)
+
+      const nextViewablePitchMatrix = deriveViewablePitchMatrix(
+        activePitchMatrix,
+        nextColumnBounds
+      )
+      if (
+        !doSparceIdedObjectMatricesMatch(
+          prevViewablePitchMatrix,
+          nextViewablePitchMatrix
+        )
+      )
+        setViewablePitchMatrix(nextViewablePitchMatrix)
+
+      const nextLayoutFacts = deriveLayoutFacts(nextViewableDegreeMatrix)
+      if (!compareLayoutFacts(prevLayoutFacts, nextLayoutFacts))
+        setLayoutFacts(nextLayoutFacts)
+    })
+  }, [activeHarpStrata])
 
   return <></>
 }
