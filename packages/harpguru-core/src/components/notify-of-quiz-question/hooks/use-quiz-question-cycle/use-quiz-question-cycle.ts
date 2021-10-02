206000d459b8083f54868f9a272bf686ccc5fffc
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-02 21:25:54 +0000

    Simplify the quiz mode global state updates
    Leveraging the newHarpStrata callback means we can circumvent the buffer
    and update the activeHarpStrata directly, resulting in faster rendering
    and simplified logic.

--- packages/harpguru-core/src/components/notify-of-quiz-question/hooks/use-quiz-question-cycle/use-quiz-question-cycle.ts
+++ packages/harpguru-core/src/components/notify-of-quiz-question/hooks/use-quiz-question-cycle/use-quiz-question-cycle.ts
@@ -1,6 +1,6 @@
-import { useGlobal, useDispatch } from 'reactn'
-import { unstable_batchedUpdates } from 'react-native'
+import { useGlobal } from 'reactn'
 import { useState, useEffect } from 'react'
+import { getHarpStrata } from 'harpstrata'
 import { DegreeIds, isPitchId } from 'harpparts'
 import type { PitchIds } from 'harpparts'
 
@@ -9,8 +9,7 @@ import {
   hasToggledIncorrectCell,
   getCounterpartDegreeId,
 } from '../../utils'
-import { ExperienceModes, FlushChannels, GlobalState } from '../../../../types'
-import { useFlushBufferedActivityToggles } from '../../../../hooks'
+import { ExperienceModes, FlushChannels } from '../../../../types'
 
 // Ask - display the question
 // Listen - allow user to input answers
@@ -27,94 +26,63 @@ export const useQuizQuestionCycle = (
   isScreenFree: boolean
 ): [DegreeIds | PitchIds, boolean] => {
   const [activeExperienceMode] = useGlobal('activeExperienceMode')
-  const [activeHarpStrata] = useGlobal('activeHarpStrata')
+  const [activeHarpStrata, setActiveHarpStrata] = useGlobal('activeHarpStrata')
   const [activeDisplayMode] = useGlobal('activeDisplayMode')
   const [bufferedActivityToggles] = useGlobal('bufferedActivityToggles')
   const [flushChannel, setFlushChannel] = useGlobal('flushChannel')
   const [activeQuizDegrees] = useGlobal('activeQuizDegrees')
-  const flushBufferedActivityToggles = useFlushBufferedActivityToggles()
 
   const [quizState, setQuizState] = useState<QuizStates>(QuizStates.Wait)
   const [quizQuestion, setQuizQuestion] = useState<DegreeIds | PitchIds>(
     getNextQuizQuestion(DegreeIds.Root, activeQuizDegrees, activeDisplayMode)
   )
 
-  // This function is involved in a quirk which requires some
-  // understanding. The reason that the returned value needs
-  // to include not only the degrees which are active on the
-  // activeHarpStrata but also the ones already in the toggle
-  // buffer is that removing toggles from the buffer without
-  // properly flushing them will result in isolated active
-  // cells (cells highlighted without their siblings being
-  // soon to be highlighted too). We *must* continue with
-  // the flushing of the buffered cells.
-  //
-  // The above scenario most commonly happens after the answer
-  // has been given and before the next question is asked and
-  // a cell is toggled. We *could* flush that toggle and only
-  // then buffer the clearing toggles, then flush those, but
-  // we need to force multiple renders for that to complete
-  // successfully, otherwise the highlighted cell will just
-  // receive the same 'inactive' props it already had and the
-  // memo will leave it unrendered. Forcing multiple renders
-  // feels janky, so the alternative is to deal with one render
-  // during the transition back to Ask, and another render
-  // during the transition to Listen. That's why you'll
-  // see another call to the clear function there. The user
-  // doesn't have a chance to buffer another toggle during
-  // that period in the current design because there is a
-  // press-blocking flash notification, so the next clear
-  // function will be guaranteed to clear the cells flushed
-  // during the Ask state if relevant.
-  const bufferClearingToggles = useDispatch((global: GlobalState):
-    | Pick<GlobalState, 'bufferedActivityToggles'>
-    | undefined => {
-    const { activeHarpStrata, bufferedActivityToggles } = global
-    if (activeHarpStrata.activeDegreeIds.length === 0) return
-    return {
-      bufferedActivityToggles: [
-        ...bufferedActivityToggles,
-        ...activeHarpStrata.activeDegreeIds,
-      ],
-    }
-  })
-
   const resetHarpFace = () => {
-    bufferClearingToggles()
-    flushBufferedActivityToggles()
+    const harpStrataProps = {
+      tuningId: activeHarpStrata.apparatus.tuningId,
+      valvingId: activeHarpStrata.apparatus.valvingId,
+      harpKeyId: activeHarpStrata.harpKeyId,
+      pozitionId: activeHarpStrata.pozitionId,
+      activeIds: [],
+    }
+    const resetHarpStrata = getHarpStrata(harpStrataProps)
+    setActiveHarpStrata(resetHarpStrata)
   }
 
-  const bufferCorrectAnswer = useDispatch(
-    (global: GlobalState): Pick<GlobalState, 'bufferedActivityToggles'> => {
-      const { activeHarpStrata } = global
-      const { harpKeyId, pozitionId } = activeHarpStrata
+  const batchAnswerActions = () => {
+    const {
+      apparatus: { tuningId, valvingId },
+      harpKeyId,
+      pozitionId,
+    } = activeHarpStrata
+    const answerDegreeIds = (() => {
       if (isPitchId(quizQuestion)) {
         const counterpartDegreeId = getCounterpartDegreeId({
           pitchId: quizQuestion,
           harpKeyId: harpKeyId,
           pozitionId: pozitionId,
         })
-        return {
-          bufferedActivityToggles: [
-            ...bufferedActivityToggles,
-            counterpartDegreeId,
-          ],
-        }
+        const answerDegreeIds = [
+          ...bufferedActivityToggles,
+          counterpartDegreeId,
+        ]
+        return answerDegreeIds
       }
-      return {
-        bufferedActivityToggles: [...bufferedActivityToggles, quizQuestion],
-      }
+      const answerDegreeIds = [...bufferedActivityToggles, quizQuestion]
+      return answerDegreeIds
+    })()
+    const harpStrataProps = {
+      tuningId,
+      valvingId,
+      harpKeyId,
+      pozitionId,
+      activeIds: answerDegreeIds,
     }
-  )
-
-  const batchAnswerActions = () => {
-    unstable_batchedUpdates(() => {
-      bufferCorrectAnswer()
-      flushBufferedActivityToggles()
-      setQuizQuestion(
-        getNextQuizQuestion(quizQuestion, activeQuizDegrees, activeDisplayMode)
-      )
-    })
+    const answerHarpStrata = getHarpStrata(harpStrataProps)
+    setActiveHarpStrata(answerHarpStrata)
+    setQuizQuestion(
+      getNextQuizQuestion(quizQuestion, activeQuizDegrees, activeDisplayMode)
+    )
   }
 
   // Start asking questions when the experience mode is set to Quiz
