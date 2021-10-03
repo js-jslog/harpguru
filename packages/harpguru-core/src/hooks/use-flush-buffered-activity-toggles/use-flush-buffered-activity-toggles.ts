2f5659733b121c9bbeeb486a5ea13e511aac9c19
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-03 16:34:33 +0000

    Remove unnecessary force function
    After much testing it seems like this is not necessary anymore. Possibly
    as a result of using a property targetted dispatch now rather than a
    full global one; although I don't have a good theory for why at this
    point.

--- packages/harpguru-core/src/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
+++ packages/harpguru-core/src/hooks/use-flush-buffered-activity-toggles/use-flush-buffered-activity-toggles.ts
@@ -1,5 +1,4 @@
 import { useGlobal, useDispatch } from 'reactn'
-import { unstable_batchedUpdates } from 'react-native'
 
 import { reduceForNewHarpStrataByToggleFlush } from './reduce-for-new-harpstrata-by-toggle-flush'
 
@@ -14,22 +13,7 @@ export const useFlushBufferedActivityToggles = (): (() => void) => {
     'activeHarpStrata'
   )
 
-  // The dispatch seems to function just
-  // fine without the batch wrapper, except
-  // when it's being called from a `setTimeout`
-  // function, which is something we use quite
-  // frequently in this project. I wonder
-  // whether this is something to do with a
-  // failure to operate properly from async
-  // contexts. This could use investigation.
-  // Possibly if I were actually returning
-  // a Promise here rather than a function
-  // which runs the Promise then things would
-  // work better. Lots to consider, but this
-  // at least appears to work well.
   return () => {
-    unstable_batchedUpdates(() => {
-      updateActiveHarpStrata()
-    })
+    updateActiveHarpStrata()
   }
 }
