52b8aa822f47225e041b5ac8a3fb5d04bcc38c27
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-03 19:25:07 +0000

    Remove unnecessary export
    This function is only required locally to this folder so the further
    export from the folder is unnecessary.

--- packages/harpguru-core/src/utils/index.ts
+++ packages/harpguru-core/src/utils/index.ts
@@ -1,4 +1,3 @@
-export { batchToggleDegreeIds } from './batch-toggle-degree-ids'
 export { getRenderableToneTuples } from './get-renderable-tone-tuples'
 export { getInputRange } from './get-input-range'
 export { getOutputRange } from './get-output-range'
