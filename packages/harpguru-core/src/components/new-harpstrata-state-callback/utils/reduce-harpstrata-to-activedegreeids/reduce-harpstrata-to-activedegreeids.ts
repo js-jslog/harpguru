dd914e41314f74fa95f070ea901fdf34938a7c21
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-07 17:39:59 +0000

    Remove nonsense TODO

--- packages/harpguru-core/src/components/new-harpstrata-state-callback/utils/reduce-harpstrata-to-activedegreeids/reduce-harpstrata-to-activedegreeids.ts
+++ packages/harpguru-core/src/components/new-harpstrata-state-callback/utils/reduce-harpstrata-to-activedegreeids/reduce-harpstrata-to-activedegreeids.ts
@@ -1,9 +1,6 @@
 import type { ActiveDegreeIds, HarpStrata } from 'harpstrata'
 
 export const reduceHarpStrataToActiveDegreeIds = (
-  // TODO: Should consider simplifying this so that
-  // only the next degree matrix is passed in rather
-  // than the entire harpstrata
   activeHarpStrata: HarpStrata
 ): ActiveDegreeIds => {
   const { activeDegreeIds: newActiveDegreeIds } = activeHarpStrata
