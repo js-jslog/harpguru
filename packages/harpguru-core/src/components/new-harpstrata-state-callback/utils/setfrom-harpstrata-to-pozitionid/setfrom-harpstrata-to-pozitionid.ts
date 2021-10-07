fc2701f987575eacf1f41516a79e4aebf526f8f4
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-07 15:36:43 +0000

    Remove unused file & rehome component util
    The isMatchPozitionIds util isn't really required and probably belongs
    being defined in the harpparts project, but let's just keep it alive
    here for now until we've got time to do that.
    
    I've reduced the specificity of the equality check too since it's not
    important that the onjects are identical, it's just important that they
    match, even if right now the two are the same thing.

--- packages/harpguru-core/src/components/new-harpstrata-state-callback/utils/setfrom-harpstrata-to-pozitionid/setfrom-harpstrata-to-pozitionid.ts
+++ packages/harpguru-core/src/components/new-harpstrata-state-callback/utils/setfrom-harpstrata-to-pozitionid/setfrom-harpstrata-to-pozitionid.ts
@@ -1,12 +1,15 @@
 import type { HarpStrata } from 'harpstrata'
 import type { PozitionIds } from 'harpparts'
 
+import { isMatchPozitionIds } from '../ismatch-pozitionids'
+
 export const setFromHarpStrataToPozitionId = (
   newHarpStrata: HarpStrata,
   prevPozitionId: PozitionIds,
   setPozitionId: (arg0: PozitionIds) => void
 ): PozitionIds => {
   const { pozitionId: nextPozitionId } = newHarpStrata
-  if (prevPozitionId !== nextPozitionId) setPozitionId(nextPozitionId)
+  if (!isMatchPozitionIds(prevPozitionId, nextPozitionId))
+    setPozitionId(nextPozitionId)
   return nextPozitionId
 }
