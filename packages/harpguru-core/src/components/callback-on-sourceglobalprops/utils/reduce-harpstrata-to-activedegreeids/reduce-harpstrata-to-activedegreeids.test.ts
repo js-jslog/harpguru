6265effb2630ce9ec5d14fcf7dbbf4a9a5128b93
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-08 14:19:02 +0000

    Add unit tests to reducer and enhance functionality
    It's not strictly necessary for this reducer to perform this check the
    way it's working currently. However it does no harm and it is arguably
    just a feature of reducers which is wise to have incase they are used in
    a dispatch in the future.
    
    Actually a further change should be made to build that future because in
    those situations the first parameter is the same as the return type when
    a dispatch is being used.

--- packages/harpguru-core/src/components/callback-on-sourceglobalprops/utils/reduce-harpstrata-to-activedegreeids/reduce-harpstrata-to-activedegreeids.test.ts
+++ packages/harpguru-core/src/components/callback-on-sourceglobalprops/utils/reduce-harpstrata-to-activedegreeids/reduce-harpstrata-to-activedegreeids.test.ts
@@ -1,10 +1,36 @@
+import { DegreeIds } from 'harpparts'
+
 import { activeCellsHarpStrata } from '../../../../test-resources'
 
 import { reduceHarpStrataToActiveDegreeIds } from './reduce-harpstrata-to-activedegreeids'
 
-test('an activeDegreeIds is returned from a harpstrata', () => {
+test('an activeDegreeIds is returned from a harpstrata if no previuos activeDegreeIds is provided', () => {
   const harpStrata = activeCellsHarpStrata
   expect(reduceHarpStrataToActiveDegreeIds(harpStrata)).toStrictEqual(
     harpStrata.activeDegreeIds
   )
+})
+
+test('the previous activeDegreeIds object is returned if it matches the one on the harpstrata', () => {
+  const harpStrata = activeCellsHarpStrata
+  const prevActiveDegreeIds = [...harpStrata.activeDegreeIds]
+  const nextActiveDegreeIds = reduceHarpStrataToActiveDegreeIds(
+    harpStrata,
+    prevActiveDegreeIds
+  )
+  expect(nextActiveDegreeIds).toBe(prevActiveDegreeIds)
+  expect(nextActiveDegreeIds).not.toBe(harpStrata.activeDegreeIds)
+  expect(nextActiveDegreeIds).toStrictEqual(harpStrata.activeDegreeIds)
+})
+
+test('an activeDegreeIds is returned from a harpstrata if the previous activeDegreeIds is different', () => {
+  const harpStrata = activeCellsHarpStrata
+  const prevActiveDegreeIds = [...harpStrata.activeDegreeIds, DegreeIds.Flat3]
+  const nextActiveDegreeIds = reduceHarpStrataToActiveDegreeIds(
+    harpStrata,
+    prevActiveDegreeIds
+  )
+  expect(nextActiveDegreeIds).not.toBe(prevActiveDegreeIds)
+  expect(nextActiveDegreeIds).not.toStrictEqual(prevActiveDegreeIds)
+  expect(nextActiveDegreeIds).toStrictEqual(harpStrata.activeDegreeIds)
 })
--- packages/harpguru-core/src/components/callback-on-sourceglobalprops/utils/reduce-harpstrata-to-activedegreeids/reduce-harpstrata-to-activedegreeids.ts
+++ packages/harpguru-core/src/components/callback-on-sourceglobalprops/utils/reduce-harpstrata-to-activedegreeids/reduce-harpstrata-to-activedegreeids.ts
@@ -1,9 +1,15 @@
 import type { HarpStrata } from 'harpstrata'
 import type { DegreeIds } from 'harpparts'
 
+import { isMatchActiveIds } from '../../../../utils'
+
 export const reduceHarpStrataToActiveDegreeIds = (
-  harpStrata: HarpStrata
+  harpStrata: HarpStrata,
+  prevActiveDegreeIds?: ReadonlyArray<DegreeIds>
 ): ReadonlyArray<DegreeIds> => {
-  const { activeDegreeIds } = harpStrata
-  return activeDegreeIds
+  const { activeDegreeIds: nextActiveDegreeIds } = harpStrata
+  if (!prevActiveDegreeIds) return nextActiveDegreeIds
+  if (isMatchActiveIds(prevActiveDegreeIds, nextActiveDegreeIds))
+    return prevActiveDegreeIds
+  return nextActiveDegreeIds
 }
