60a5ba35857c22bf4d01329c470d0c69917a8d4c
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-28 21:47:21 +0000

    Add "slightly" functional slider
    The slider is not perfect, but it does show that something functional is
    possible. I'm not sold on how usable it is though..

--- packages/harpguru-core/src/components/zoom-slide-vertical/zoom-slide-vertical.tsx
+++ packages/harpguru-core/src/components/zoom-slide-vertical/zoom-slide-vertical.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, View } from 'react-native'
+import { useGlobal } from 'reactn'
+import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
+import MultiSlider from '@ptomasroos/react-native-multi-slider'
 
 import { useSizes } from '../../hooks'
 
 export const ZoomSlideVertical = (): React.ReactElement => {
   const { dynamicSizes } = useSizes()
+  const [, setSourceColumnBounds] = useGlobal('sourceColumnBounds')
+  const [columnBounds] = useGlobal('columnBounds')
   const styles = StyleSheet.create({
     componentWrapper: {
       ...StyleSheet.absoluteFillObject,
@@ -20,18 +24,56 @@ export const ZoomSlideVertical = (): React.ReactElement => {
     sliderWrapper: {
       backgroundColor: 'yellow',
       flex: 5,
+      justifyContent: 'center',
     },
     bottomLabelWrapper: {
-      backgroundColor: 'blue',
+      backgroundColor: 'red',
       flex: 1,
+    },
+    track: {
+      backgroundColor: 'black',
+    },
+    marker: {
+      backgroundColor: 'black',
+    },
+    selectedd: {
+      backgroundColor: 'black',
     },
   })
 
+  if (columnBounds === 'FIT') return <View style={styles.componentWrapper} />
+
+  const [start, end] = columnBounds
+  const diff = end - start
+
+  const onValuesChangeFinish = (values: number[]) => {
+    const startHole = values[0] - 1
+    const endHole = startHole + diff
+    setSourceColumnBounds([startHole, endHole])
+  }
+
   return (
     <View style={styles.componentWrapper}>
-      <View style={styles.topLabelWrapper}></View>
-      <View style={styles.sliderWrapper}></View>
-      <View style={styles.bottomLabelWrapper}></View>
+      <View style={styles.topLabelWrapper}>
+        <Text>{columnBounds[0]}</Text>
+      </View>
+      <View style={styles.sliderWrapper}>
+        <MultiSlider
+          vertical={true}
+          values={[1]}
+          min={1}
+          max={7}
+          snapped={true}
+          sliderLength={150}
+          onValuesChangeFinish={onValuesChangeFinish}
+          trackStyle={styles.track}
+          markerStyle={styles.marker}
+          selectedStyle={styles.selectedd}
+        />
+      </View>
+      <View style={styles.bottomLabelWrapper}>
+        <Text>{columnBounds[1]}</Text>
+      </View>
     </View>
   )
 }
