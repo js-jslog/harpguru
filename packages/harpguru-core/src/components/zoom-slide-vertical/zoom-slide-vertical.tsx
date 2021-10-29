6ed645a5303c66cc29ef253cddc2bec1c87b0ad7
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-29 23:02:14 +0000

    Update slider display values independent to columnBounds
    We want to feed back to the user what the columnBounds are that they
    are selecting before they release and produce the time consuming
    rerender of the harpface.

--- packages/harpguru-core/src/components/zoom-slide-vertical/zoom-slide-vertical.tsx
+++ packages/harpguru-core/src/components/zoom-slide-vertical/zoom-slide-vertical.tsx
@@ -1,6 +1,6 @@
 import { useGlobal } from 'reactn'
 import { StyleSheet, View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import MultiSlider from '@ptomasroos/react-native-multi-slider'
 
 import { useSizes } from '../../hooks'
@@ -8,7 +8,18 @@ import { useSizes } from '../../hooks'
 export const ZoomSlideVertical = (): React.ReactElement => {
   const { dynamicSizes } = useSizes()
   const [, setSourceColumnBounds] = useGlobal('sourceColumnBounds')
-  const [columnBounds] = useGlobal('columnBounds')
+  const [globalDisplayColumnBounds] = useGlobal('columnBounds')
+  // This local version of columnBounds is required because it needs to be
+  // updated without producing the application wide rerendering which updating
+  // either `columnBounds` or `sourceColumnBounds` would.
+  // In addition, it's necessary to drive this local version of column bounds
+  // by the `columnBounds` rather than the `sourceColumnBounds` because we must
+  // never drive renders by the "source" global variables but we must always
+  // update "source" global variables rather than their non-source counterparts.
+  // See callback-on-sourceglobalprops for details.
+  const [localColumnBounds, setLocalColumnBounds] = useState(
+    globalDisplayColumnBounds
+  )
   const styles = StyleSheet.create({
     componentWrapper: {
       ...StyleSheet.absoluteFillObject,
@@ -41,9 +52,10 @@ export const ZoomSlideVertical = (): React.ReactElement => {
     },
   })
 
-  if (columnBounds === 'FIT') return <View style={styles.componentWrapper} />
+  if (localColumnBounds === 'FIT')
+    return <View style={styles.componentWrapper} />
 
-  const [start, end] = columnBounds
+  const [start, end] = localColumnBounds
   const diff = end - start
 
   const onValuesChangeFinish = (values: number[]) => {
@@ -51,28 +63,34 @@ export const ZoomSlideVertical = (): React.ReactElement => {
     const endHole = startHole + diff
     setSourceColumnBounds([startHole, endHole])
   }
+  const onValuesChange = (values: number[]) => {
+    const startHole = values[0] - 1
+    const endHole = startHole + diff
+    setLocalColumnBounds([startHole, endHole])
+  }
 
   return (
     <View style={styles.componentWrapper}>
       <View style={styles.topLabelWrapper}>
-        <Text>{columnBounds[0]}</Text>
+        <Text>{localColumnBounds[0] + 1}</Text>
       </View>
       <View style={styles.sliderWrapper}>
         <MultiSlider
           vertical={true}
-          values={[1]}
+          values={[localColumnBounds[0] + 1]}
           min={1}
           max={7}
           snapped={true}
           sliderLength={150}
           onValuesChangeFinish={onValuesChangeFinish}
+          onValuesChange={onValuesChange}
           trackStyle={styles.track}
           markerStyle={styles.marker}
           selectedStyle={styles.selectedd}
         />
       </View>
       <View style={styles.bottomLabelWrapper}>
-        <Text>{columnBounds[1]}</Text>
+        <Text>{localColumnBounds[1] + 1}</Text>
       </View>
     </View>
   )
