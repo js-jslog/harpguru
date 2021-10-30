dcc7541791730b23f1674670139542186cf4d29e
js-jslog <jhs4jbs@hotmail.co.uk>
2021-10-30 18:36:30 +0000

    Replace malfunctioning 3rd party slider
    The previous slider would fail to slide back from max value.
    
    I'm not completely in love with this alternative, but at least it works
    for now.

--- packages/harpguru-core/src/components/zoom-slide-vertical/zoom-slide-vertical.tsx
+++ packages/harpguru-core/src/components/zoom-slide-vertical/zoom-slide-vertical.tsx
@@ -1,8 +1,9 @@
+import VerticalSlider from 'rn-vertical-slider'
 import { useGlobal } from 'reactn'
 import { StyleSheet, View, Text } from 'react-native'
 import React, { useState } from 'react'
-import MultiSlider from '@ptomasroos/react-native-multi-slider'
 
+import { getColors } from '../../utils'
 import { doSparceIdedObjectMatricesMatch } from '../../packages/do-sparce-ided-object-matrices-match'
 import { useSizes } from '../../hooks'
 
@@ -36,76 +37,74 @@ const ZoomSlideVerticalVisible = ({
   totalHoles,
 }: ZoomSlideVerticalVisibleProps): React.ReactElement => {
   const { dynamicSizes } = useSizes()
+  const { inertOutline, harpguruPink, pageColor } = getColors()
   const [, setSourceColumnBounds] = useGlobal('sourceColumnBounds')
   const [sliderBounds, setSliderBounds] = useState(columnBounds)
   const styles = StyleSheet.create({
     componentWrapper: {
       ...StyleSheet.absoluteFillObject,
       width: dynamicSizes.zoomSlideWidth,
-      backgroundColor: 'green',
       left: dynamicSizes['9'], // legend width is going to have to become a named variable
       flexDirection: 'column',
     },
-    topLabelWrapper: {
-      backgroundColor: 'red',
+    columnBoundsLabelWrapper: {
+      backgroundColor: inertOutline,
+      justifyContent: 'center',
+      alignItems: 'center',
       flex: 1,
     },
+    columnBoundsLabel: {
+      fontSize: dynamicSizes['9'],
+      color: pageColor,
+    },
     sliderWrapper: {
-      backgroundColor: 'yellow',
       flex: 5,
       justifyContent: 'center',
+      alignItems: 'center',
     },
     bottomLabelWrapper: {
       backgroundColor: 'red',
       flex: 1,
-    },
-    track: {
-      backgroundColor: 'black',
-    },
-    marker: {
-      backgroundColor: 'black',
-    },
-    selectedd: {
-      backgroundColor: 'black',
     },
   })
 
   const [start, end] = sliderBounds
   const diff = end - start
 
-  const onValuesChangeFinish = (values: number[]) => {
-    const startHole = values[0] - 1
+  const onComplete = (values: number) => {
+    const startHole = values - 1
     const endHole = startHole + diff
     setSourceColumnBounds([startHole, endHole])
   }
-  const onValuesChange = (values: number[]) => {
-    const startHole = values[0] - 1
+  const onChange = (value: number) => {
+    const startHole = value - 1
     const endHole = startHole + diff
     setSliderBounds([startHole, endHole])
   }
 
   return (
     <View style={styles.componentWrapper}>
-      <View style={styles.topLabelWrapper}>
-        <Text>{sliderBounds[0] + 1}</Text>
+      <View style={styles.columnBoundsLabelWrapper}>
+        <Text style={styles.columnBoundsLabel}>{sliderBounds[1] + 1}</Text>
       </View>
       <View style={styles.sliderWrapper}>
-        <MultiSlider
-          vertical={true}
-          values={[sliderBounds[0] + 1]}
+        <VerticalSlider
+          value={sliderBounds[0] + 1}
+          disabled={false}
           min={1}
           max={totalHoles - diff}
-          snapped={true}
-          sliderLength={150}
-          onValuesChangeFinish={onValuesChangeFinish}
-          onValuesChange={onValuesChange}
-          trackStyle={styles.track}
-          markerStyle={styles.marker}
-          selectedStyle={styles.selectedd}
+          onChange={onChange}
+          onComplete={onComplete}
+          width={dynamicSizes[6]}
+          height={dynamicSizes.rowHeight * 3}
+          step={1}
+          borderRadius={5}
+          minimumTrackTintColor={harpguruPink}
+          maximumTrackTintColor={inertOutline}
         />
       </View>
-      <View style={styles.bottomLabelWrapper}>
-        <Text>{sliderBounds[1] + 1}</Text>
+      <View style={styles.columnBoundsLabelWrapper}>
+        <Text style={styles.columnBoundsLabel}>{sliderBounds[0] + 1}</Text>
       </View>
     </View>
   )
