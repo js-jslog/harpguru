92487a93f58e1a7e03f12eec45fb46058e85d520
js-jslog <jhs4jbs@hotmail.co.uk>
2021-11-07 21:59:43 +0000

    Refactor fact util out to separate file

---
+++ packages/harpguru-core/src/components/zoom-slide-vertical-visible/utils/get-slide-facts/get-slide-facts.ts
@@ -0,0 +1,26 @@
+import { getWindowDimensions } from '../../../../packages/get-window-dimensions'
+
+type SlideFacts = {
+  readonly trackLength: number
+  readonly slideLength: number
+  readonly slideSpan: number
+  readonly slotCount: number
+}
+export const getSlideFacts = (
+  restrictingColumnBounds: readonly [number, number],
+  totalHoles: number
+): SlideFacts => {
+  const slotCount = totalHoles + 1
+  const { shortEdge: trackLength } = getWindowDimensions()
+  const slotLength = trackLength / slotCount
+  const [startBound, endBound] = restrictingColumnBounds
+  const slideSpan = endBound - startBound
+  const slideLength = slotLength * slideSpan
+
+  return {
+    trackLength,
+    slideLength,
+    slideSpan,
+    slotCount,
+  }
+}
--- packages/harpguru-core/src/components/zoom-slide-vertical-visible/zoom-slide-vertical-visible.tsx
+++ packages/harpguru-core/src/components/zoom-slide-vertical-visible/zoom-slide-vertical-visible.tsx
@@ -6,10 +6,9 @@ import React, { useState } from 'react'
 
 import { ZoomSlideLabels } from '../zoom-slide-labels'
 import { getColors } from '../../utils'
-import { getWindowDimensions } from '../../packages/get-window-dimensions'
 import { useSizes } from '../../hooks'
 
-import { getGestureHandlerCallbacks } from './utils'
+import { getGestureHandlerCallbacks, getSlideFacts } from './utils'
 import { useLabelStateSetterRef } from './hooks'
 
 type ZoomSlideVerticalVisibleProps = {
@@ -20,18 +19,16 @@ export const ZoomSlideVerticalVisible = (
   props: ZoomSlideVerticalVisibleProps
 ): React.ReactElement => {
   const { restrictingColumnBounds, totalHoles } = props
-  const slotCount = totalHoles - 1
-  const { shortEdge: trackLength } = getWindowDimensions()
-  const slotSize = trackLength / slotCount
+  const { trackLength, slideLength, slideSpan, slotCount } = getSlideFacts(
+    restrictingColumnBounds,
+    totalHoles
+  )
 
   const { dynamicSizes } = useSizes()
   const { inertOutline } = getColors()
 
   const [slideOffset, setSlideOffset] = useState<number>(0)
   const [, setSourceColumnBounds] = useGlobal('sourceColumnBounds')
-  const [startSlot, endSlot] = restrictingColumnBounds
-  const slideSpan = endSlot - startSlot
-  const slideLength = slotSize * slideSpan
   const styles = StyleSheet.create({
     componentWrapper: {
       ...StyleSheet.absoluteFillObject,
