c7f7c30ff392bec18a3576450ca5b49e2482b515
js-jslog <jhs4jbs@hotmail.co.uk>
2021-11-07 16:57:02 +0000

    Add test and functionality for max offset calculation

--- packages/harpguru-core/src/components/zoom-slide-vertical-new/utils/get-with-gesture-offset/get-with-gesture-offset.test.ts
+++ packages/harpguru-core/src/components/zoom-slide-vertical-new/utils/get-with-gesture-offset/get-with-gesture-offset.test.ts
@@ -1,19 +1,59 @@
 import { getWithGestureOffset } from './get-with-gesture-offset'
 
 test('minimum offset following gesture is 0', () => {
-  const offset_1 = 0
+  const slideLength = 10
+  const trackLength = 20
+
+  const prevOffset_1 = 0
   const translationY_1 = 0
-  expect(getWithGestureOffset(offset_1, translationY_1)).toBe(0)
+  expect(
+    getWithGestureOffset(prevOffset_1, translationY_1, slideLength, trackLength)
+  ).toBe(0)
 
-  const offset_2 = 0
+  const prevOffset_2 = 0
   const translationY_2 = -10
-  expect(getWithGestureOffset(offset_2, translationY_2)).toBe(0)
+  expect(
+    getWithGestureOffset(prevOffset_2, translationY_2, slideLength, trackLength)
+  ).toBe(0)
 
-  const offset_3 = 100
+  const prevOffset_3 = 100
   const translationY_3 = -100
-  expect(getWithGestureOffset(offset_3, translationY_3)).toBe(0)
+  expect(
+    getWithGestureOffset(prevOffset_3, translationY_3, slideLength, trackLength)
+  ).toBe(0)
 
-  const offset_4 = 100
+  const prevOffset_4 = 100
   const translationY_4 = -200
-  expect(getWithGestureOffset(offset_4, translationY_4)).toBe(0)
+  expect(
+    getWithGestureOffset(prevOffset_4, translationY_4, slideLength, trackLength)
+  ).toBe(0)
+})
+
+test('maximum offset following gesture takes slider to end of track', () => {
+  const slideLength = 10
+  const trackLength = 20
+
+  const prevOffset_1 = 10
+  const translationY_1 = 0
+  expect(
+    getWithGestureOffset(prevOffset_1, translationY_1, slideLength, trackLength)
+  ).toBe(10)
+
+  const prevOffset_2 = 0
+  const translationY_2 = 10
+  expect(
+    getWithGestureOffset(prevOffset_2, translationY_2, slideLength, trackLength)
+  ).toBe(10)
+
+  const prevOffset_3 = 0
+  const translationY_3 = 100
+  expect(
+    getWithGestureOffset(prevOffset_3, translationY_3, slideLength, trackLength)
+  ).toBe(10)
+
+  const prevOffset_4 = 100
+  const translationY_4 = 1
+  expect(
+    getWithGestureOffset(prevOffset_4, translationY_4, slideLength, trackLength)
+  ).toBe(10)
 })
--- packages/harpguru-core/src/components/zoom-slide-vertical-new/utils/get-with-gesture-offset/get-with-gesture-offset.ts
+++ packages/harpguru-core/src/components/zoom-slide-vertical-new/utils/get-with-gesture-offset/get-with-gesture-offset.ts
@@ -1,8 +1,12 @@
 export const getWithGestureOffset = (
   prevOffset: number,
-  translationY: number
+  translationY: number,
+  slideLength: number,
+  trackLength: number
 ): number => {
   const nextAbsoluteOffset = prevOffset + translationY
+  const maxSlideOffset = trackLength - slideLength
   if (nextAbsoluteOffset <= 0) return 0
+  if (nextAbsoluteOffset >= maxSlideOffset) return maxSlideOffset
   return 999
 }
