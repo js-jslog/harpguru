64cd7fed7c38cbb0a043914e3df2c1b553b24a9d
js-jslog <jhs4jbs@hotmail.co.uk>
2021-11-09 18:04:46 +0000

    Remove unused facts from util

--- packages/harpguru-core/src/components/zoom-slide-vertical-visible/utils/get-slide-facts/get-slide-facts.ts
+++ packages/harpguru-core/src/components/zoom-slide-vertical-visible/utils/get-slide-facts/get-slide-facts.ts
@@ -1,34 +1,29 @@
 import { getWindowDimensions } from '../../../../packages/get-window-dimensions'
 
 type SlideFacts = {
-  readonly slideHeadIndex: number
-  readonly slideTailIndex: number
   readonly trackLength: number
   readonly slideHeadOffset: number
   readonly slideLength: number
-  readonly slideSpan: number
   readonly slotCount: number
+  readonly slideSpan: number
 }
 export const getSlideFacts = (
   columnBounds: readonly [number, number],
   columnCount: number
 ): SlideFacts => {
-  const [slideHeadIndex, slideTailIndex] = columnBounds
+  const [startBound, endBound] = columnBounds
   const slotCount = columnCount - 1
   const { shortEdge: trackLength } = getWindowDimensions()
   const slotLength = trackLength / slotCount
-  const slideHeadOffset = slotLength * slideHeadIndex
-  const [startBound, endBound] = columnBounds
+  const slideHeadOffset = slotLength * startBound
   const slideSpan = endBound - startBound
   const slideLength = slotLength * slideSpan
 
   return {
-    slideHeadIndex,
-    slideTailIndex,
     trackLength,
     slideHeadOffset,
     slideLength,
-    slideSpan,
     slotCount,
+    slideSpan,
   }
 }
