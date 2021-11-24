76d953eafe3b636f72be98504a97181e28eb018b
js-jslog <jhs4jbs@hotmail.co.uk>
2021-11-24 20:46:29 +0000

    Fix second harpface layout issue

--- packages/harpguru-core/src/components/harp-face/hooks/use-styles.ts
+++ packages/harpguru-core/src/components/harp-face/hooks/use-styles.ts
@@ -21,7 +21,9 @@ export const useStyles = (
     [harpfaceIndex === 'harpface1' ? 0 : 1]: { harpfaceRows, harpfaceColumns },
   } = layoutFacts
 
-  const octaveColumnGroups = useOctaveColumnGroups(harpfaceIndex)
+  // TODO: Should we perhaps just make useOctaveColumnGroups always use
+  // harpface1 rather than requiring that it be passed
+  const octaveColumnGroups = useOctaveColumnGroups('harpface1')
   const { length: groupCount } = octaveColumnGroups
 
   const styles = StyleSheet.create({
