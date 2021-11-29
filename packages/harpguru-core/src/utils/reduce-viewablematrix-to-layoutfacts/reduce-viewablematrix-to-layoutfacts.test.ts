2445fb13f589bde32686eab76dd18d0b20520c09
js-jslog <jhs4jbs@hotmail.co.uk>
2021-11-29 20:56:58 +0000

    Add unit tests for recently extended chromatic functionality

--- packages/harpguru-core/src/utils/reduce-viewablematrix-to-layoutfacts/reduce-viewablematrix-to-layoutfacts.test.ts
+++ packages/harpguru-core/src/utils/reduce-viewablematrix-to-layoutfacts/reduce-viewablematrix-to-layoutfacts.test.ts
@@ -1,40 +1,95 @@
 import { reduceViewableMatrixToLayoutFacts } from './reduce-viewablematrix-to-layoutfacts'
 
-const viewableMatrix = {
+const viewableMatrixChromatic = {
   harpface1: [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ],
+  harpface2: [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ],
+}
+const viewableMatrixDiatonic = {
+  harpface1: viewableMatrixChromatic.harpface1,
 }
 
 test('the previous layoutFacts are returned if they match the derived ones', () => {
-  const prevLayoutFacts = {
+  const prevLayoutFactsDiatonic = {
     harpface1: {
       harpfaceColumns: 10,
       harpfaceRows: 4,
     },
   } as const
+  const prevLayoutFactsChromatic = {
+    harpface1: {
+      harpfaceColumns: 10,
+      harpfaceRows: 4,
+    },
+    harpface2: {
+      harpfaceColumns: 10,
+      harpfaceRows: 2,
+    },
+  } as const
   expect(
-    reduceViewableMatrixToLayoutFacts(prevLayoutFacts, viewableMatrix)
-  ).toBe(prevLayoutFacts)
+    reduceViewableMatrixToLayoutFacts(
+      prevLayoutFactsDiatonic,
+      viewableMatrixDiatonic
+    )
+  ).toBe(prevLayoutFactsDiatonic)
+  expect(
+    reduceViewableMatrixToLayoutFacts(
+      prevLayoutFactsChromatic,
+      viewableMatrixChromatic
+    )
+  ).toBe(prevLayoutFactsChromatic)
 })
 
 test('an appropriate layoutFacts is returned for a given viewableMatrix', () => {
-  const prevLayoutFacts = {
-    harpface1: {
-      harpfaceColumns: 1,
-      harpfaceRows: 1,
-    },
-  } as const
-  const expectedLayoutFacts = {
+  const prevLayoutFactsDiatonic = {
     harpface1: {
       harpfaceColumns: 10,
       harpfaceRows: 4,
     },
   } as const
+  const prevLayoutFactsChromatic = {
+    harpface1: {
+      harpfaceColumns: 10,
+      harpfaceRows: 4,
+    },
+    harpface2: {
+      harpfaceColumns: 10,
+      harpfaceRows: 2,
+    },
+  } as const
+  const expectedLayoutFactsDiatonic = {
+    harpface1: {
+      harpfaceColumns: 10,
+      harpfaceRows: 4,
+    },
+  } as const
+  const expectedLayoutFactsChromatic = {
+    harpface1: {
+      harpfaceColumns: 10,
+      harpfaceRows: 4,
+    },
+    harpface2: {
+      harpfaceColumns: 10,
+      harpfaceRows: 2,
+    },
+  } as const
   expect(
-    reduceViewableMatrixToLayoutFacts(prevLayoutFacts, viewableMatrix)
-  ).toStrictEqual(expectedLayoutFacts)
+    reduceViewableMatrixToLayoutFacts(
+      prevLayoutFactsChromatic,
+      viewableMatrixDiatonic
+    )
+  ).toStrictEqual(expectedLayoutFactsDiatonic)
+  expect(
+    reduceViewableMatrixToLayoutFacts(
+      prevLayoutFactsDiatonic,
+      viewableMatrixChromatic
+    )
+  ).toStrictEqual(expectedLayoutFactsChromatic)
 })
