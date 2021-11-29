c2d80e21bb893f67c900014a70d05b14dca4386c
js-jslog <jhs4jbs@hotmail.co.uk>
2021-11-29 19:40:02 +0000

    Remove unnecessary TODO
    There probably is a need to rename this if we come up with a better name
    for the `HarpFaceFacts` type and it's associates. That's not in scope
    right now and that task has already been recorded in the github project.

--- packages/harpparts/src/types/typeguards.ts
+++ packages/harpparts/src/types/typeguards.ts
@@ -1,6 +1,5 @@
 import type { ChromaticHarpFaceFacts, HarpFaceFacts } from './types'
 
-// TODO: rename this function
 export const isChromaticHarpFace = <T>(
   props: HarpFaceFacts<T>
 ): props is ChromaticHarpFaceFacts<T> => {
