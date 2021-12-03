10aeb260006e54cd42b9e979c6e6d88bf3982f1f
js-jslog <jhs4jbs@hotmail.co.uk>
2021-12-03 12:58:39 +0000

    Use typeguard to replace adhoc type checking
    This wouldn't have strictly fitted in to the feature change which it's
    being performed in. However, the feature change (adding chromatics) lead
    to a review of the function (as you can see from the TODO above the
    changed lines in this commit), and this became a relevant change as a
    result.

--- packages/harpstrata/src/get-harp-strata/utils/get-active-ids-pair/get-active-ids-pair.ts
+++ packages/harpstrata/src/get-harp-strata/utils/get-active-ids-pair/get-active-ids-pair.ts
@@ -1,4 +1,4 @@
-import { DegreeIds } from 'harpparts'
+import { DegreeIds, isPitchId } from 'harpparts'
 import type { PitchIds } from 'harpparts'
 
 import {
@@ -13,15 +13,12 @@ import type { IsActiveProps, ActiveIdsPair } from '../../../types'
 // it's going to just give you all of the active pairs for the whole context.
 export const getActiveIdsPair = (props: IsActiveProps): ActiveIdsPair => {
   const { activeIds } = props
-  // TODO: Can replace this logic with a typeguard now. It took a
-  // minute for me to figure out what was going on.
-  const possibleDegreeId = activeIds[0] as DegreeIds
-  if (Object.values(DegreeIds).includes(possibleDegreeId)) {
-    const activeDegreeIds = [...(activeIds as ReadonlyArray<DegreeIds>)].sort()
-    const activePitchIds = [...getCounterpartPitchIds(props)].sort()
+  if (isPitchId(activeIds[0])) {
+    const activePitchIds = [...(activeIds as ReadonlyArray<PitchIds>)].sort()
+    const activeDegreeIds = [...getCounterpartDegreeIds(props)].sort()
     return { activeDegreeIds, activePitchIds }
   }
-  const activePitchIds = [...(activeIds as ReadonlyArray<PitchIds>)].sort()
-  const activeDegreeIds = [...getCounterpartDegreeIds(props)].sort()
+  const activeDegreeIds = [...(activeIds as ReadonlyArray<DegreeIds>)].sort()
+  const activePitchIds = [...getCounterpartPitchIds(props)].sort()
   return { activeDegreeIds, activePitchIds }
 }
