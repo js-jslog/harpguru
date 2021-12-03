27df452d9a908cab1d2368ffeae7f738a6fd8fb6
js-jslog <jhs4jbs@hotmail.co.uk>
2021-12-03 13:00:40 +0000

    Remove TODO
    Not because it's been done or isn't relevant anymore, but because it's
    actually not as critical as it initially seemed and an issue has been
    raised to look in to it: https://github.com/js-jslog/harpguru/issues/136

--- packages/harpstrata/src/get-harp-strata/utils/get-active-ids-pair/get-active-ids-pair.ts
+++ packages/harpstrata/src/get-harp-strata/utils/get-active-ids-pair/get-active-ids-pair.ts
@@ -7,10 +7,6 @@ import {
 } from '../get-counterpart-active-ids'
 import type { IsActiveProps, ActiveIdsPair } from '../../../types'
 
-// TODO: This function definitely need to deal with both of the harpface matrices
-// if there are 2 available. Or it needs to be called in a way that deals with both.
-// I think I favour the first option here since the name definitely suggest that
-// it's going to just give you all of the active pairs for the whole context.
 export const getActiveIdsPair = (props: IsActiveProps): ActiveIdsPair => {
   const { activeIds } = props
   if (isPitchId(activeIds[0])) {
