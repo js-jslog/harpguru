ab215158df664172644a6396bccb7930c97be5f4
js-jslog <jhs4jbs@hotmail.co.uk>
2022-01-03 21:37:51 +0000

    Format tunings in readable format

--- packages/harpparts/src/tuning/constants/chromatic-tunings.ts
+++ packages/harpparts/src/tuning/constants/chromatic-tunings.ts
@@ -58,122 +58,58 @@ const {
   db5,
 } = ReedTuningPitches
 
+// prettier-ignore
 export const soloSixteenHoleChromatic = {
   harpface1: [
     // 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16
-    [c1, e1, g1, c2, c2, e2, g2, c3, c3, e3, g3, c4, c4, e4, g4, c5] as const,
-    [d1, f1, a1, b1, d2, f2, a2, b2, d3, f3, a3, b3, d4, f4, a4, b4] as const,
+    [ c1 , e1 , g1 , c2 , c2 , e2 , g2 , c3 , c3 , e3 , g3 , c4 , c4 , e4 , g4 , c5 ] as const,
+    [ d1 , f1 , a1 , b1 , d2 , f2 , a2 , b2 , d3 , f3 , a3 , b3 , d4 , f4 , a4 , b4 ] as const,
   ] as const,
   harpface2: [
     // 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16
-    [
-      db1,
-      f1,
-      ab1,
-      db2,
-      db2,
-      f2,
-      ab2,
-      db3,
-      db3,
-      f3,
-      ab3,
-      db4,
-      db4,
-      f4,
-      ab4,
-      db5,
-    ] as const,
-    [
-      eb1,
-      gb1,
-      bb1,
-      c2,
-      eb2,
-      gb2,
-      bb2,
-      c3,
-      eb3,
-      gb3,
-      bb3,
-      c4,
-      eb4,
-      gb4,
-      bb4,
-      c5,
-    ] as const,
+    [ db1, f1 , ab1, db2, db2, f2 , ab2, db3, db3, f3 , ab3, db4, db4, f4 , ab4, db5] as const,
+    [ eb1, gb1, bb1, c2 , eb2, gb2, bb2, c3 , eb3, gb3, bb3, c4 , eb4, gb4, bb4, c5 ] as const,
   ] as const,
 } as const
 
+// prettier-ignore
 export const soloTwelveHoleChromatic = {
   harpface1: [
     // 1    2    3    4    5    6    7    8    9   10   11   12
-    [c2, e2, g2, c3, c3, e3, g3, c4, c4, e4, g4, c5] as const,
-    [d2, f2, a2, b2, d3, f3, a3, b3, d4, f4, a4, b4] as const,
+    [ c2 , e2 , g2 , c3 , c3 , e3 , g3 , c4 , c4 , e4 , g4 , c5 ] as const,
+    [ d2 , f2 , a2 , b2 , d3 , f3 , a3 , b3 , d4 , f4 , a4 , b4 ] as const,
   ] as const,
   harpface2: [
     // 1    2    3    4    5    6    7    8    9   10   11   12
-    [db2, f2, ab2, db3, db3, f3, ab3, db4, db4, f4, ab4, db5] as const,
-    [eb2, gb2, bb2, c3, eb3, gb3, bb3, c4, eb4, gb4, bb4, c5] as const,
+    [ db2, f2 , ab2, db3, db3, f3 , ab3, db4, db4, f4 , ab4, db5] as const,
+    [ eb2, gb2, bb2, c3 , eb3, gb3, bb3, c4 , eb4, gb4, bb4, c5 ] as const,
   ] as const,
 } as const
 
+// prettier-ignore
 export const orchestraSixteenHoleChromatic = {
   harpface1: [
     // 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16
-    [g0, c1, c1, e1, g1, c2, c2, e2, g2, c3, c3, e3, g3, c4, c4, e4] as const,
-    [a0, b0, d1, f1, a1, b1, d2, f2, a2, b2, d3, f3, a3, b3, d4, f4] as const,
+    [ g0 , c1 , c1 , e1 , g1 , c2 , c2 , e2 , g2 , c3 , c3 , e3 , g3 , c4 , c4 , e4 ] as const,
+    [ a0 , b0 , d1 , f1 , a1 , b1 , d2 , f2 , a2 , b2 , d3 , f3 , a3 , b3 , d4 , f4 ] as const,
   ] as const,
   harpface2: [
     // 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16
-    [
-      ab0,
-      db1,
-      db1,
-      f1,
-      ab1,
-      db2,
-      db2,
-      f2,
-      ab2,
-      db3,
-      db3,
-      f3,
-      ab3,
-      db4,
-      db4,
-      f4,
-    ] as const,
-    [
-      bb0,
-      c1,
-      eb1,
-      gb1,
-      bb1,
-      c2,
-      eb2,
-      gb2,
-      bb2,
-      c3,
-      eb3,
-      gb3,
-      bb3,
-      c4,
-      eb4,
-      gb4,
-    ] as const,
+    [ ab0, db1, db1, f1 , ab1, db2, db2, f2 , ab2, db3, db3, f3 , ab3, db4, db4, f4 ] as const,
+    [ bb0, c1 , eb1, gb1, bb1, c2 , eb2, gb2, bb2, c3 , eb3, gb3, bb3, c4 , eb4, gb4] as const,
   ] as const,
 }
 
+// prettier-ignore
 export const orchestraTwelveHoleChromatic = {
   harpface1: [
     // 1    2    3    4    5    6    7    8    9   10   11   12
-    [g1, c2, c2, e2, g2, c3, c3, e3, g3, c4, c4, e4] as const,
-    [a1, b1, d2, f2, a2, b2, d3, f3, a3, b3, d4, f4] as const,
+    [ g1 , c2 , c2 , e2 , g2 , c3 , c3 , e3 , g3 , c4 , c4 , e4 ] as const,
+    [ a1 , b1 , d2 , f2 , a2 , b2 , d3 , f3 , a3 , b3 , d4 , f4 ] as const,
   ] as const,
   harpface2: [
     // 1    2    3    4    5    6    7    8    9   10   11   12
-    [ab1, db2, db2, f2, ab2, db3, db3, f3, ab3, db4, db4, f4] as const,
-    [bb1, c2, eb2, gb2, bb2, c3, eb3, gb3, bb3, c4, eb4, gb4] as const,
+    [ ab1, db2, db2, f2 , ab2, db3, db3, f3 , ab3, db4, db4, f4 ] as const,
+    [ bb1, c2 , eb2, gb2, bb2, c3 , eb3, gb3, bb3, c4 , eb4, gb4] as const,
   ] as const,
 }
