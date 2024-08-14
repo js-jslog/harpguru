59f15ac668d317c9096846c7ab54dfdfd5a99703
sinfiej <joseph.sinfield@rullion.co.uk>
2024-08-14 21:24:29 +0000

    Fix all the unit tests and create root script

--- packages/harpguru-core/jest-setup.js
+++ packages/harpguru-core/jest-setup.js
@@ -1,4 +1,4 @@
-require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests()
+require('react-native-reanimated/lib/module/reanimated2/jestUtils.js').setUpTests()
 
 jest.mock('react-native-reanimated', () =>
   require('react-native-reanimated/mock')
